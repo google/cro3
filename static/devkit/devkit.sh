b12c45d44f831b68eecdc27e49a84bd243e1e517
cmasone@google.com <cmasone@google.com@06c00378-0e64-4dae-be16-12b19f9950a1>
2009-10-30 21:50:53 +0000

    devkit: create needed dirs for apt if they're not there.
    starting the devkit upstart job can fail if two dirs that apt seems to
    need aren't there.  This makes the script create them.
    
    Review URL: http://chromereview.prom.corp.google.com/1181014
    
    
    git-svn-id: svn://chrome-svn/chromeos/trunk@139 06c00378-0e64-4dae-be16-12b19f9950a1

--- static/devkit/devkit.sh
+++ static/devkit/devkit.sh
@@ -17,6 +17,8 @@ sudo rm -f /etc/init/software-update.conf
 sudo echo "deb http://archive.ubuntu.com/ubuntu karmic main restricted multiverse universe" >> /tmp/devsrc 
 
 sudo cp /tmp/devsrc /etc/apt/sources.list
+sudo mkdir -p /var/cache/apt/archives/partial
+sudo mkdir -p /var/log/apt
 sudo apt-get update
 
 sudo apt-get install -y vim
