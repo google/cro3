304a184d864a92d5d79edbd3ac0213e5b9e36455
adlr@google.com <adlr@google.com@06c00378-0e64-4dae-be16-12b19f9950a1>
2009-11-18 21:43:21 +0000

    Devkit: split out developer customization.
    This way, a user can create their own devkit-custom.sh file,
    optionally based on the template supplied. Creating/editing this file
    will not cause any files in the source tree to be edited, thus an
    individual developer's changes will not easily be accidentally checked
    in. It also makes it easier to keep the same devkit-custom.sh file
    around when switching git branches.
    
    Developers should not, in general, have to edit devkit.sh.
    
    Review URL: http://chromereview.prom.corp.google.com/1188043
    
    
    git-svn-id: svn://chrome-svn/chromeos/trunk@281 06c00378-0e64-4dae-be16-12b19f9950a1

--- static/devkit/devkit.sh
+++ static/devkit/devkit.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Copyright (c) 2009 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
@@ -8,7 +8,7 @@ set -e
 
 DEVKIT_URL=$(grep ^CHROMEOS_DEVSERVER /etc/lsb-release | cut -d = -f 2-) 
 
-if [ "x" = "x$DEVKIT_URL" ]
+if [ -z "$DEVKIT_URL" ]
 then
   echo "No devkit server specified in /etc/lsb-release"
   exit 1
@@ -18,21 +18,17 @@ sudo mount -o remount,rw /
 
 sudo rm -f /etc/init/software-update.conf
 
-sudo echo "deb http://archive.ubuntu.com/ubuntu karmic main restricted multiverse universe" >> /tmp/devsrc 
+sudo echo "deb http://chromeos-deb.corp.google.com/ubuntu" \
+  "karmic main restricted multiverse universe" > /etc/apt/sources.list
 
-sudo cp /tmp/devsrc /etc/apt/sources.list
 sudo mkdir -p /var/cache/apt/archives/partial
 sudo mkdir -p /var/log/apt
 sudo apt-get update
 
-sudo apt-get install -y vim || true
-sudo apt-get install -y sshfs || true
-sudo apt-get install -y gdb || true
+wget "$DEVKIT_URL/static/devkit/devkit-custom.sh" -O - | sudo /bin/sh || true
 
-sudo wget $DEVKIT_URL/static/devkit/vimrc.txt -O /home/chronos/.vimrc
-sudo chmod a+r /home/chronos/.vimrc
-
-sudo wget $DEVKIT_URL/static/devkit/chromeos-builder -O /usr/bin/chromeos-builder
+sudo wget "$DEVKIT_URL/static/devkit/chromeos-builder" \
+  -O /usr/bin/chromeos-builder
 sudo chmod a+x /usr/bin/chromeos-builder
 
-sudo wget $DEVKIT_URL/static/devkit/devkit.conf -O /etc/init/devkit.conf
+sudo wget "$DEVKIT_URL/static/devkit/devkit.conf" -O /etc/init/devkit.conf
