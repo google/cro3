699b1d30e97f12097fc788b1475c39be1f6ffd81
Arkaitz Ruiz Alvarez <arkaitzr@chromium.org>
2011-06-23 13:10:29 -0700

    Create developer mode install script
    This directory will contain all the necessary files to add
    some capabilities to a base image booted in developer mode. It
    will download and bootstrap first a version of emerge so the
    user can install packages. It will also give the option of
    installing a set of developer packages.
    
    BUG=chromium-os:16841
    TEST=None till actual code is added. This is a first commit
    adding the needed files.
    
    Change-Id: I17aae26a164ab361de059840749a5977f2ec3947
    Reviewed-on: http://gerrit.chromium.org/gerrit/3122
    Reviewed-by: Chris Sosa <sosa@chromium.org>
    Tested-by: Arkaitz Ruiz Alvarez <arkaitzr@chromium.org>

---
+++ dev-install/dev_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# This script downloads and installs the basic packages that the user needs
+# in developer mode. It also takes care of some configuration details
+# that arise from not havin write access to the root filesystem.
+
+# TODO(arkaitzr): more details (and actual code) to come.
+
+# Get the static Python binary and portage (emerge script and python modules).
+
+# Set PYTHONPATH so it can find the python modules for portage.
+
+# Set configuration files for portage.
+
+# Run emerge and install .
+
