30dd2d0973bf7f3e5f06e8258b2d427ce2b61f21
Ahmad Sharif <asharif@chromium.org>
2011-06-30 16:56:24 -0700

    Added ability to install missing compilers when using cros_gcc_config.
    Example: cros_gcc_config i686-pc-linux-gnu-4.6.0
    
    will install cross-i686-pc-linux-gnu/gcc-4.6.0 and copy over the
    necessary libraries to the relevant boards.
    
    Also fixed a bug with copying over libs for arm.
    
    BUG=none
    TEST=cros_gcc_config i686-pc-linux-gnu-4.6.0
    This installed gcc-4.6.0 with the correct use flags even if gold was the
    default linker.
    cros_gcc_config <number> to switch to the config of that number.
    
    Change-Id: I40ce020bdcc9513df172a937d177b85208e37aff
    Reviewed-on: http://gerrit.chromium.org/gerrit/3532
    Tested-by: asharif <asharif@chromium.org>
    Reviewed-by: David James <davidjames@chromium.org>

--- toolchain_utils.sh
+++ toolchain_utils.sh
@@ -9,15 +9,31 @@
 get_ctarget_from_board()
 {
   local board="$1"
-  local base_board=$(echo ${board} | cut -d '_' -f 1)
-  local board_overlay=$(cros_overlay_list --board="$base_board" --primary_only)
+  local base_board="$(echo $board | cut -d '_' -f 1)"
+  local board_overlay="$(cros_overlay_list --board="$base_board"\
+    --primary_only)"
   cat "$board_overlay/toolchain.conf"
+}
+
+is_number()
+{
+  echo "$1" | grep -q "^[0-9]\+$"
+}
+
+is_config_installed()
+{
+  gcc-config -l | cut -d" " -f3 | grep -q "$1$"
+}
+
+get_installed_atom_from_config()
+{
+  local gcc_path="$(gcc-config -B "$1")"
+  equery b "$gcc_path" | head -n1
 }
 
 get_atom_from_config()
 {
-  local gcc_path="$(gcc-config -B "$1")"
-  equery b ${gcc_path} | head -n1
+  echo "$1" | sed -E "s|(.*)-(.*)|cross-\1/gcc-\2|g"
 }
 
 get_ctarget_from_atom()
@@ -33,8 +49,7 @@ copy_gcc_libs_helper()
   local dir_path=$(dirname "$file_path")
   info "Copying $file_path symlink and file to $target_location/$dir_path/."
   sudo mkdir -p "$target_location/$dir_path"
-  sudo cp -a "$file_path" "$target_location/$dir_path/"
-  sudo cp -a "$(readlink -f $file_path)" "$target_location/$dir_path/"
+  sudo cp -a "$file_path"* "$target_location/$dir_path/"
   local env_d_file="$target_location/etc/env.d/05gcc"
   info "Adding $dir_path to LDPATH in file $env_d_file"
   sudo mkdir -p $(dirname "$env_d_file")
@@ -53,7 +68,7 @@ copy_gcc_libs()
   local libgcc_file=$(portageq contents / $atom | \
                       grep /libgcc_s.so$)
   local libstdcxx_file=$(portageq contents / $atom | \
-                         grep /libstdc++.so)
+                         grep /libstdc++.so$)
   if [[ -z "$libgcc_file" || -z "$libstdcxx_file" ]]
   then
     error "Could not find libgcc_s.so/libstdcxx_s.so. Is\
@@ -65,17 +80,85 @@ copy_gcc_libs()
   return 0
 }
 
+get_current_binutils_config()
+{
+  local ctarget="$1"
+  binutils-config -l | grep "$ctarget" | grep "*" | cut -d" " -f3
+}
+
+emerge_gcc()
+{
+  local atom="$1"
+  local ctarget="$(get_ctarget_from_atom $atom)"
+  mask_file="/etc/portage/package.mask/cross-$ctarget"
+  moved_mask_file=0
+
+  # Move the package mask file elsewhere.
+  if [[ -f "$mask_file" ]]
+  then
+    temp_file="$(mktemp)"
+    sudo mv "$mask_file" "$temp_file"
+    moved_mask_file=1
+  fi
+
+  USE+=" multislot"
+  if echo "$atom" | grep -q "gcc-4.6.0$"
+  then
+    USE+=" svn_sources"
+    old_binutils_config="$(get_current_binutils_config $ctarget)"
+    if [[ "$old_binutils_config" != "$ctarget-2.20.1" ]]
+    then
+      sudo binutils-config "$ctarget-2.20.1"
+    fi
+  fi
+  sudo ACCEPT_KEYWORDS="*" USE="$USE" emerge ="$atom"
+  emerge_retval=$?
+
+  # Move the package mask file back.
+  if [[ $moved_mask_file -eq 1 ]]
+  then
+    sudo mv "$temp_file" "$mask_file"
+  fi
+
+  if [[ ! -z "$old_binutils_config" &&
+        "$old_binutils_config" != "$(get_current_binutils_config $ctarget)" ]]
+  then
+    sudo binutils-config "$old_binutils_config"
+  fi
+
+  return $emerge_retval
+}
+
 cros_gcc_config()
 {
-  sudo gcc-config "$1" || return $?
-
   # Return if we're not switching profiles.
   if [[ "$1" == -* ]]
   then
-    return 0
+    sudo gcc-config "$1"
+    return $?
   fi
 
-  local atom=$(get_atom_from_config "$1")
+  # cros_gcc_config can be called like:
+  # cros_gcc_config <number> to switch config to that
+  # number. In that case we should just try to switch to
+  # that config and not try to install a missing one.
+  if ! is_number "$1" && ! is_config_installed "$1"
+  then
+    info "Configuration $1 not found."
+    info "Trying to emerge it..."
+    local atom="$(get_atom_from_config "$1")"
+    emerge_gcc "$atom" || die "Could not install $atom"
+  fi
+
+  sudo gcc-config "$1" || die "Could not switch to $1"
+
+  cros_install_libs_for_config "$1"
+}
+
+
+cros_install_libs_for_config()
+{
+  local atom=$(get_installed_atom_from_config "$1")
   if [[ $atom != cross* ]]
   then
     warn "$atom is not a cross-compiler."
@@ -84,14 +167,14 @@ cros_gcc_config()
   fi
 
   # Now copy the lib files into all possible boards.
-  local ctarget=$(get_ctarget_from_atom "$atom")
+  local ctarget="$(get_ctarget_from_atom "$atom")"
   for board_root in /build/*
   do
-    local board=$(basename $board_root)
-    local board_tc=$(get_ctarget_from_board $board)
+    local board="$(basename $board_root)"
+    local board_tc="$(get_ctarget_from_board $board)"
     if [[ "${board_tc}" == "${ctarget}" ]]
     then
-      copy_gcc_libs "$board_root" $atom
+      copy_gcc_libs "$board_root" "$atom"
     fi
   done
 }
