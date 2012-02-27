c9bb10819c76cf72b20457c297b2ec7bae2dc9c2
Chris Sosa <sosa@chromium.org>
2012-02-27 14:52:44 -0800

    Remove un-needed set -x/+x since ssh issues have been long resolved.
    BUG=chromium-os:20985
    TEST=Ran image_to_live
    
    Change-Id: Ibced33d39753d12794ffcb330850e7620598027f
    Reviewed-on: https://gerrit.chromium.org/gerrit/16857
    Tested-by: Chris Sosa <sosa@chromium.org>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>
    Commit-Ready: Chris Sosa <sosa@chromium.org>

--- host/image_to_live.sh
+++ host/image_to_live.sh
@@ -425,9 +425,6 @@ function main() {
 
   remote_reboot
 
-  # TODO(sosa): Remove once ssh debugging issues are resolved.
-  set -x
-
   if [[ ${FLAGS_update_hostkey} -eq ${FLAGS_TRUE} ]]; then
     local known_hosts="${HOME}/.ssh/known_hosts"
     cp "${known_hosts}" "${known_hosts}~"
@@ -451,8 +448,6 @@ function main() {
     local release_description=$(echo ${REMOTE_OUT} | cut -d '=' -f 2)
     info "Update was successful and rebooted to $release_description"
   fi
-
-  set +x
 
   print_time_elapsed
 
