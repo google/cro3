cf1b0688b6138345b5cc41c007de3a494d9b94d1
Chris Sosa <sosa@chromium.org>
2012-06-08 19:58:59 -0700

    Add apache setup script to set up an apache server to run with the devserver.
    This script and accompany configs sets up apache to run on port 8082 and direct
    non-file traffic to the devserver i.e. for any paths not in the static dir.
    This means all RPCs will go to the running devserver on 8080 yet apache
    will handle requests to files.
    
    Once this script is run. We need only modify the autotest config to contact
    the devserver on port 8082 rather than 8080 allowing for easy staging/testing
    of apache with an autotest server.
    
    BUG=chromium-os:26451
    TEST=Set up locally against my own static dir with the devserver running
    in production settings. Made calls to download and wait_for_url and also
    ensured I could grab files from static/archive.
    
    Change-Id: Ia1ff2f3da7f038e64d7a577fed340b8b7d7797c0
    Reviewed-on: https://gerrit.chromium.org/gerrit/24939
    Tested-by: Chris Sosa <sosa@chromium.org>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>
    Reviewed-by: Scott Zawalski <scottz@chromium.org>

---
+++ apache/setup_apache.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This script can be used to set up apache to interact with the devserver.
+# This apache server will run on port 8082 and rewrite requests to the
+# devserver that aren't stored in the IMAGE_ROOT.
+
+# Usage:
+# ./setup_apache.sh -- Sets up using default static directory.
+# ./setup_apache.sh DIR -- Sets up apache to serve images from DIR.
+
+DEFAULT_IMAGE_ROOT=/home/chrome-test/images
+APACHE_CONFIG=/etc/apache2
+ARCHIVE_ROOT=/var/www
+MY_DIR="$(dirname $0)"
+
+set -e
+
+main () {
+  sudo apt-get install apache2
+
+  # Copy over configuration data.
+  mkdir -m 755 -p "${ARCHIVE_ROOT}"
+  cp -f "${MY_DIR}"/htaccess "${ARCHIVE_ROOT}/.htaccess"
+  cp -f "${MY_DIR}"/ports.conf "${APACHE_CONFIG}"
+  cp -f "${MY_DIR}"/devserver "${APACHE_CONFIG}"/sites-available
+
+  sudo chmod a+r -R ${ARCHIVE_ROOT}
+
+  # Enable configurations.
+  ln -sf "${APACHE_CONFIG}"/sites-available/devserver \
+    "${APACHE_CONFIG}"/sites-enabled
+
+  ln -sf "${APACHE_CONFIG}"/mods-available/proxy.load \
+    "${APACHE_CONFIG}"/mods-enabled
+  ln -sf "${APACHE_CONFIG}"/mods-available/proxy_http.load \
+    "${APACHE_CONFIG}"/mods-enabled
+
+  # Setup devserver archive location.
+  local image_root="${DEFAULT_IMAGE_ROOT}"
+  [ -n "${1}" ] && image_root="${1}"
+
+  local static_dir="${ARCHIVE_ROOT}"/static
+  if [ -e "${static_dir}" ]; then
+    unlink "${static_dir}"
+  fi
+
+  ln -sf "${image_root}" "${static_dir}"
+
+  /etc/init.d/apache2 restart
+}
+
+main $@
