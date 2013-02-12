a9cf3c57dc38243f481050f0e18ae1c7fd2b7801
Gilad Arnold <garnold@chromium.org>
2013-02-12 10:42:54 -0800

    Utilities needed for update payload checking.
    These provide required functionality for an upcoming payload checking
    tool. Includes two modules: format_utils.py and histogram.py, along with
    unittest modules. Note that the run_unittests script can be used to run
    these modules, just run from host/lib:
    
     $ ../../run_unittests
    
    Otherwise, unit tests are run by the ebuild, too (see CQ-DEPEND for the
    CL that implements this).
    
    BUG=chromium-os:34911,chromium-os:33607
    TEST=Unit tested; function as intended in upcoming client code.
    CQ-DEPEND=I4f23c961e4ed3d56bffe719d44927bd4a45ae8b3
    
    Change-Id: I72e2199ad6b9161c5ba1be86ef92245eccfd308a
    Reviewed-on: https://gerrit.chromium.org/gerrit/43431
    Reviewed-by: Gilad Arnold <garnold@chromium.org>
    Tested-by: Gilad Arnold <garnold@chromium.org>
    Commit-Queue: Gilad Arnold <garnold@chromium.org>

---
+++ host/lib/histogram_unittest.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Unit tests for histogram.py."""
+
+import unittest
+
+from histogram import Histogram
+import format_utils
+
+
+class HistogramTest(unittest.TestCase):
+  @staticmethod
+  def AddHumanReadableSize(size):
+    fmt = format_utils.BytesToHumanReadable(size)
+    return '%s (%s)' % (size, fmt) if fmt else str(size)
+
+  def CompareToExpectedDefault(self, actual_str):
+    expected_str = (
+        'Yes |################    | 5 (83.3%)\n'
+        'No  |###                 | 1 (16.6%)'
+    )
+    self.assertEqual(actual_str, expected_str)
+
+  def testExampleHistogram(self):
+    self.CompareToExpectedDefault(str(Histogram([('Yes', 5), ('No', 1)])))
+
+  def testFromCountDict(self):
+    self.CompareToExpectedDefault(str(Histogram.FromCountDict(
+        {'Yes': 5, 'No': 1})))
+
+  def testFromKeyList(self):
+    self.CompareToExpectedDefault(str(Histogram.FromKeyList(
+        ['Yes', 'Yes', 'No', 'Yes', 'Yes', 'Yes'])))
+
+  def testCustomScale(self):
+    expected_str = (
+        'Yes |#### | 5 (83.3%)\n'
+        'No  |     | 1 (16.6%)'
+    )
+    actual_str = str(Histogram([('Yes', 5), ('No', 1)], scale=5))
+    self.assertEqual(actual_str, expected_str)
+
+  def testCustomFormatter(self):
+    expected_str = (
+        'Yes |################    | 5000 (4.8 KiB) (83.3%)\n'
+        'No  |###                 | 1000 (16.6%)'
+    )
+    actual_str = str(Histogram([('Yes', 5000), ('No', 1000)],
+                               formatter=self.AddHumanReadableSize))
+    self.assertEqual(actual_str, expected_str)
+
+
+if __name__ == '__main__':
+  unittest.main()
