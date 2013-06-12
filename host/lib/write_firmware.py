8ae67891011a213050991a483d013988c535554d
Vadim Bendebury <vbendeb@chromium.org>
2013-06-11 17:45:15 -0700

    cros_bundle_firmware: fix modification inadvertently left behind
    The last minute change in the code was not reverted, resulting in
    failures to run the utility when creating a flasher.
    
    BUG=chrome-os-partner:18447
    TEST=manual
      . verified that cros_bundle firmware and cros_write_firmware work as expected.
    
    Change-Id: If6b854017d6d5f693bd7acb93ae0aa3f0975ecd7
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/58278
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- host/lib/write_firmware.py
+++ host/lib/write_firmware.py
@@ -681,7 +681,7 @@ class WriteFirmware:
 
       bl2_handler = ExynosBl2(self._tools, self._out)
       bl2_file = bl2_handler.Configure(self._fdt, spl_load_size,
-                                       bl2, 'flasher', False)
+                                       bl2, 'flasher', True)
       data = self._tools.ReadFile(bl1) + self._tools.ReadFile(bl2_file)
 
       # Pad BL2 out to the required size.
