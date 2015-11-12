7b9b6a9fd0694afcea67d8c2650df468400a14d7
Dan Shi <dshi@chromium.org>
2015-11-12 01:00:29 -0800

    Add retry in downloading Android artifacts.
    BUG=chromium:512668
    TEST=local test
    
    Change-Id: Ic084079fd9eac71ac4cca40a702f1337d9eca2aa
    Reviewed-on: https://chromium-review.googlesource.com/312503
    Commit-Ready: Dan Shi <dshi@google.com>
    Tested-by: Dan Shi <dshi@google.com>
    Reviewed-by: Dan Shi <dshi@google.com>

--- android_build.py
+++ android_build.py
@@ -9,13 +9,24 @@ from __future__ import print_function
 import apiclient
 import httplib2
 import io
+import subprocess
 
 from apiclient import discovery
 from oauth2client.client import SignedJwtAssertionCredentials
 
+import retry
+
+
 CREDENTIAL_SCOPE = 'https://www.googleapis.com/auth/androidbuild.internal'
 DEFAULT_BUILDER = 'androidbuildinternal'
 DEFAULT_CHUNKSIZE = 20*1024*1024
+# Maximum attempts to interact with Launch Control API.
+MAX_ATTEMPTS = 10
+# Timeout in minutes for downloading attempt.
+DOWNLOAD_TIMEOUT_MINS = 30
+# Timeout in minutes for API query.
+QUERY_TIMEOUT_MINS = 1
+
 
 class AndroidBuildFetchError(Exception):
   """Exception to raise when failed to make calls to Android build server."""
@@ -28,6 +39,7 @@ class BuildAccessor(object):
   credential_info = None
 
   @classmethod
+  @retry.retry(Exception, timeout_min=QUERY_TIMEOUT_MINS)
   def _GetServiceObject(cls):
     """Returns a service object with given credential information."""
     if not cls.credential_info:
@@ -37,8 +49,8 @@ class BuildAccessor(object):
         cls.credential_info['client_email'],
         cls.credential_info['private_key'], CREDENTIAL_SCOPE)
     http_auth = credentials.authorize(httplib2.Http())
-    service_obj = discovery.build(DEFAULT_BUILDER, 'v1', http=http_auth)
-    return service_obj
+    return discovery.build(DEFAULT_BUILDER, 'v1', http=http_auth)
+
 
   @classmethod
   def _VerifyBranch(cls, service_obj, branch, build_id, target):
@@ -56,7 +68,7 @@ class BuildAccessor(object):
     """
     builds = service_obj.build().list(
         buildType='submitted', branch=branch, buildId=build_id, target=target,
-        maxResults=0).execute()
+        maxResults=0).execute(num_retries=MAX_ATTEMPTS)
     if not builds:
       raise AndroidBuildFetchError(
           'Failed to locate build with branch %s, build id %s and target %s.' %
@@ -91,12 +103,13 @@ class BuildAccessor(object):
     # Get all artifacts for the given build_id and target.
     artifacts = service_obj.buildartifact().list(
         buildType='submitted', buildId=build_id, target=target,
-        attemptId='latest', maxResults=0).execute()
+        attemptId='latest', maxResults=0).execute(num_retries=MAX_ATTEMPTS)
     return artifacts['artifacts']
 
   @classmethod
+  @retry.retry(Exception, timeout_min=DOWNLOAD_TIMEOUT_MINS)
   def Download(cls, branch, build_id, target, resource_id, dest_file):
-    """Get the list of artifacts for given build id and target.
+    """Download the list of artifacts for given build id and target.
 
     Args:
       branch: branch of the desired build.
@@ -108,6 +121,9 @@ class BuildAccessor(object):
     service_obj = cls._GetServiceObject()
     cls._VerifyBranch(service_obj, branch, build_id, target)
 
+    # Delete partially downloaded file if exists.
+    subprocess.call(['rm', '-rf', dest_file])
+
     # TODO(dshi): Add retry logic here to avoid API flakes.
     download_req = service_obj.buildartifact().get_media(
         buildType='submitted', buildId=build_id, target=target,
@@ -117,9 +133,12 @@ class BuildAccessor(object):
           fh, download_req, chunksize=DEFAULT_CHUNKSIZE)
       done = None
       while not done:
-        _, done = downloader.next_chunk()
+        _, done = downloader.next_chunk(num_retries=MAX_ATTEMPTS)
+
 
   @classmethod
+  @retry.retry(Exception, timeout_min=QUERY_TIMEOUT_MINS,
+               blacklist=[AndroidBuildFetchError])
   def GetLatestBuildID(cls, target, branch):
     """Get the latest build ID for the given target and branch.
 
@@ -134,7 +153,7 @@ class BuildAccessor(object):
     service_obj = cls._GetServiceObject()
     builds = service_obj.build().list(
         buildType='submitted', branch=branch, target=target, successful=True,
-        maxResults=1).execute()
+        maxResults=1).execute(num_retries=MAX_ATTEMPTS)
     if not builds or not builds['builds']:
       raise AndroidBuildFetchError(
           'Failed to locate build with branch %s and target %s.' %
