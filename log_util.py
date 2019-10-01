66ce09d5c5b022ff31c750d468d09920decfbaf6
Achuith Bhandarkar <achuith@chromium.org>
2019-10-01 16:25:43 +0200

    log_util: Fix lint errors.
    BUG=chromium:403086
    TEST=cros flash
    
    Change-Id: Ie92c1de37bf5090faef0ddbe8a545a2572636da6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/1833684
    Tested-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Amin Hassani <ahassani@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>

--- log_util.py
+++ log_util.py
@@ -1,12 +1,15 @@
+# -*- coding: utf-8 -*-
 # Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 """Logging via CherryPy."""
 
+from __future__ import print_function
+
 import re
 
-import cherrypy
+import cherrypy  # pylint: disable=import-error
 
 
 class Loggable(object):
