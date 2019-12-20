1a301e4cc291a0b924b14b09911f1ca51de8eada
Robert Tarasov <tutankhamen@chromium.org>
2019-12-19 17:00:03 -0800

    contrib/gfx: Add steam_game_info.py script.
    This script helps to retreive the required information about any steam
    game.
    
    BUG=None
    TEST=None
    
    Change-Id: I7f82de7d1e6e325c941af4e2cf985c4359330830
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/1977900
    Tested-by: Robert Tarasov <tutankhamen@chromium.org>
    Commit-Queue: Robert Tarasov <tutankhamen@chromium.org>
    Reviewed-by: David Riley <davidriley@chromium.org>

---
+++ contrib/gfx/steam_game_info.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+from __future__ import print_function
+import http.client
+import re
+import argparse
+import json
+import sys
+
+# This script retreives the specified game information from the Steam DB
+
+parser = argparse.ArgumentParser(description=
+  'Retreives the specified game information from the Steam DB.')
+parser.add_argument('gameid', help='Steam application/game id')
+args = parser.parse_args()
+
+conn = http.client.HTTPSConnection('steamdb.info')
+try:
+  conn.request('GET', '/app/%s/' % args.gameid)
+  resp = conn.getresponse()
+  if resp.status != 200:
+    raise Exception('Response status isn\'t 200')
+except Exception as err:
+  print('Unable to retrieve the data for the game with id %s: %s' % (args.gameid, str(err)),
+        file=sys.stderr)
+  conn.close()
+  exit(-1)
+
+data = resp.read().decode('utf-8')
+
+conn.close()
+
+data_results = {}
+data_results['gameid'] = args.gameid
+try:
+  data_results['game_name'] = re.findall(r'\<td itemprop\=\"name\"\>(.*)\<\/td\>',
+                                         data, re.MULTILINE)[0]
+  data_results['platforms'] = re.findall(r'operatingSystem\" content\="(.*)\"\>',
+                                         data, re.MULTILINE)[0]
+except Exception:
+  print('Unable to parse steamdb.info response')
+  exit(-1)
+
+print(json.dumps(data_results, indent=2, sort_keys=True))
