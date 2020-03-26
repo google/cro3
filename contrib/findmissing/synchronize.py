b34fd152fc381606e026a44a44697e8fb659ece8
Hirthanan Subenderan <hirthanan@google.com>
2020-03-25 20:23:17 -0700

    findmissing: Run subprocess commands synchronously
    subprocess Popen executes commands in a seperate process. This was
    followed by a p.wait since we don't want to run anything in parallel.
    Replaced these calls with run which executes synchronously.
    
    BUG=None
    TEST=None
    
    Change-Id: I18fb8362445ee49b64430d10a1edc142aacfbdb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/2121363
    Reviewed-by: Curtis Malainey <cujomalainey@chromium.org>
    Commit-Queue: Hirthanan Subenderan <hirthanan@google.com>
    Tested-by: Hirthanan Subenderan <hirthanan@google.com>

--- contrib/findmissing/synchronize.py
+++ contrib/findmissing/synchronize.py
@@ -5,10 +5,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Setup module containing script to Synchronize kernel repositories + database.
-
-TODO(hirthanan): abstract out subprocess calls to function to take cmd + block
-"""
+"""Setup module containing script to Synchronize kernel repositories + database."""
 
 from __future__ import print_function
 import os
@@ -27,17 +24,16 @@ def synchronize_upstream(upstream_kernel_metadata):
 
     if not os.path.exists(destdir):
         print('Cloning %s into %s' % (repo, destdir))
-        cmd = ('git clone %s %s' % (repo, destdir)).split(' ')
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-        p.wait()
-
+        clone = ['git', 'clone', '-q', repo, destdir]
+        subprocess.run(clone)
     else:
         os.chdir(destdir)
 
-        print('Updating %s into %s' % (repo, destdir))
-        cmd = 'git checkout master; git pull'.split(' ')
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-        p.wait()
+        print('Pulling latest changes in %s into %s' % (repo, destdir))
+        checkout_master = ['git', 'checkout', '-q', 'master']
+        pull = ['git', 'pull', '-q']
+        subprocess.run(checkout_master)
+        subprocess.run(pull)
 
     os.chdir(common.WORKDIR)
 
@@ -52,54 +48,38 @@ def synchronize_custom(custom_kernel_metadata):
 
     if not os.path.exists(destdir):
         print('Cloning %s into %s' % (repo, destdir))
-        cmd = ('git clone %s %s' % (repo, destdir)).split(' ')
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-        p.wait()
+        clone = ['git', 'clone', '-q', repo, destdir]
+        subprocess.run(clone)
 
         os.chdir(destdir)
         for branch in custom_kernel_metadata.branches:
             branch_name = get_branch_name(branch)
-            cmd = ('git checkout -b %s origin/%s' % (branch_name, branch_name)).split(' ')
-            p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-            p.wait()
 
-        cmd = 'git remote add upstream %s; git fetch upstream' % (upstream_destdir)
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-        p.wait()
+            print('Creating local branch %s in destdir %s' % (branch_name, destdir))
+            checkout_branch = ['git', 'checkout', '-q', branch_name]
+            subprocess.run(checkout_branch)
 
+        print('Add remote upstream %s to destdir %s' % (upstream_destdir, destdir))
+        add_upstream_remote = ['git', 'remote', 'add', 'upstream', upstream_destdir]
+        fetch_upstream = ['git', 'fetch', '-q', 'upstream']
+        subprocess.run(add_upstream_remote)
+        subprocess.run(fetch_upstream)
     else:
         os.chdir(destdir)
 
         print('Updating %s into %s' % (repo, destdir))
-        cmd = 'git reset --hard HEAD; git fetch origin'
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-        p.wait()
+        hard_reset = ['git', 'reset', '-q', '--hard', 'HEAD']
+        fetch_origin = ['git', 'fetch', '-q', 'origin']
+        subprocess.run(hard_reset)
+        subprocess.run(fetch_origin)
 
         for branch in custom_kernel_metadata.branches:
             branch_name = get_branch_name(branch)
-            cmd = ('git rev-parse --verify %s' % (branch_name)).split(' ')
-            p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-            p.wait()
-
-            output, _ = p.communicate()
-            if output:
-                cmd = ('git checkout %s' % (branch_name)).split(' ')
-                p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-                p.wait()
-
-                cmd = 'git pull'.split(' ')
-                p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-                p.wait()
-
-                output, _ = p.communicate()
-                if not output:
-                    cmd = ('git reset --hard origin/%s' % (branch_name)).split(' ')
-                    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-                    p.wait()
-            else:
-                cmd = ('git checkout -b %s origin/%s' % (branch_name, branch_name)).split(' ')
-                p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-                p.wait()
+            print('Updating local branch %s in destdir %s' % (branch_name, destdir))
+            checkout_branch = ['git', 'checkout', '-q', branch_name]
+            pull = ['git', 'pull', '-q']
+            subprocess.run(checkout_branch)
+            subprocess.run(pull)
 
     os.chdir(common.WORKDIR)
 
