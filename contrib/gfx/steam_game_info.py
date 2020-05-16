8fe4cbf32f86a85f63adbd8ac36cb907e3cfde33
Robert Tarasov <tutankhamen@google.com>
2020-05-15 23:16:50 -0700

    contrib/gfx: Fixed problem with retrieving steamdb data
    Fixed problem with request header with response code 403 and potential
    ssl handshake with response code 503 while trying to retrieve game
    information from steamdb.info
    
    BUG=b:156779807
    TEST=./steam_hame_info.py 440
    
    Change-Id: I1bd2c28ce89ff88f12809f0a68d625869d59dcb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/2205167
    Reviewed-by: Ilja H. Friedel <ihf@chromium.org>
    Commit-Queue: Robert Tarasov <tutankhamen@chromium.org>
    Tested-by: Robert Tarasov <tutankhamen@chromium.org>

--- contrib/gfx/steam_game_info.py
+++ contrib/gfx/steam_game_info.py
@@ -5,6 +5,7 @@
 from __future__ import print_function
 import html
 import http.client
+import ssl
 import re
 import argparse
 import json
@@ -16,13 +17,18 @@ parser = argparse.ArgumentParser(description=
   'Retreives the specified game information from the Steam DB.')
 parser.add_argument('gameid', help='Steam application/game id')
 args = parser.parse_args()
-
-conn = http.client.HTTPSConnection('steamdb.info')
+ssl_context = ssl.create_default_context()
+conn = http.client.HTTPSConnection('steamdb.info', context=ssl_context)
 try:
-  conn.request('GET', '/app/%s/' % args.gameid)
+  headers = {
+      'User-Agent':
+          'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
+          'Chrome/81.0.2228.0 Safari/537.36',
+  }
+  conn.request('GET', '/app/%s/' % args.gameid, None, headers)
   resp = conn.getresponse()
   if resp.status != 200:
-    raise Exception('Response status isn\'t 200')
+    raise Exception(f'Response status is {resp.status} (expected 200)')
 except Exception as err:
   print('Unable to retrieve the data for the game with id %s: %s' % (args.gameid, str(err)),
         file=sys.stderr)
