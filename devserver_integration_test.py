2aa3428e8dc906784e4699bb4a942f060e50c395
Amin Hassani <ahassani@chromium.org>
2020-11-18 01:18:19 +0000

    Revert "autoupdate.py: Deprecate autoupdate.py"
    This reverts commit 3de3e2bad7d6707a0548ead60ff4ab19f731ce7f.
    
    Reason for revert: It turns out moblab still has this use case and there
    are external users use it too. so I guess we have to just suck it up and
    put this back.
    
    Original change's description:
    > autoupdate.py: Deprecate autoupdate.py
    >
    > This mechanism is not used in the lab anymore and has been replaced by
    > gs_cache/nebraska_wrapper.py in crrev.com/c/2258234.
    >
    > BUG=chromium:1078188
    > TEST=./devserver_integration_test.py
    >
    > Change-Id: Ifa007df983aab49c808360df6ba0b62b7031221c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/2486466
    > Tested-by: Amin Hassani <ahassani@chromium.org>
    > Commit-Queue: Amin Hassani <ahassani@chromium.org>
    > Reviewed-by: Jae Hoon Kim <kimjae@chromium.org>
    
    Bug: chromium:1078188, chromium:1149703
    Change-Id: Ic9a1841419de5d4a7f2158e432cdaf3f79b273e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/2546047
    Tested-by: Amin Hassani <ahassani@chromium.org>
    Commit-Queue: Amin Hassani <ahassani@chromium.org>
    Reviewed-by: Amin Hassani <ahassani@chromium.org>

--- devserver_integration_test.py
+++ devserver_integration_test.py
@@ -26,7 +26,11 @@ import unittest
 
 from string import Template
 
+from xml.dom import minidom
+
 import requests
+
+from six.moves import urllib
 
 import psutil  # pylint: disable=import-error
 
@@ -211,6 +215,64 @@ class DevserverTestBase(unittest.TestCase):
     self.pid = None
     self.devserver = None
 
+
+  def VerifyHandleUpdate(self, label, use_test_payload=True,
+                         appid='{DEV-BUILD}'):
+    """Verifies that we can send an update request to the devserver.
+
+    This method verifies (using a fake update_request blob) that the devserver
+    can interpret the payload and give us back the right payload.
+
+    Args:
+      label: Label that update is served from e.g. <board>-release/<version>
+      use_test_payload: If set to true, expects to serve payload under
+        testdata/ and does extra checks i.e. compares hash and content of
+        payload.
+      appid: The APP ID of the board.
+
+    Returns:
+      url of the update payload if we verified the update.
+    """
+    update_label = '/'.join([UPDATE, label])
+    response = self._MakeRPC(
+        update_label, data=UPDATE_REQUEST.substitute({'appid': appid}),
+        critical_update=True)
+    self.assertNotEqual('', response)
+    self.assertIn('deadline="now"', response)
+
+    # Parse the response and check if it contains the right result.
+    dom = minidom.parseString(response)
+    update = dom.getElementsByTagName('updatecheck')[0]
+    expected_static_url = '/'.join([self.devserver_url, STATIC, label])
+    url = self.VerifyV3Response(update, expected_static_url)
+
+    # Verify the image we download is correct since we already know what it is.
+    if use_test_payload:
+      connection = urllib.request.urlopen(url)
+      contents = connection.read().decode('utf-8')
+      connection.close()
+      self.assertEqual('Developers, developers, developers!\n', contents)
+
+    return url
+
+  def VerifyV3Response(self, update, expected_static_url):
+    """Verifies the update DOM from a v3 response and returns the url."""
+    # Parse the response and check if it contains the right result.
+    urls = update.getElementsByTagName('urls')[0]
+    url = urls.getElementsByTagName('url')[0]
+
+    static_url = url.getAttribute('codebase')
+    # Static url's end in /.
+    self.assertEqual(expected_static_url + '/', static_url)
+
+    manifest = update.getElementsByTagName('manifest')[0]
+    packages = manifest.getElementsByTagName('packages')[0]
+    package = packages.getElementsByTagName('package')[0]
+    filename = package.getAttribute('name')
+    self.assertEqual(TEST_UPDATE_PAYLOAD_NAME, filename)
+
+    return os.path.join(static_url, filename)
+
   def _MakeRPC(self, rpc, data=None, timeout=None, **kwargs):
     """Makes an RPC call to the devserver.
 
@@ -276,6 +338,9 @@ class DevserverBasicTests(AutoStartDevserverTestBase):
   they are lumped with the remote tests here.
   """
 
+  def testHandleUpdateV3(self):
+    self.VerifyHandleUpdate(label=LABEL)
+
   def testXBuddyLocalAlias(self):
     """Extensive local image xbuddy unittest.
 
@@ -310,7 +375,7 @@ class DevserverBasicTests(AutoStartDevserverTestBase):
 
       xbuddy_path = '/'.join([build_id, item])
       logging.info('Testing xbuddy path %s', xbuddy_path)
-      response = self._MakeRPC('/'.join([XBUDDY, xbuddy_path]), timeout=3)
+      response = self._MakeRPC('/'.join([XBUDDY, xbuddy_path]))
       self.assertEqual(response, data)
 
       expected_dir = '/'.join([self.devserver_url, STATIC, build_id])
@@ -320,6 +385,14 @@ class DevserverBasicTests(AutoStartDevserverTestBase):
       response = self._MakeRPC('/'.join([XBUDDY, xbuddy_path]),
                                relative_path=True)
       self.assertEqual(response, build_id)
+
+    logging.info('Verifying the actual payload data')
+    url = self.VerifyHandleUpdate(build_id, use_test_payload=False)
+    logging.info('Verify the actual content of the update payload')
+    connection = urllib.request.urlopen(url)
+    contents = connection.read().decode('utf-8')
+    connection.close()
+    self.assertEqual(update_data, contents)
 
   def testPidFile(self):
     """Test that using a pidfile works correctly."""
@@ -338,6 +411,37 @@ class DevserverExtendedTests(AutoStartDevserverTestBase):
   1) runner has access to the Google Storage bucket where builders store builds.
   2) time. These tests actually download the artifacts needed.
   """
+
+  def testStageAndUpdate(self):
+    """Tests core stage/update autotest workflow where with a test payload."""
+    build_id = 'eve-release/R78-12499.0.0'
+    archive_url = 'gs://chromeos-image-archive/%s' % build_id
+
+    response = self._MakeRPC(IS_STAGED, archive_url=archive_url,
+                             artifacts='full_payload,stateful')
+    self.assertEqual(response, 'False')
+
+    logging.info('Staging update artifacts')
+    self._MakeRPC(STAGE, archive_url=archive_url,
+                  artifacts='full_payload,stateful')
+    logging.info('Staging complete. '
+                 'Verifying files exist and are staged in the staging '
+                 'directory.')
+    response = self._MakeRPC(IS_STAGED, archive_url=archive_url,
+                             artifacts='full_payload,stateful')
+    self.assertEqual(response, 'True')
+    staged_dir = os.path.join(self.test_data_path, build_id)
+    self.assertTrue(os.path.isdir(staged_dir))
+    self.assertTrue(os.path.exists(
+        os.path.join(staged_dir, devserver_constants.UPDATE_FILE)))
+    self.assertTrue(os.path.exists(
+        os.path.join(staged_dir, devserver_constants.UPDATE_METADATA_FILE)))
+    self.assertTrue(os.path.exists(
+        os.path.join(staged_dir, devserver_constants.STATEFUL_FILE)))
+
+    logging.info('Verifying we can update using the stage update artifacts.')
+    self.VerifyHandleUpdate(build_id, use_test_payload=False,
+                            appid='{01906EA2-3EB2-41F1-8F62-F0B7120EFD2E}')
 
   @unittest.skip('crbug.com/640063 Broken test.')
   def testStageAutotestAndGetPackages(self):
