5079ac7c7980152d12e25fb6be9d2c376c6994e7
Tom Hughes <tomhughes@chromium.org>
2020-12-04 11:19:53 -0800

    coil_urls.py: Add script for fixing common URLs for COIL
    BUG=b:175056924
    TEST=cd coil_urls && ./coil_urls_test.py
    
    Change-Id: Iaf5f9b92cdea1bdbd53c3529bce6d4d17d6246e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/2577621
    Reviewed-by: Bernie Thompson <bhthompson@chromium.org>
    Reviewed-by: Andrew de los Reyes <adlr@chromium.org>
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Tom Hughes <tomhughes@chromium.org>

---
+++ contrib/coil_urls/coil_urls.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Fixes common URLs for COIL.
+"""
+import argparse
+import logging
+import re
+import shutil
+import subprocess
+import sys
+import tempfile
+
+
+class Pattern:
+    def __init__(self, match_pattern, repl_pattern):
+        self.match_pattern = match_pattern
+        self.repl_pattern = repl_pattern
+
+
+CHROMIUM_REGEX = r'(chromium.googlesource.com[^\s]*\/\+\/)(master)'
+CHROMIUM_REGEX_ALT =\
+    r'(chromium.googlesource.com[^\s]*\/\+\/)(refs\/heads\/master)'
+GITHUB_REGEX = r'(github.com[^\s]*\/)(master)'
+DEFAULT_REPL_PATTERN = r'\1HEAD'
+
+PATTERNS = [
+    Pattern(match_pattern=CHROMIUM_REGEX, repl_pattern=DEFAULT_REPL_PATTERN),
+    Pattern(match_pattern=CHROMIUM_REGEX_ALT,
+            repl_pattern=DEFAULT_REPL_PATTERN),
+    Pattern(match_pattern=GITHUB_REGEX, repl_pattern=DEFAULT_REPL_PATTERN),
+]
+
+
+def fix_line(text: str):
+    # iterate through patterns. if line is changed, return
+    for pattern in PATTERNS:
+        new_str, count = re.subn(pattern.match_pattern, pattern.repl_pattern,
+                                 text)
+        if count > 0:
+            return new_str
+
+    return text
+
+
+def fix_file(file: str):
+    try:
+        with tempfile.NamedTemporaryFile() as tmp_file:
+            logging.debug('created temp file: %s', tmp_file.name)
+
+            # replace line-by-line
+            with open(file) as orig_file:
+                for line in orig_file:
+                    newline = fix_line(line)
+
+                    # write newline to temp file
+                    tmp_file.write(newline.encode())
+
+            # replace orig with temp file
+            tmp_file.flush()
+            shutil.copyfile(tmp_file.name, file)
+    except UnicodeDecodeError:
+        # skip any files that aren't utf8
+        pass
+
+
+def is_git_dir():
+    cmd = [
+        'git', 'rev-parse', '--git-dir'
+    ]
+    logging.debug('Running command: "%s"', ' '.join(cmd))
+    ret = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return ret.returncode == 0
+
+
+def find_files():
+    git_cmd = ['git', 'grep', '--name-only']
+    grep_cmd = ['grep', '-r', '--files-with-matches']
+
+    if is_git_dir():
+        cmd = git_cmd
+    else:
+        cmd = grep_cmd
+
+    cmd.append('master')
+    logging.debug('Running command: "%s"', ' '.join(cmd))
+    ret = subprocess.run(cmd, stdout=subprocess.PIPE, check=True)
+    return ret.stdout.decode().splitlines()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    log_level_choices = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+    parser.add_argument(
+        '--log_level', '-l',
+        choices=log_level_choices,
+        default='INFO'
+    )
+
+    args = parser.parse_args()
+    logging.basicConfig(level=args.log_level)
+
+    for file_name in find_files():
+        fix_file(file_name)
+
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ contrib/coil_urls/coil_urls_test.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+import os
+import stat
+import tempfile
+import unittest
+
+from coil_urls import fix_line
+from coil_urls import find_files
+from coil_urls import fix_file
+
+
+class TestRegex(unittest.TestCase):
+
+    def test_url_only(self):
+        self.assertEqual(fix_line(
+            'https://chromium.googlesource.com/chromiumos/docs/+/master/developer_guide.md'),
+            'https://chromium.googlesource.com/chromiumos/docs/+/HEAD/developer_guide.md')
+
+    def test_url_with_ref(self):
+        self.assertEqual(fix_line(
+            'https://chromium.googlesource.com/chromiumos/docs/+/refs/heads/master/developer_guide.md'),
+            'https://chromium.googlesource.com/chromiumos/docs/+/HEAD/developer_guide.md')
+
+    def test_text_and_url(self):
+        self.assertEqual(fix_line(
+            'Some text https://chromium.googlesource.com/chromiumos/docs/+/master/developer_guide.md after'),
+            'Some text https://chromium.googlesource.com/chromiumos/docs/+/HEAD/developer_guide.md after')
+
+    def test_multiple_urls(self):
+        self.assertEqual(fix_line(
+            'https://chromium.googlesource.com/chromiumos/docs/+/master/developer_guide.md'
+            ' and https://chromium.googlesource.com/chromiumos/docs/+/master/developer_guide.md'),
+            'https://chromium.googlesource.com/chromiumos/docs/+/HEAD/developer_guide.md'
+            ' and https://chromium.googlesource.com/chromiumos/docs/+/HEAD/developer_guide.md')
+
+    def test_no_url(self):
+        self.assertEqual(fix_line(
+            'Some text without a URL.'),
+            'Some text without a URL.')
+
+    def test_github_url(self):
+        self.assertEqual(fix_line(
+            'https://github.com/google/googletest/blob/master/googlemock/docs/cook_book.md#delegating-calls-to-a-parent-class'),
+            'https://github.com/google/googletest/blob/HEAD/googlemock/docs/cook_book.md#delegating-calls-to-a-parent-class')
+
+
+class TestFindFiles(unittest.TestCase):
+    def test_find_files(self):
+        self.assertEqual(find_files(), ['coil_urls.py', 'coil_urls_test.py'])
+
+
+class TestFixFile(unittest.TestCase):
+    TEST_FILE_TEXT = """
+Some text without a URL.
+https://chromium.googlesource.com/chromiumos/docs/+/master/developer_guide.md
+Some more text.
+    """
+
+    TEST_FILE_TEXT_EXPECTED = """
+Some text without a URL.
+https://chromium.googlesource.com/chromiumos/docs/+/HEAD/developer_guide.md
+Some more text.
+    """
+
+    TEST_FILE_NOT_UTF8 = b'\x80abc'
+
+    def test_fix_file(self):
+        tmp_file = tempfile.NamedTemporaryFile()
+
+        with open(tmp_file.name, 'w') as test_file:
+            test_file.write(self.TEST_FILE_TEXT)
+
+        fix_file(tmp_file.name)
+
+        with open(tmp_file.name) as test_file:
+            text = test_file.read()
+            self.assertEqual(text, self.TEST_FILE_TEXT_EXPECTED)
+
+        tmp_file.close()
+
+    def test_file_not_utf8(self):
+        tmp_file = tempfile.NamedTemporaryFile()
+
+        with open(tmp_file.name, 'wb') as test_file:
+            test_file.write(self.TEST_FILE_NOT_UTF8)
+
+        fix_file(tmp_file.name)
+
+        with open(tmp_file.name, 'rb') as test_file:
+            text = test_file.read()
+            self.assertEqual(text, self.TEST_FILE_NOT_UTF8)
+
+        tmp_file.close()
+
+    def test_metadata_not_changed(self):
+        tmp_file = tempfile.NamedTemporaryFile()
+
+        with open(tmp_file.name, 'w') as test_file:
+            test_file.write(self.TEST_FILE_TEXT)
+        orig_mode = stat.S_IFREG |\
+            stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | \
+            stat.S_IRGRP | stat.S_IXGRP | \
+            stat.S_IROTH | stat.S_IXOTH
+        os.chmod(tmp_file.name, orig_mode)
+
+        fix_file(tmp_file.name)
+
+        # metadata should be preserved
+        st = os.stat(tmp_file.name)
+        self.assertEqual(orig_mode, st.st_mode)
+
+        tmp_file.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
