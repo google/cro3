edfba49d7c6515a9c22aa9654b36a580adca6256
Amin Hassani <ahassani@chromium.org>
2021-04-20 10:19:49 -0700

    nebraska: Move it to python3
    BUG=chromium:998225
    TEST=nebraska_unittest.py
    
    Change-Id: I6c6183d38265e6ab66811e95e2abe3a3e641effb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/2837692
    Tested-by: Amin Hassani <ahassani@chromium.org>
    Auto-Submit: Amin Hassani <ahassani@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Amin Hassani <ahassani@chromium.org>

--- nebraska/nebraska.py
+++ nebraska/nebraska.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Copyright 2018 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
