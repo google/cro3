7cfc810c92283afff7942c05a75b3f91ecea8357
C Shapiro <shapiroc@chromium.org>
2021-05-17 13:31:27 -0500

    (2 of 2) Splitting out Dut/Test services
    See Cq-Depend CL for rationale.
    
    BUG=None
    TEST=FEATURES=test sudo emerge test-server
    
    Cq-Depend: chromium:2900820
    Change-Id: I90b299c171490549211042117f8d6de21514b687
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/2899780
    Tested-by: C Shapiro <shapiroc@chromium.org>
    Commit-Queue: C Shapiro <shapiroc@chromium.org>
    Reviewed-by: Jae Hoon Kim <kimjae@chromium.org>

--- test/src/chromiumos/testservice/cmd/testservice/testserviceserver.go
+++ test/src/chromiumos/testservice/cmd/testservice/testserviceserver.go
@@ -37,54 +37,6 @@ func newTestServiceServer(l net.Listener, logger *log.Logger) (*grpc.Server, err
 	return server, nil
 }
 
-// ProvisionDut installs a specified version of Chrome OS on the DUT, along
-// with any specified DLCs.
-//
-// If the DUT is already on the specified version of Chrome OS, the OS will
-// not be provisioned.
-//
-// If the DUT already has the specified list of DLCs, only the missing DLCs
-// will be provisioned.
-func (s *TestServiceServer) ProvisionDut(ctx context.Context, req *api.ProvisionDutRequest) (*longrunning.Operation, error) {
-	s.logger.Println("Received api.ProvisionDutRequest: ", *req)
-	op := s.Manager.NewOperation()
-	s.Manager.SetResult(op.Name, &api.ProvisionDutResponse{})
-	return op, nil
-}
-
-// ProvisionLacros installs a specified version of Lacros on the DUT.
-//
-// If the DUT already has the specified version of Lacros, Lacros will not be
-// provisioned.
-func (s *TestServiceServer) ProvisionLacros(ctx context.Context, req *api.ProvisionLacrosRequest) (*longrunning.Operation, error) {
-	s.logger.Println("Received api.ProvisionLacrosRequest: ", *req)
-	op := s.Manager.NewOperation()
-	s.Manager.SetResult(op.Name, &api.ProvisionLacrosResponse{})
-	return op, nil
-}
-
-// ProvisionAsh installs a specified version of ash-chrome on the DUT.
-//
-// This directly overwrites the version of ash-chrome on the current root
-// disk partition.
-func (s *TestServiceServer) ProvisionAsh(ctx context.Context, req *api.ProvisionAshRequest) (*longrunning.Operation, error) {
-	s.logger.Println("Received api.ProvisionAshRequest: ", *req)
-	op := s.Manager.NewOperation()
-	s.Manager.SetResult(op.Name, &api.ProvisionAshResponse{})
-	return op, nil
-}
-
-// ProvisionArc installs a specified version of ARC on the DUT.
-//
-// This directly overwrites the version of ARC on the current root
-// disk partition.
-func (s *TestServiceServer) ProvisionArc(ctx context.Context, req *api.ProvisionArcRequest) (*longrunning.Operation, error) {
-	s.logger.Println("Received api.ProvisionArcRequest: ", *req)
-	op := s.Manager.NewOperation()
-	s.Manager.SetResult(op.Name, &api.ProvisionArcResponse{})
-	return op, nil
-}
-
 // RunTests runs the requested tests.
 func (s *TestServiceServer) RunTests(ctx context.Context, req *api.RunTestsRequest) (*longrunning.Operation, error) {
 	s.logger.Println("Received api.RunTestsRequest: ", *req)
