c664f82336677e0fc5308d8a6e1b570edb564147
Andrew Lamb <andrewlamb@chromium.org>
2021-06-08 10:50:09 -0600

    dev-util: Add script to run testplan.go on example SourceTestPlan.
    - Just for demonstration purposes / seeing changes that are hard
    to unit test, e.g. logging.
    
    BUG=b:189223005
    TEST=CQ
    
    Change-Id: I8f6ae3465488b45a76e00247de6e378b6b6f0121
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/2946773
    Tested-by: Andrew Lamb <andrewlamb@chromium.org>
    Auto-Submit: Andrew Lamb <andrewlamb@chromium.org>
    Reviewed-by: Jaques Clapauch <jaquesc@google.com>
    Reviewed-by: C Shapiro <shapiroc@chromium.org>
    Commit-Queue: C Shapiro <shapiroc@chromium.org>

---
+++ src/chromiumos/test/plan/cmd/run_example.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+#
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Runs testplan.go on an example SourceTestPlan, with config from
+# config-internal.
+
+set -e
+
+usage="Run testplan.go on an example SourceTestPlan.
+
+This script is just for seeing an example run of testplan.go an experimenting
+with changes difficult to unit test (e.g. logging). It directly calls 'go run'
+outside of Portage, so will fail if dependencies are not installed.
+
+This script uses the BuildSummaryList and DutAttributesList checked into
+config-internal.
+
+Flags are passed through to testplan.go's generate command,
+e.g. '-alsologtostderr'.
+
+Usage: ${0} [options] <testplan.go generate flags>
+
+Options:
+  -h, --help       This help output."
+
+while [[ $# -ne 0 ]]; do
+  case $1 in
+    -h|--help)
+    exec printf '%b\n' "${usage}"
+    ;;
+  *)
+    ARGS+=( "$1" )
+    ;;
+  esac
+  shift
+done
+set -- "${ARGS[@]}" "$@"
+
+script_dir="$(dirname "$(realpath -e "${BASH_SOURCE[0]}")")"
+
+cd "${script_dir}"
+
+config_internal_dir="$(realpath -e ../../../../../../../config-internal)"
+
+dut_attributes="${config_internal_dir}/dut_attributes/generated/dut_attributes.jsonproto"
+build_summary="${config_internal_dir}/build/generated/build_summary.jsonproto"
+
+if [[ ! -f ${dut_attributes} ]]; then
+    echo "Expected to find DutAttributesList at ${dut_attributes}"
+    exit 1
+else
+    echo "Using DutAttributeList at ${dut_attributes}"
+fi
+
+if [[ ! -f ${build_summary} ]]; then
+    echo "Expected to find BuildSummaryList at ${build_summary}"
+    exit 1
+else
+    echo "Using BuildSummaryList at ${build_summary}"
+fi
+
+out=$(mktemp -d)/coverage_rules.jsonproto
+
+echo "Running testplan.go, writing CoverageRules to ${out}"
+
+set -x
+
+go run testplan.go generate \
+  -plan example_source_test_plan.textpb \
+  -dutattributes "${dut_attributes}" \
+  -buildsummary "${build_summary}" \
+  -out "${out}" \
+  "$@"
