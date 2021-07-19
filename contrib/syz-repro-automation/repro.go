b113fd51610ef638bb12623b63654446144c5a5e
Grant Hugh <ghugh@google.com>
2021-07-19 23:09:47 +0000

    syz-repro-automation: flash kernel onto DUT
    Added a function to flash the kernel onto a leased DUT using the
    hostname. Also added a helper function to get the boardname from the
    hostname of the leased DUT
    
    BUG=b:193906330
    TEST=go run repro.go
    
    Change-Id: Id4c4a0b86a1baac6476bd6bf363ca2bd6fc42231
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3040144
    Commit-Queue: Grant Hugh <ghugh@chromium.org>
    Tested-by: Grant Hugh <ghugh@chromium.org>
    Reviewed-by: Zubin Mithra <zsm@chromium.org>

--- contrib/syz-repro-automation/repro.go
+++ contrib/syz-repro-automation/repro.go
@@ -14,6 +14,49 @@ import (
 	"strings"
 )
 
+func flashKernel(hostname string, imageId string) error {
+	board, err := getDutBoard(hostname)
+	if err != nil {
+		return err
+	}
+	cmd := exec.Command("cros", "flash", "--board="+board, "ssh://root@"+hostname+".cros",
+		"xBuddy://remote/"+board+"-debug-kernel-postsubmit/"+imageId)
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+
+	log.Printf("Flashing kernel onto DUT...")
+	err = cmd.Run()
+	if err != nil {
+		return fmt.Errorf("error flashing kernel onto DUT: %v: %v", err, stderr.String())
+	}
+	log.Printf("Finished flashing kernel onto DUT")
+
+	return nil
+}
+
+func getDutBoard(hostname string) (string, error) {
+	cmd := exec.Command("crosfleet", "dut", "info", hostname)
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	if err != nil {
+		return "", fmt.Errorf("error getting dut info: %v: %v", err, stderr.String())
+	}
+
+	/* out.String() looks like:
+	DUT_HOSTNAME=chromeos6-row18-rack16-host4.cros
+	MODEL=garg
+	BOARD=octopus
+	SERVO_HOSTNAME=chromeos6-row17-rack16-labstation1
+	SERVO_PORT=9985
+	SERVO_SERIAL=G1911051544 */
+	lines := strings.Split(out.String(), "\n")
+	return strings.Split(lines[2], "=")[1], nil
+}
+
 func leaseDut(model string, minutes int) (string, error) {
 	cmd := exec.Command("crosfleet", "dut", "lease", "-model", model, "--minutes", strconv.Itoa(minutes))
 	var out bytes.Buffer
@@ -38,6 +81,7 @@ func leaseDut(model string, minutes int) (string, error) {
 func main() {
 	model := flag.String("model", "garg", "Model for leased DUT")
 	minutes := flag.Int("minutes", 60, "Number of minutes to lease DUT")
+	imageId := flag.String("imageid", "R93-14027.0.0-49783-8844471042827170672", "Kernel image id to flash onto DUT")
 
 	flag.Parse()
 
@@ -46,5 +90,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Println(hostname)
+	err = flashKernel(hostname, *imageId)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
