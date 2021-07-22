9b4310d8efe7e5269df92cf68e48adb963d43cb7
Grant Hugh <ghugh@google.com>
2021-07-22 20:47:38 +0000

    syz-repro-automation: add runSyzRepro
    Added runSyzRepro function which runs syz-repro on the log file, which
    is given as the sole argument to this tool. Output from syz-repro is
    directed to a file in the syzkaller directory.
    
    Also added functionality to print command output to console in runCmd.
    
    BUG=b:193906330
    TEST=go run repro.go
    
    Change-Id: Ib30368cf1dffc3dfe4772329554d198be296176a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3046718
    Reviewed-by: Zubin Mithra <zsm@chromium.org>
    Commit-Queue: Grant Hugh <ghugh@chromium.org>
    Tested-by: Grant Hugh <ghugh@chromium.org>

--- contrib/syz-repro-automation/repro.go
+++ contrib/syz-repro-automation/repro.go
@@ -6,21 +6,59 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"strconv"
 	"strings"
+	"time"
 )
 
-func runCmd(cmdStr ...string) (string, error) {
+type dutConfig struct {
+	Targets       []string `json:"targets"`
+	TargetDir     string   `json:"target_dir"`
+	TargetReboot  bool     `json:"target_reboot"`
+	StartupScript string   `json:"startup_script"`
+	Pstore        bool     `json:"pstore"`
+}
+
+type syzreproConfig struct {
+	Name      string    `json:"name"`
+	Target    string    `json:"target"`
+	Reproduce bool      `json:"reproduce"`
+	HTTP      string    `json:"http"`
+	Workdir   string    `json:"workdir"`
+	Syzkaller string    `json:"syzkaller"`
+	Type      string    `json:"type"`
+	SSHKey    string    `json:"sshkey"`
+	Procs     int       `json:"procs"`
+	DUTConfig dutConfig `json:"vm"`
+}
+
+const (
+	syzReproTimeout = 20 * time.Minute
+)
+
+func runCmd(print bool, cmdStr ...string) (string, error) {
 	cmd := exec.Command(cmdStr[0], cmdStr[1:]...)
 	var out bytes.Buffer
 	var stderr bytes.Buffer
+
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
-
+	if print {
+		stdoutW := io.MultiWriter(&out, os.Stdout)
+		stderrW := io.MultiWriter(&stderr, os.Stdout)
+		cmd.Stdout = stdoutW
+		cmd.Stderr = stderrW
+	}
 	if err := cmd.Run(); err != nil {
 		return "", fmt.Errorf("%v: %v", err, stderr.String())
 	}
@@ -28,9 +66,83 @@ func runCmd(cmdStr ...string) (string, error) {
 	return out.String(), nil
 }
 
+func runCmdLog(outputLog string, timeout time.Duration, cmdStr ...string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, cmdStr[0], cmdStr[1:]...)
+
+	outputFile, err := os.Create(outputLog)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+
+	writer := io.Writer(outputFile)
+	cmd.Stdout = writer
+	cmd.Stderr = writer
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("cmd error: %v, ctx err: %v", err, ctx.Err())
+	}
+
+	return nil
+}
+
+func runSyzRepro(paths map[string]string, hostname string, reproLog string) error {
+	syzreproTempDir, err := ioutil.TempDir("", "syzrepro-temp")
+	if err != nil {
+		return fmt.Errorf("unable to create tempdir: %v", err)
+	}
+
+	workdir := filepath.Join(syzreproTempDir, "workdir")
+	if err = os.Mkdir(workdir, 0775); err != nil {
+		return fmt.Errorf("unable to create workdir: %v", err)
+	}
+	defer os.RemoveAll(syzreproTempDir)
+
+	config := syzreproConfig{
+		Name:      "syzrepro-automation",
+		Target:    "linux/amd64",
+		Reproduce: true,
+		HTTP:      "localhost:56700",
+		Workdir:   workdir,
+		Syzkaller: paths["syzkaller"],
+		Type:      "isolated",
+		SSHKey:    paths["sshKey"],
+		Procs:     1,
+		DUTConfig: dutConfig{
+			Targets:       []string{hostname + ".cros"},
+			TargetDir:     "/tmp",
+			TargetReboot:  false,
+			StartupScript: paths["startupScript"],
+			Pstore:        true,
+		},
+	}
+
+	configPath := filepath.Join(syzreproTempDir, "config.cfg")
+	configFile, err := os.Create(configPath)
+	if err != nil {
+		return fmt.Errorf("unable to create syzkaller configfile: %v", err)
+	}
+	defer configFile.Close()
+
+	if err := json.NewEncoder(configFile).Encode(config); err != nil {
+		return fmt.Errorf("invalid syzkaller configuration: %v", err)
+	}
+
+	outputLog := filepath.Join(paths["syzkaller"], "outputLog-"+time.Now().Format("2006-01-02-15:04:05"))
+	log.Printf("Running syz-repro, output directed to %v\n", outputLog)
+	if err = runCmdLog(outputLog, syzReproTimeout, paths["syzrepro"], "-config="+configPath, "-vv", "10", reproLog); err != nil {
+		return fmt.Errorf("error running syz-repro: %v", err)
+	}
+
+	return nil
+}
+
 func abandonDut(hostname string) {
 	log.Println("Abandoning DUT at " + hostname + "...")
-	ret, err := runCmd("crosfleet", "dut", "abandon", hostname)
+	ret, err := runCmd(false, "crosfleet", "dut", "abandon", hostname)
 	if err != nil {
 		log.Fatal(fmt.Errorf("error abandoning DUT: %v", err))
 	}
@@ -39,7 +151,7 @@ func abandonDut(hostname string) {
 
 func getLatestImage(board string) (string, error) {
 	bucket := "gs://chromeos-image-archive/" + board + "-debug-kernel-postsubmit"
-	ret, err := runCmd("gsutil.py", "ls", bucket)
+	ret, err := runCmd(false, "gsutil.py", "ls", bucket)
 	if err != nil {
 		return "", err
 	}
@@ -56,22 +168,22 @@ func getLatestImage(board string) (string, error) {
 	return latestImage, nil
 }
 
-func flashKernel(hostname string, imageId string) error {
+func flashKernel(hostname string, imageID string) error {
 	board, err := getDutBoard(hostname)
 	if err != nil {
 		return err
 	}
-	if imageId == "" {
+	if imageID == "" {
 		log.Printf("Image id not provided, fetching latest image for board %v...\n", board)
-		imageId, err = getLatestImage(board)
+		imageID, err = getLatestImage(board)
 		if err != nil {
 			return err
 		}
 	}
 	log.Printf("Flashing kernel onto DUT...")
 	ssh := "ssh://root@" + hostname + ".cros"
-	xBuddy := "xBuddy://remote/" + board + "-debug-kernel-postsubmit/" + imageId
-	if _, err = runCmd("cros", "flash", "--board="+board, ssh, xBuddy); err != nil {
+	xBuddy := "xBuddy://remote/" + board + "-debug-kernel-postsubmit/" + imageID
+	if _, err = runCmd(true, "cros", "flash", "--board="+board, ssh, xBuddy); err != nil {
 		return fmt.Errorf("error flashing kernel onto DUT: %v", err)
 	}
 	log.Printf("Finished flashing kernel onto DUT")
@@ -80,7 +192,7 @@ func flashKernel(hostname string, imageId string) error {
 }
 
 func getDutBoard(hostname string) (string, error) {
-	ret, err := runCmd("crosfleet", "dut", "info", hostname)
+	ret, err := runCmd(false, "crosfleet", "dut", "info", hostname)
 	if err != nil {
 		return "", fmt.Errorf("error getting dut info: %v", err)
 	}
@@ -98,7 +210,7 @@ func getDutBoard(hostname string) (string, error) {
 
 func leaseDut(model string, minutes int) (string, error) {
 	log.Printf("Leasing model %v device for %v minutes...\n", model, minutes)
-	ret, err := runCmd("crosfleet", "dut", "lease", "-model", model, "--minutes", strconv.Itoa(minutes))
+	ret, err := runCmd(true, "crosfleet", "dut", "lease", "-model", model, "--minutes", strconv.Itoa(minutes))
 	if err != nil {
 		return "", fmt.Errorf("error leasing device: %v", err)
 	}
@@ -111,12 +223,44 @@ func leaseDut(model string, minutes int) (string, error) {
 	return strings.Split(ret, " ")[1], nil
 }
 
+func checkPaths(paths []string) error {
+	for _, path := range paths {
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			return fmt.Errorf("path for %v is invalid: %v", err)
+		}
+	}
+	return nil
+}
+
 func main() {
 	model := flag.String("model", "garg", "Model for leased DUT")
 	minutes := flag.Int("minutes", 60, "Number of minutes to lease DUT")
-	imageId := flag.String("imageid", "", "Kernel image id to flash onto DUT")
+	imageID := flag.String("imageid", "", "Kernel image id to flash onto DUT")
 
 	flag.Parse()
+
+	if flag.Arg(0) == "" {
+		log.Fatal("must provide a log file to run syz-repro on")
+	}
+
+	syzkallerDir := os.Getenv("SYZKALLER")
+	if syzkallerDir == "" {
+		log.Fatal("environment variable SYZKALLER is not set")
+	}
+	syzrepro := filepath.Join(syzkallerDir, "bin", "syz-repro")
+	sshKey := filepath.Join(syzkallerDir, "testing_rsa")
+	startupScript := filepath.Join(syzkallerDir, "startup_script.sh")
+
+	if err := checkPaths([]string{syzkallerDir, syzrepro, sshKey, startupScript}); err != nil {
+		log.Fatal(err)
+	}
+
+	paths := map[string]string{
+		"syzkaller":     syzkallerDir,
+		"syzrepro":      syzrepro,
+		"sshKey":        sshKey,
+		"startupScript": startupScript,
+	}
 
 	hostname, err := leaseDut(*model, *minutes)
 	if err != nil {
@@ -124,8 +268,11 @@ func main() {
 	}
 	defer abandonDut(hostname)
 
-	err = flashKernel(hostname, *imageId)
-	if err != nil {
+	if err = flashKernel(hostname, *imageID); err != nil {
+		panic(err)
+	}
+
+	if err = runSyzRepro(paths, hostname, flag.Arg(0)); err != nil {
 		panic(err)
 	}
 }
