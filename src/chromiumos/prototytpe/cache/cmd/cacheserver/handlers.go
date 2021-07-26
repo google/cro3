c1fd0e153640314fff74c2ee4603465dbd2bcf73
Jaques Clapauch <jaquesc@google.com>
2021-07-26 19:46:30 +0000

    Removing deprecated lib
    BUG=None
    TEST="go run . &; curl --http2
    localhost:8080/v1/cache/gs/?source_url=gs://jql/to_download --output
    /tmp/out"
    
    Change-Id: Ic461090def9e9cdfc40a39ff4909750fa4b64fbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3055150
    Commit-Queue: Jaques Clapauch <jaquesc@google.com>
    Commit-Queue: Allen Li <ayatane@chromium.org>
    Tested-by: Jaques Clapauch <jaquesc@google.com>
    Auto-Submit: Jaques Clapauch <jaquesc@google.com>
    Reviewed-by: Allen Li <ayatane@chromium.org>

--- src/chromiumos/prototytpe/cache/cmd/cacheserver/handlers.go
+++ src/chromiumos/prototytpe/cache/cmd/cacheserver/handlers.go
@@ -11,7 +11,8 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 const sourceURLKey = "source_url"
@@ -31,7 +32,7 @@ func InstantiateHandlers(port int, cacheLocation string) error {
 
 	// Clean up on SIGINT and SIGTERM
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(c, os.Interrupt, unix.SIGTERM)
 	go func() {
 		<-c
 		cache.Close()
