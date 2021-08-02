afba9afb9acad4536b58f223358f96bb6d060b1f
C Shapiro <shapiroc@chromium.org>
2021-08-02 11:46:45 -0500

    Metadata server basic plumbing
    Basic wiring first.
    
    Actual impl will iterate over all the jsonproto configs, find the
    matching config based on the scanned identity values and return the
    corresponding config.
    
    BUG=b:188679614
    TEST=unit
    
    Cq-Depend: chromium:3061100
    Change-Id: I32c6060ca0a11bf35bb1c93dd71e3b4ab1e9a4c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3064579
    Commit-Queue: C Shapiro <shapiroc@chromium.org>
    Tested-by: C Shapiro <shapiroc@chromium.org>
    Auto-Submit: C Shapiro <shapiroc@chromium.org>
    Reviewed-by: Sean McAllister <smcallis@google.com>

---
+++ src/chromiumos/build/cmd/metadataserver/metadataserver.go
@@ -0,0 +1,38 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Implements metadata_service.proto (see proto for details)
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"go.chromium.org/chromiumos/config/go/build/api"
+	"google.golang.org/grpc"
+)
+
+// MetadataServer implementation of metadata_service.proto
+type MetadataServer struct {
+	logger *log.Logger
+}
+
+// newMetadataServer creates a new metadata service server to listen to rpc requests.
+func newMetadataServer(l net.Listener, logger *log.Logger) (*grpc.Server, error) {
+	s := &MetadataServer{
+		logger: logger,
+	}
+	server := grpc.NewServer()
+	api.RegisterMetadataServiceServer(server, s)
+	logger.Println("metadataservice listening to requests at ", l.Addr().String())
+	return server, nil
+}
+
+func (s *MetadataServer) GetDeviceConfig(ctx context.Context,
+	req *api.GetDeviceConfigRequest) (*api.GetDeviceConfigResponse, error) {
+	s.logger.Println("Received api.GetDeviceConfig: ", *req)
+	s.logger.Println("TODO(shapiroc): implement")
+	return &api.GetDeviceConfigResponse{}, nil
+}
---
+++ src/chromiumos/build/cmd/metadataserver/metadataserver_test.go
@@ -0,0 +1,55 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"log"
+	"net"
+	"testing"
+
+	"go.chromium.org/chromiumos/config/go/build/api"
+	"google.golang.org/grpc"
+)
+
+// MetadataServer handles empty GetDeviceConfig
+func TestMetadataServer_GetDeviceConfig_Empty(t *testing.T) {
+	var logBuf bytes.Buffer
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		t.Fatal("Failed to create a net listener: ", err)
+	}
+
+	ctx := context.Background()
+	srv, err := newMetadataServer(
+		l,
+		log.New(&logBuf, "", log.LstdFlags|log.LUTC),
+	)
+	if err != nil {
+		t.Fatalf("Failed to start MetadataServer: %v", err)
+	}
+	go srv.Serve(l)
+	defer srv.Stop()
+
+	conn, err := grpc.Dial(l.Addr().String(), grpc.WithInsecure())
+	if err != nil {
+		t.Fatalf("Failed to dial: %v", err)
+	}
+	defer conn.Close()
+
+	cl := api.NewMetadataServiceClient(conn)
+	response, err := cl.GetDeviceConfig(ctx, &api.GetDeviceConfigRequest{})
+	if err != nil {
+		t.Fatalf("Failed at MetadataServer.GetDeviceConfig: %v", err)
+	}
+	if err != nil {
+		t.Fatalf("Failed at get response: %v", err)
+	}
+
+	if response == nil {
+		t.Fatalf("Failed to handle empty request")
+	}
+}
