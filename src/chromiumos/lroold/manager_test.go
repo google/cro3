1294ad0d9a27a3c113c6d13bf0c9034ce9319187
Otabek Kasimov <otabek@google.com>
2021-08-25 16:25:52 -0700

    lroold: Add package to with with old longrunning package
    BUG=b:195062451
    TEST=unittest, local emerge
    
    Change-Id: Ie0cc94a3fe3fb12469e3543dcab6c0c5d2fd56e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3120305
    Commit-Queue: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Jaques Clapauch <jaquesc@google.com>
    Tested-by: Otabek Kasimov <otabek@google.com>
    Reviewed-by: Jaques Clapauch <jaquesc@google.com>

---
+++ src/chromiumos/lroold/manager_test.go
@@ -0,0 +1,50 @@
+package lroold_test
+
+import (
+	"context"
+	"net"
+	"testing"
+
+	"chromiumos/lroold"
+
+	"go.chromium.org/chromiumos/config/go/api/test/tls/dependencies/longrunning"
+	"go.chromium.org/chromiumos/config/go/test/api"
+	"go.chromium.org/chromiumos/config/go/api/test/tls"
+	"google.golang.org/grpc"
+)
+
+type exampleServer struct {
+	tls.UnimplementedCommonServer
+	*lroold.Manager
+}
+
+func (s *exampleServer) Serve(l net.Listener) error {
+	s.Manager = lroold.New()
+	defer s.Manager.Close()
+	server := grpc.NewServer()
+	tls.RegisterCommonServer(server, s)
+	longrunning.RegisterOperationsServer(server, s.Manager)
+	return server.Serve(l)
+}
+
+func (s *exampleServer) RunTests(ctx context.Context, req *api.RunTestsRequest) (*longrunning.Operation, error) {
+	op := s.Manager.NewOperation()
+	go s.RunTests(ctx, req)
+	return op, nil
+}
+
+func RunServer() {
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+	s := exampleServer{}
+	if err := s.Serve(l); err != nil {
+		panic(err)
+	}
+}
+
+func TestServe(t *testing.T) {
+	go RunServer()
+	// TODO(shapiroc): Add testing
+}
