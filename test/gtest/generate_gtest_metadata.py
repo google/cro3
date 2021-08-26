3c4f459bad07c5a1bdb5220d0d8eb348dd29cc83
Jesse McGuire <jessemcguire@google.com>
2021-08-26 17:25:30 +0000

    F20: Gtest metadata generation support
    In order to add gtest as a first class harness, a mechanism is needed to
    generate the appropriate metadata for the test cases.
    
    This solution will consolidates test case metadata generation while
    leaving the actual test build/deployment details federated amongst
    product areas.
    
    BUG=b:198202240
    TEST=Tested as part of gtest-tests build, tested with multiple tests
    (extra fake tests), and generated/decompiled protobuf data to ensure
    correctness
    
    Cq-Depend: chromium:3116544, chromium:3139178
    Change-Id: Ie1e92271577fc0c5b411de638e20fdd338da694a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3123329
    Tested-by: Jesse McGuire <jessemcguire@google.com>
    Commit-Queue: Jesse McGuire <jessemcguire@google.com>
    Reviewed-by: Seewai Fu <seewaifu@google.com>
    Reviewed-by: Derek Beckett <dbeckett@chromium.org>
    Reviewed-by: C Shapiro <shapiroc@chromium.org>

---
+++ test/gtest/generate_gtest_metadata.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Ignore indention messages, since legacy scripts use 2 spaces instead of 4.
+# pylint: disable=bad-indentation,docstring-section-indent
+# pylint: disable=docstring-trailing-quotes
+
+"""Generate gtest metadata for testexecserv from provided yaml"""
+
+import argparse
+import pathlib
+import sys
+import yaml
+import six
+import jsonschema
+
+from google.protobuf import text_format
+from chromiumos.test.api import test_case_metadata_pb2 as tc_metadata_pb
+from chromiumos.test.api import test_harness_pb2 as th_pb
+from chromiumos.test.api import test_case_pb2 as tc_pb
+
+
+def _test_case_exec_factory() -> th_pb.TestHarness:
+    """Factory method to build TestCaseExec proto objects"""
+    return tc_metadata_pb.TestCaseExec(test_harness=th_pb.TestHarness(
+        gtest=th_pb.TestHarness.Gtest()))
+
+
+def _test_case_info_factory(data: dict) -> list:
+    """Factory method to build TestCaseInfo proto objects
+
+    Args:
+        data: dict representing the 'owners' yaml data
+    """
+    return tc_metadata_pb.TestCaseInfo(
+        owners=[tc_metadata_pb.Contact(email=x['email']) for x in data])
+
+
+def _test_case_factory(case_data: dict, suite_name: str) -> tc_pb.TestCase:
+    """Factory method for build TestCase proto objects
+
+    Args:
+        case_data: dict representing the 'cases' yaml data
+        suite_name: string representing the 'name' yaml field
+    """
+    tags = [tc_pb.TestCase.Tag(value=t) for t in case_data['tags']]
+    tc_id = tc_pb.TestCase.Id(value=f'gtest.{suite_name}.{case_data["id"]}')
+    name = f'{suite_name}.{case_data["id"]}'
+    return tc_pb.TestCase(id=tc_id, name=name, tags=tags)
+
+
+def _test_case_metadata_factory(
+        case: tc_pb.TestCase, case_exec: tc_metadata_pb.TestCaseExec,
+        case_info: tc_metadata_pb.TestCaseInfo
+) -> tc_metadata_pb.TestCaseMetadata:
+    """Factory method for building TestCaseMetadata proto objects"""
+    return tc_metadata_pb.TestCaseMetadata(test_case=case,
+                                           test_case_exec=case_exec,
+                                           test_case_info=case_info)
+
+
+def _test_case_list_factory(input_data: dict) -> list:
+    """Factory method for batch building TestCaseMetadata objects"""
+    suite_name = input_data['name']
+    test_case_exec = _test_case_exec_factory()
+    test_case_info = _test_case_info_factory(input_data['owners'])
+
+    test_cases = [
+        _test_case_factory(tc, suite_name) for tc in input_data['cases']
+    ]
+
+    test_case_metadata = [
+        _test_case_metadata_factory(case, test_case_exec, test_case_info)
+        for case in test_cases
+    ]
+
+    return test_case_metadata
+
+
+def _validate_yaml_file(parsed_yaml: dict, schema: dict):
+    """Validate the gtest yaml file against the schema
+
+    The validate() method will throw an exception if the yaml is not valid.
+
+    Args:
+        parsed_yaml: dict representation of the yaml file
+        schema: dict representation of the yaml schema
+    """
+    jsonschema.validate(instance=parsed_yaml, schema=schema)
+
+
+def main(input_files: list, output_file: pathlib.Path,
+         yaml_schema_file: pathlib.Path):
+    """Main entry point
+
+    Args:
+        input_files: list of pathlib.Path objects for yaml metadata files
+        output_file: pathlib.Path object for storing generated proto metadata
+        yaml_schema_file: pathlib.Path object representing the YAML schema
+    """
+    test_case_metadata = []
+
+    schema = yaml.safe_load(yaml_schema_file.read_text())
+
+    for f in input_files:
+        yaml_data = yaml.safe_load(f.read_text())
+        _validate_yaml_file(yaml_data, schema)
+        test_case_metadata.extend(_test_case_list_factory(yaml_data))
+
+    test_case_metadata_list = tc_metadata_pb.TestCaseMetadataList(
+        values=test_case_metadata)
+
+    # Make sure the appropriate directory structure is created.
+    # Mimic "mkdir -p" here.
+    output_file.parent.mkdir(parents=True, exist_ok=True)
+    output_file.write_bytes(test_case_metadata_list.SerializeToString())
+
+
+def _argparse_existing_file_factory(path: str) -> pathlib.Path:
+    """Create path objects
+
+    Verify the files specified on the command line against:
+        1) The file exists
+        2) The file is a file, not a directory or other type
+
+    Args:
+        path: path to file specified (str)
+    """
+    p = pathlib.Path(path)
+    if not p.exists():
+        raise argparse.ArgumentTypeError(
+            f"The specified file '{path}' does not exist!")
+
+    if not p.is_file():
+        raise argparse.ArgumentTypeError(
+            f"The specified file '{path}' is not a file!")
+
+    return p
+
+
+def _argparse_file_factory(path: str) -> pathlib.Path:
+    """Factory method that builds a pathlib.Path object"""
+    return pathlib.Path(path)
+
+
+if __name__ == '__main__':
+    if six.PY2:
+        print('ERROR: Python2 detected, this script only runs with python3!')
+        sys.exit(1)
+
+    parser = argparse.ArgumentParser(
+        description='Generate Gtest harness test case metadata')
+    parser.add_argument('--output_file',
+                        help='Output file to write proto metadata',
+                        type=_argparse_file_factory,
+                        required=True)
+    parser.add_argument('--yaml_schema',
+                        help='YAML schema file to validate test metadata',
+                        type=_argparse_existing_file_factory,
+                        required=True)
+    parser.add_argument('files',
+                        help='Gtest YAML metadata files',
+                        metavar='INPUT_YAML',
+                        type=_argparse_existing_file_factory,
+                        nargs='+')
+    parser.add_argument(
+        '--dump',
+        help='Dump pretty printed protobuf to stdout. For debugging purposes',
+        action='store_true',
+        default=False,
+        required=False)
+    args = parser.parse_args()
+    main(args.files, args.output_file, args.yaml_schema)
+
+    if args.dump:
+        metadata_list = tc_metadata_pb.TestCaseMetadataList()
+        metadata_list.ParseFromString(args.output_file.read_bytes())
+        print(text_format.MessageToString(metadata_list))
---
+++ test/gtest/gtest_unittest_const.py
@@ -0,0 +1,324 @@
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Ignore indention messages, since legacy scripts use 2 spaces instead of 4.
+# pylint: disable=bad-indentation,docstring-section-indent
+# pylint: disable=docstring-trailing-quotes
+
+# Ignore line too long errors as many lines are large byte-arrays over 80 char
+# pylint: disable=line-too-long
+"""Consts used by the generate_gtest_metadata unit tests."""
+
+###### VALID YAML DATA #######
+VALID_YAML_SINGLE_CASE_NO_TAG = """---
+author: "New Team"
+name: "MyFakeTest"
+harness: "gtest"
+category: "functional"
+owners:
+  - email: "owneremail@google.com"
+
+
+cases:
+  - id: "FakeCase1"
+    tags: []
+    criteria: "Nothing special"
+
+target_bin_location: "/usr/local/fake_test_path/test_binary"
+...
+"""
+VALID_YAML_SINGLE_CASE_NO_TAG_PROTOBUF = b'\nW\n4\n\x1c\n\x1agtest.MyFakeTest.FakeCase1\x12\x14MyFakeTest.FakeCase1\x12\x04\n\x02"\x00\x1a\x19\n\x17\n\x15owneremail@google.com'
+
+VALID_YAML_SINGLE_CASE_ONE_TAG = """---
+author: "Existing Team"
+name: "MyFakeTest2"
+harness: "gtest"
+category: "functional"
+owners:
+  - email: "owneremail@google.com"
+
+
+cases:
+  - id: "FakeCaseX"
+    tags: ["tag one"]
+    criteria: "Just one tag"
+
+target_bin_location: "/usr/local/fake_test_path/test_binary"
+...
+"""
+VALID_YAML_SINGLE_CASE_ONE_TAG_PROTOBUF = b'\nd\nA\n\x1d\n\x1bgtest.MyFakeTest2.FakeCaseX\x12\x15MyFakeTest2.FakeCaseX\x1a\t\n\x07tag one\x12\x04\n\x02"\x00\x1a\x19\n\x17\n\x15owneremail@google.com'
+
+VALID_YAML_SINGLE_CASE_MULTIPLE_TAGS = """---
+author: "The Best Team"
+name: "YetAnotherTest"
+harness: "gtest"
+category: "functional"
+owners:
+  - email: "myemail@chromium.org"
+
+
+cases:
+  - id: "AnotherFakeCase"
+    tags: ["tag one", "two tag", "another tag", "this_tag", "my*tag", "your&tag"]
+    criteria: "Single case with multiple tags"
+
+target_bin_location: "/usr/bin/fake_test_path/test_binary"
+...
+"""
+VALID_YAML_SINGLE_CASE_MULTIPLE_TAGS_PROTOBUF = b'\n\xb2\x01\n\x8f\x01\n&\n$gtest.YetAnotherTest.AnotherFakeCase\x12\x1eYetAnotherTest.AnotherFakeCase\x1a\t\n\x07tag one\x1a\t\n\x07two tag\x1a\r\n\x0banother tag\x1a\n\n\x08this_tag\x1a\x08\n\x06my*tag\x1a\n\n\x08your&tag\x12\x04\n\x02"\x00\x1a\x18\n\x16\n\x14myemail@chromium.org'
+
+VALID_YAML_MULTIPLE_CASE_NO_TAG = """---
+author: "New Team"
+name: "MyFakeTest"
+harness: "gtest"
+category: "functional"
+owners:
+  - email: "owneremail@google.com"
+
+
+cases:
+  - id: "FakeCase1"
+    tags: []
+    criteria: "Nothing special"
+  - id: "FakeCase2"
+    tags: []
+    criteria: "Nothing special in case 2"
+
+target_bin_location: "/usr/local/fake_test_path/test_binary"
+...
+"""
+VALID_YAML_MULTIPLE_CASE_NO_TAG_PROTOBUF = b'\nW\n4\n\x1c\n\x1agtest.MyFakeTest.FakeCase1\x12\x14MyFakeTest.FakeCase1\x12\x04\n\x02"\x00\x1a\x19\n\x17\n\x15owneremail@google.com\nW\n4\n\x1c\n\x1agtest.MyFakeTest.FakeCase2\x12\x14MyFakeTest.FakeCase2\x12\x04\n\x02"\x00\x1a\x19\n\x17\n\x15owneremail@google.com'
+
+VALID_YAML_MULTIPLE_CASE_ONE_TAG = """---
+author: "Multi-Case Team"
+name: "MultiCase_FakeTest"
+harness: "gtest"
+category: "functional"
+owners:
+  - email: "multiowners@google.com"
+  - email: "secondowner@google.com"
+
+
+cases:
+  - id: "FakeCaseX"
+    tags: ["tag one"]
+    criteria: "Just one tag"
+  - id: "FakeCaseY"
+    tags: ["tag two"]
+    criteria: "Just another single tag"
+
+target_bin_location: "/tmp/test"
+...
+"""
+VALID_YAML_MULTIPLE_CASE_ONE_TAG_PROTOBUF = b'\n\x8d\x01\nO\n$\n"gtest.MultiCase_FakeTest.FakeCaseX\x12\x1cMultiCase_FakeTest.FakeCaseX\x1a\t\n\x07tag one\x12\x04\n\x02"\x00\x1a4\n\x18\n\x16multiowners@google.com\n\x18\n\x16secondowner@google.com\n\x8d\x01\nO\n$\n"gtest.MultiCase_FakeTest.FakeCaseY\x12\x1cMultiCase_FakeTest.FakeCaseY\x1a\t\n\x07tag two\x12\x04\n\x02"\x00\x1a4\n\x18\n\x16multiowners@google.com\n\x18\n\x16secondowner@google.com'
+
+VALID_YAML_MULTIPLE_CASE_MULTIPLE_TAGS = """---
+author: "Multi-Case Team"
+name: "MultiCase_FakeTest"
+harness: "gtest"
+category: "functional"
+owners:
+  - email: "multiowners@google.com"
+  - email: "secondowner@google.com"
+  - email: "thirdowner@chromium.org"
+
+
+cases:
+  - id: "FakeCaseX"
+    tags: ["one tag", "two tag", "three tag", "four", "five tag", "six tag", "seven tag", "more"]
+    criteria: "Just one tag"
+  - id: "FakeCaseY"
+    tags: ["tag one", "tag two"]
+    criteria: "Just another single tag with random punctuation .afdk;jhg"
+  - id: "FakeCaseZ"
+    tags: ["one", "now", "never", "tag"]
+    criteria: "Multi-tags"
+
+target_bin_location: "/tmp/test/gtest/my_test"
+...
+"""
+VALID_YAML_MULTIPLE_CASE_MULTIPLE_TAGS_PROTOBUF = b'\n\xf5\x01\n\x9b\x01\n$\n"gtest.MultiCase_FakeTest.FakeCaseX\x12\x1cMultiCase_FakeTest.FakeCaseX\x1a\t\n\x07one tag\x1a\t\n\x07two tag\x1a\x0b\n\tthree tag\x1a\x06\n\x04four\x1a\n\n\x08five tag\x1a\t\n\x07six tag\x1a\x0b\n\tseven tag\x1a\x06\n\x04more\x12\x04\n\x02"\x00\x1aO\n\x18\n\x16multiowners@google.com\n\x18\n\x16secondowner@google.com\n\x19\n\x17thirdowner@chromium.org\n\xb3\x01\nZ\n$\n"gtest.MultiCase_FakeTest.FakeCaseY\x12\x1cMultiCase_FakeTest.FakeCaseY\x1a\t\n\x07tag one\x1a\t\n\x07tag two\x12\x04\n\x02"\x00\x1aO\n\x18\n\x16multiowners@google.com\n\x18\n\x16secondowner@google.com\n\x19\n\x17thirdowner@chromium.org\n\xbb\x01\nb\n$\n"gtest.MultiCase_FakeTest.FakeCaseZ\x12\x1cMultiCase_FakeTest.FakeCaseZ\x1a\x05\n\x03one\x1a\x05\n\x03now\x1a\x07\n\x05never\x1a\x05\n\x03tag\x12\x04\n\x02"\x00\x1aO\n\x18\n\x16multiowners@google.com\n\x18\n\x16secondowner@google.com\n\x19\n\x17thirdowner@chromium.org'
+
+VALID_YAML_ALL_FILES_PROTOBUF = b'\nW\n4\n\x1c\n\x1agtest.MyFakeTest.FakeCase1\x12\x14MyFakeTest.FakeCase1\x12\x04\n\x02"\x00\x1a\x19\n\x17\n\x15owneremail@google.com\nd\nA\n\x1d\n\x1bgtest.MyFakeTest2.FakeCaseX\x12\x15MyFakeTest2.FakeCaseX\x1a\t\n\x07tag one\x12\x04\n\x02"\x00\x1a\x19\n\x17\n\x15owneremail@google.com\n\xb2\x01\n\x8f\x01\n&\n$gtest.YetAnotherTest.AnotherFakeCase\x12\x1eYetAnotherTest.AnotherFakeCase\x1a\t\n\x07tag one\x1a\t\n\x07two tag\x1a\r\n\x0banother tag\x1a\n\n\x08this_tag\x1a\x08\n\x06my*tag\x1a\n\n\x08your&tag\x12\x04\n\x02"\x00\x1a\x18\n\x16\n\x14myemail@chromium.org\nW\n4\n\x1c\n\x1agtest.MyFakeTest.FakeCase1\x12\x14MyFakeTest.FakeCase1\x12\x04\n\x02"\x00\x1a\x19\n\x17\n\x15owneremail@google.com\nW\n4\n\x1c\n\x1agtest.MyFakeTest.FakeCase2\x12\x14MyFakeTest.FakeCase2\x12\x04\n\x02"\x00\x1a\x19\n\x17\n\x15owneremail@google.com\n\x8d\x01\nO\n$\n"gtest.MultiCase_FakeTest.FakeCaseX\x12\x1cMultiCase_FakeTest.FakeCaseX\x1a\t\n\x07tag one\x12\x04\n\x02"\x00\x1a4\n\x18\n\x16multiowners@google.com\n\x18\n\x16secondowner@google.com\n\x8d\x01\nO\n$\n"gtest.MultiCase_FakeTest.FakeCaseY\x12\x1cMultiCase_FakeTest.FakeCaseY\x1a\t\n\x07tag two\x12\x04\n\x02"\x00\x1a4\n\x18\n\x16multiowners@google.com\n\x18\n\x16secondowner@google.com\n\xf5\x01\n\x9b\x01\n$\n"gtest.MultiCase_FakeTest.FakeCaseX\x12\x1cMultiCase_FakeTest.FakeCaseX\x1a\t\n\x07one tag\x1a\t\n\x07two tag\x1a\x0b\n\tthree tag\x1a\x06\n\x04four\x1a\n\n\x08five tag\x1a\t\n\x07six tag\x1a\x0b\n\tseven tag\x1a\x06\n\x04more\x12\x04\n\x02"\x00\x1aO\n\x18\n\x16multiowners@google.com\n\x18\n\x16secondowner@google.com\n\x19\n\x17thirdowner@chromium.org\n\xb3\x01\nZ\n$\n"gtest.MultiCase_FakeTest.FakeCaseY\x12\x1cMultiCase_FakeTest.FakeCaseY\x1a\t\n\x07tag one\x1a\t\n\x07tag two\x12\x04\n\x02"\x00\x1aO\n\x18\n\x16multiowners@google.com\n\x18\n\x16secondowner@google.com\n\x19\n\x17thirdowner@chromium.org\n\xbb\x01\nb\n$\n"gtest.MultiCase_FakeTest.FakeCaseZ\x12\x1cMultiCase_FakeTest.FakeCaseZ\x1a\x05\n\x03one\x1a\x05\n\x03now\x1a\x07\n\x05never\x1a\x05\n\x03tag\x12\x04\n\x02"\x00\x1aO\n\x18\n\x16multiowners@google.com\n\x18\n\x16secondowner@google.com\n\x19\n\x17thirdowner@chromium.org'
+
+###### INVALID YAML DATA #######
+INVALID_YAML_NO_AUTHOR = """---
+name: "MyFakeTest"
+harness: "gtest"
+category: "functional"
+owners:
+  - email: "owneremail@google.com"
+
+
+cases:
+  - id: "FakeCase1"
+    tags: []
+    criteria: "Nothing special"
+
+target_bin_location: "/usr/local/fake_test_path/test_binary"
+...
+"""
+
+INVALID_YAML_NO_NAME = """---
+author: "New Team"
+harness: "gtest"
+category: "functional"
+owners:
+  - email: "owneremail@google.com"
+
+
+cases:
+  - id: "FakeCase1"
+    tags: []
+    criteria: "Nothing special"
+
+target_bin_location: "/usr/local/fake_test_path/test_binary"
+...
+"""
+
+INVALID_YAML_NO_HARNESS = """---
+author: "New Team"
+name: "MyFakeTest"
+category: "functional"
+owners:
+  - email: "owneremail@google.com"
+
+
+cases:
+  - id: "FakeCase1"
+    tags: []
+    criteria: "Nothing special"
+
+target_bin_location: "/usr/local/fake_test_path/test_binary"
+...
+"""
+
+INVALID_YAML_NO_CATEGORY = """---
+author: "New Team"
+name: "MyFakeTest"
+harness: "gtest"
+owners:
+  - email: "owneremail@google.com"
+
+
+cases:
+  - id: "FakeCase1"
+    tags: []
+    criteria: "Nothing special"
+
+target_bin_location: "/usr/local/fake_test_path/test_binary"
+...
+"""
+
+INVALID_YAML_NO_OWNERS = """---
+author: "New Team"
+name: "MyFakeTest"
+harness: "gtest"
+category: "functional"
+
+cases:
+  - id: "FakeCase1"
+    tags: []
+    criteria: "Nothing special"
+
+target_bin_location: "/usr/local/fake_test_path/test_binary"
+...
+"""
+
+INVALID_YAML_NO_EMAIL = """---
+author: "New Team"
+name: "MyFakeTest"
+harness: "gtest"
+category: "functional"
+owners:
+  - not_correct_field: "owneremail@google.com"
+
+
+cases:
+  - id: "FakeCase1"
+    tags: []
+    criteria: "Nothing special"
+
+target_bin_location: "/usr/local/fake_test_path/test_binary"
+...
+"""
+
+INVALID_YAML_NO_CASES = """---
+author: "New Team"
+name: "MyFakeTest"
+harness: "gtest"
+category: "functional"
+owners:
+  - email: "owneremail@google.com"
+
+target_bin_location: "/usr/local/fake_test_path/test_binary"
+...
+"""
+
+INVALID_YAML_NO_CASE_ID = """---
+author: "New Team"
+name: "MyFakeTest"
+harness: "gtest"
+category: "functional"
+owners:
+  - email: "owneremail@google.com"
+
+
+cases:
+  - tags: []
+    criteria: "Nothing special"
+
+target_bin_location: "/usr/local/fake_test_path/test_binary"
+...
+"""
+
+INVALID_YAML_NO_CASE_TAGS = """---
+author: "New Team"
+name: "MyFakeTest"
+harness: "gtest"
+category: "functional"
+owners:
+  - email: "owneremail@google.com"
+
+
+cases:
+  - id: "FakeCase1"
+    criteria: "Nothing special"
+
+target_bin_location: "/usr/local/fake_test_path/test_binary"
+...
+"""
+
+INVALID_YAML_NO_CASE_CRITERIA = """---
+author: "New Team"
+name: "MyFakeTest"
+harness: "gtest"
+category: "functional"
+owners:
+  - email: "owneremail@google.com"
+
+
+cases:
+  - id: "FakeCase1"
+    tags: []
+
+target_bin_location: "/usr/local/fake_test_path/test_binary"
+...
+"""
+
+INVALID_YAML_NO_CASE_TARGET_LOC = """---
+author: "New Team"
+name: "MyFakeTest"
+harness: "gtest"
+category: "functional"
+owners:
+  - email: "owneremail@google.com"
+
+
+cases:
+  - id: "FakeCase1"
+    tags: []
+    criteria: "Nothing special"
+...
+"""
