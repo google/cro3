2e3bfbe0ef4bc0e9d90edd3d6991614c825e9982
Derek Beckett <dbeckett@chromium.org>
2021-09-24 14:25:37 -0700

    f20 - remove telemetry from container deps
    Its baked into the test image it seems, and autotest is directly calling
    for that. It should not be needed.
    
    Container size down to ~1.7gb after this and other changes land
    
    BUG=b:200970609
    TEST=build-dockerimage.sh & run_container.py using login_LoginSuccess as
    the test on a freshly flashed DUt.
    
    Change-Id: I06cc99d20f18ba80f67b336fc2bb1bc38db07edf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3183599
    Tested-by: Derek Beckett <dbeckett@chromium.org>
    Auto-Submit: Derek Beckett <dbeckett@chromium.org>
    Reviewed-by: Jesse McGuire <jessemcguire@google.com>
    Reviewed-by: Seewai Fu <seewaifu@google.com>
    Commit-Queue: Derek Beckett <dbeckett@chromium.org>

--- test/container/utils/container_prep.py
+++ test/container/utils/container_prep.py
@@ -22,23 +22,23 @@ from python.lib import container_util  # noqa: E402
 def parse_local_arguments():
   """Parse the CLI."""
   parser = argparse.ArgumentParser(
-    description='Prep Tauto, Tast, & Services for DockerBuild.')
+      description='Prep Tauto, Tast, & Services for DockerBuild.')
   parser.add_argument('-chroot',
-            dest='chroot',
-            help='chroot (String): The chroot path to use.')
+                      dest='chroot',
+                      help='chroot (String): The chroot path to use.')
   parser.add_argument('-sysroot',
-            dest='sysroot',
-            help=' sysroot (String): The sysroot path to use.')
+                      dest='sysroot',
+                      help=' sysroot (String): The sysroot path to use.')
   parser.add_argument('-path',
-            dest='path',
-            help='path (String): Path to write artifacts to.')
+                      dest='path',
+                      help='path (String): Path to write artifacts to.')
   parser.add_argument('-force_path',
-            dest='force_path',
-            default=False,
-            help='Delete anything conflicting in the outpath.')
+                      dest='force_path',
+                      default=False,
+                      help='Delete anything conflicting in the outpath.')
   parser.add_argument('-src',
-            dest='src',
-            help='path (String): The src tree path.')
+                      dest='src',
+                      help='path (String): The src tree path.')
 
   args = parser.parse_args()
   return args
@@ -50,11 +50,11 @@ class DockerPrep():
   def __init__(self, args):
     """@param args (ArgumentParser): .chroot, .sysroot, .path."""
     self.args = args
-    self.full_autotest= ''
-    self.full_bin= ''
-    self.full_out= ''
-    self.build_path= ''
-    self.chroot_bin= ''
+    self.full_autotest = ''
+    self.full_bin = ''
+    self.full_out = ''
+    self.build_path = ''
+    self.chroot_bin = ''
     self.chroot = self.args.chroot
     self.src_root = self.args.src
     self.sysroot = self.args.sysroot
@@ -65,7 +65,7 @@ class DockerPrep():
     """Build up the paths needed in local mem."""
     self.build_path = os.path.join(self.chroot, self.sysroot)
     self.full_autotest = os.path.join(self.build_path,
-                      'usr/local/build/autotest')
+                                      'usr/local/build/autotest')
     self.chroot_bin = os.path.join(self.chroot, 'usr/bin')
     self.full_bin = os.path.join(self.build_path, 'usr/bin')
     self.full_out = os.path.join(self.build_path, self.args.path)
@@ -77,9 +77,9 @@ class DockerPrep():
     if not os.path.isdir(self.full_autotest):
       if not os.path.exists(self.full_autotest):
         raise Exception('Autotest path %s does not exist' %
-                self.full_autotest)
+                        self.full_autotest)
       raise Exception('Autotest path %s is not a directory' %
-              self.full_autotest)
+                      self.full_autotest)
 
     if not os.path.isdir(self.build_path):
       if not os.path.exists(self.build_path):
@@ -94,13 +94,13 @@ class DockerPrep():
         shutil.rmtree(self.full_out)
       else:
         raise Exception('outpath %s exists and force is not set.' %
-                self.full_out)
+                        self.full_out)
     os.makedirs(self.full_out, exist_ok=True)
 
   def create_tarball(self):
     """Copy the Stuff."""
     builder = container_util.AutotestTarballBuilder(
-      os.path.dirname(self.full_autotest), self.full_out, self.chroot)
+        os.path.dirname(self.full_autotest), self.full_out, self.chroot)
 
     builder.BuildFullAutotestandTastTarball()
 
@@ -110,32 +110,32 @@ class DockerPrep():
               .format(out=self.full_out))
 
   def remove_unused_deps(self):
-    UNUSED = ['chrome_test']
+    UNUSED = ['chrome_test', 'telemetry_dep']
     for dep in UNUSED:
       os.system('rm -r {out}/autotest/client/deps/{dep}'.format(
-        out=self.full_out, dep=dep))
+          out=self.full_out, dep=dep))
 
   def copy_services(self):
     """Copy services needed for Docker."""
     shutil.copy(os.path.join(self.chroot_bin, 'testexecserver'),
-          self.full_out)
+                self.full_out)
 
   def copy_metadata(self):
     """Return the absolute path of the metadata files."""
     # Relative to build
     _BUILD_METADATA_FILES = [
-      ('usr/local/build/autotest/autotest_metadata.pb',
-       os.path.join(self.full_out, 'autotest_metadata.pb')),
-      ('usr/share/tast/metadata/local/cros.pb',
-       os.path.join(self.full_out, 'local_cros.pb')),
-      ('build/share/tast/metadata/local/crosint.pb',
-       os.path.join(self.full_out, 'crosint.pb'))
+        ('usr/local/build/autotest/autotest_metadata.pb',
+         os.path.join(self.full_out, 'autotest_metadata.pb')),
+        ('usr/share/tast/metadata/local/cros.pb',
+         os.path.join(self.full_out, 'local_cros.pb')),
+        ('build/share/tast/metadata/local/crosint.pb',
+         os.path.join(self.full_out, 'crosint.pb'))
     ]
 
     # relative to chroot.
     _CHROOT_METADATA_FILES = [('usr/share/tast/metadata/remote/cros.pb',
-                   os.path.join(self.full_out,
-                        'remote_cros.pb'))]
+                               os.path.join(self.full_out,
+                                            'remote_cros.pb'))]
 
     for f, d in _BUILD_METADATA_FILES:
       full_md_path = container_util.FromSysrootPath(self.build_path, f)
