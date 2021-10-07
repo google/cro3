7c46a63cc2c57fb549ff73548b7896fd8901cdc8
Otabek Kasimov <otabek@google.com>
2021-10-06 17:56:17 -0700

    cros-provision: Create empty place holder fro CLI.
    BUG=b:199943048
    TEST=build
    
    Change-Id: Iad435911f6fcc12162263b2b9385c7d9ba2dda7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3210513
    Commit-Queue: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Derek Beckett <dbeckett@chromium.org>
    Commit-Queue: Seewai Fu <seewaifu@google.com>
    Tested-by: Otabek Kasimov <otabek@google.com>
    Auto-Submit: Otabek Kasimov <otabek@google.com>
    Reviewed-by: Derek Beckett <dbeckett@chromium.org>
    Reviewed-by: Seewai Fu <seewaifu@google.com>

---
+++ src/chromiumos/test/provision/cmd/cros-provision/main.go
@@ -0,0 +1,16 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package main implements the cros-provision used to setup CrOS devices.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Printf("Not implemented yet :)")
+	os.Exit(2)
+}
