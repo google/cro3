dd6ba4c29aad92ece31d0d2a902c5970f0fa4228
Lat Ware <latware@google.com>
2021-11-24 12:23:20 -0800

    Fixing callbox server build.
    BUG=b:207431904
    TEST=./callbox/docker/build-dockerimage.sh ~/chromiumos/chroot/ /build/amd64-generic --output /tmp/test_finder.jsonpb
    
    Change-Id: Ic10be9cd38476ed3c46864aa703387b2bec36c33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3300685
    Reviewed-by: Seewai Fu <seewaifu@google.com>
    Reviewed-by: Derek Beckett <dbeckett@chromium.org>
    Reviewed-by: Andrew Lamb <andrewlamb@chromium.org>
    Commit-Queue: Seewai Fu <seewaifu@google.com>
    Commit-Queue: Derek Beckett <dbeckett@chromium.org>
    Tested-by: Lat Ware <latware@google.com>
    Auto-Submit: Lat Ware <latware@google.com>

--- src/chromiumos/test/callbox/docker/build-dockerimage.sh
+++ src/chromiumos/test/callbox/docker/build-dockerimage.sh
@@ -8,5 +8,69 @@ readonly script_dir="$(dirname "$(realpath -e "${BASH_SOURCE[0]}")")"
 
 source "${script_dir}/../../../../../test/docker/util.sh"
 
-build_container_image --service "callbox-server" --docker_file "${script_dir}/Dockerfile" --tags "${tags}" --output "${output}" "$@"
+usage() {
+    echo "Usage: $0 <chroot> <sysroot> [options] [key=value...]"
+    echo
+    echo "Build a docker container for the cros-callbox service."
+    echo
+    echo "Args:"
+    echo "  chroot  - Path to the ChromeOS chroot on the host system."
+    echo "  sysroot - Path inside of the chroot to the board sysroot."
+    echo "  labels  - Zero or more key=value strings to apply as labels to container."
+    echo
+    echo "Options:"
+    echo "  --tags/-t - Comma separated list of tag names to apply to container"
+    exit 1
+}
+
+if [[ $# -lt 2 ]]; then
+    usage
+fi
+
+readonly chroot_path="$1"; shift
+readonly sysroot_path="$1"; shift
+
+host=""
+project=""
+tags=""
+output=""
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --host|-h)
+            host="$2"
+            shift 2
+            ;;
+        --project|-p)
+            project="$2"
+            shift 2
+            ;;
+        --tags|-t)
+            tags="$2"
+            shift 2
+            ;;
+        --output|-o)
+            output="$2"
+            shift 2
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+
+readonly output_dir="tmp/docker/croscallbox"
+readonly full_output_dir="${chroot_path}/${sysroot_path}/${output_dir}"
+
+prep_container
+
+build_container_image                                \
+    --service "cros-callbox"                         \
+    --docker_file "${script_dir}/Dockerfile"         \
+    --chroot "${chroot_path}"                        \
+    --tags "${tags}"                                 \
+    --output "${output}"                             \
+    --host "${host}"                                 \
+    --project "${project}"                           \
+    "${@}"
 
