d0921d3c52a50799b052f0ac66161ce6a2235eb2
Jack Rosenthal <jrosenth@chromium.org>
2022-02-04 10:55:33 -0700

    cros_unibuild_convert: Fix URL to chromeos-config README
    Branch was moved to "main".  Using "HEAD" for that works as well.
    
    BUG=none
    TEST=open URL
    
    Change-Id: Idd665d5b4f64030aea3a696414db7049c6429a55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3440931
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Auto-Submit: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Jacob Rasmussen <jacobraz@google.com>
    Commit-Queue: Jacob Rasmussen <jacobraz@google.com>

--- contrib/cros_unibuild_convert.py
+++ contrib/cros_unibuild_convert.py
@@ -99,7 +99,7 @@ CROS_WORKON_LOCALNAME="platform/empty-project"
 inherit cros-workon cros-unibuild
 
 DESCRIPTION="ChromeOS model configuration"
-HOMEPAGE="https://chromium.googlesource.com/chromiumos/platform2/+/master/chromeos-config/README.md"
+HOMEPAGE="https://chromium.googlesource.com/chromiumos/platform2/+/HEAD/chromeos-config/README.md"
 
 LICENSE="BSD-Google"
 SLOT="0"
