832b11e2da11cecf560518f6ec2b23786a4c4109
Katherine Threlkeld <kathrelkeld@google.com>
2022-02-08 13:51:21 -0800

    [Test libs] Initial registration code
    Code to collect information about test services which live in chromiumos
    src/.  When run, creates an output.json file (included here for example)
    which can be used by Test Services to find and start test libs.
    
    BUG=b:206658624
    TEST=used in proof-of-concept
    
    Change-Id: I4f399805f13eefda008cc199fe3fbdd05cac7e93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3449270
    Reviewed-by: Jaques Clapauch <jaquesc@google.com>
    Commit-Queue: Katherine Threlkeld <kathrelkeld@chromium.org>
    Tested-by: Katherine Threlkeld <kathrelkeld@chromium.org>

---
+++ src/chromiumos/test/lib-reg/lib_reg.go
@@ -0,0 +1,55 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package common defines shared resources across registration and test libs service.
+package main
+
+import (
+	"errors"
+	"strings"
+)
+
+// LibReg repsresents the information provided by a single library
+// when registering.
+type LibReg struct {
+	Name        string   `json:"name"`
+	APIType     string   `json:"api_type"`
+	Version     int      `json:"version"`
+	Image       string   `json:"image"`
+	RunOptions  string   `json:"run_options"`
+	Port        string   `json:"port"`
+	Ping        string   `json:"ping"`
+	Owners      []string `json:"owners"`
+	Description string   `json:"description"`
+}
+
+// Validate returns an error if the registration info has any issues.
+func (r *LibReg) Validate() error {
+	problems := []string{}
+	if r.Name == "" {
+		problems = append(problems, "Name cannot be blank")
+	}
+	if r.APIType == "" {
+		problems = append(problems, "APIType cannot be blank")
+	}
+	if r.APIType != "REST" {
+		problems = append(problems, "Unrecognized API type of "+r.APIType)
+	}
+	if r.Image == "" {
+		problems = append(problems, "Image name cannot be blank")
+	}
+	if r.Port == "" {
+		problems = append(problems, "Port name cannot be blank")
+	}
+	if len(r.Owners) == 0 {
+		problems = append(problems, "Provide at least one owner")
+	}
+	if r.Description == "" {
+		problems = append(problems, "Description string cannot be blank")
+	}
+	if len(problems) != 0 {
+		return errors.New(strings.Join(problems, "; "))
+	}
+	return nil
+}
---
+++ src/chromiumos/test/lib-reg/main.go
@@ -0,0 +1,126 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package main implements registration for test libraries.
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	servicesFile   = "services.txt"
+	defaultLogPath = "/tmp/test-libs-registration.log"
+)
+
+// newLogger creates a logger - using go default logger.
+func newLogger(logFile *os.File) *log.Logger {
+	mw := io.MultiWriter(logFile, os.Stderr)
+	return log.New(mw, "", log.LstdFlags|log.LUTC)
+}
+
+// readRegInfo parses a single library's file into a LibReg struct.
+func readRegInfo(infoPath string) (*LibReg, error) {
+	infoFile, err := os.Open(infoPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not open info file: %s", err)
+	}
+	defer infoFile.Close()
+
+	bytes, err := ioutil.ReadAll(infoFile)
+	if err != nil {
+		return nil, fmt.Errorf("could not read info file: %s", err)
+	}
+
+	reg := &LibReg{}
+	err = json.Unmarshal(bytes, reg)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse info file: %s", err)
+	}
+
+	err = reg.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return reg, nil
+}
+
+// WriteRegInfo creates a JSON file referencing all valid test libraries listed
+// line-by-line in servicesFile.  This JSON file is consumable by Test Libs Service.
+func WriteRegInfo(srcPath, outputPath string, logger *log.Logger) error {
+	// Locate src/ directory for relative file paths.
+	if filepath.Base(srcPath) != "src" {
+		logger.Println("Thought src/ was at", srcPath)
+		return errors.New("Could not find src/ - was src-path entered correctly?")
+	}
+
+	// Read in input.
+	in, err := os.Open(servicesFile)
+	if err != nil {
+		return fmt.Errorf("Could not open input file: %s", err)
+	}
+	defer in.Close()
+
+	// Find all register-able services.
+	libs := []*LibReg{}
+	scanner := bufio.NewScanner(in)
+	for scanner.Scan() {
+		txt := scanner.Text()
+		if txt == "" || strings.HasPrefix(txt, "//") {
+			continue
+		}
+		rInfo, err := readRegInfo(filepath.Join(srcPath, txt))
+		if err != nil {
+			logger.Printf("Could not get service info for %s: %s", txt, err)
+			continue
+		}
+		libs = append(libs, rInfo)
+	}
+
+	// Write output for later use.
+	regsJSON, err := json.Marshal(libs)
+	if err != nil {
+		return fmt.Errorf("Could not marshal output JSON: %s", err)
+	}
+	regsJSON = append(regsJSON, byte('\n')) // Add a newline for cros lint check.
+	ioutil.WriteFile(outputPath, regsJSON, 0644)
+	return nil
+}
+
+func main() {
+	var srcPath, outputPath, logPath string
+	flag.StringVar(&srcPath, "src-path", "",
+		"Path to src/ location.")
+	flag.StringVar(&outputPath, "output-path", "output.json",
+		"Path to output file location (will be overwritten if exists).")
+	flag.StringVar(&logPath, "log-path", defaultLogPath,
+		"Path to log output (also printed to stdout)")
+	flag.Parse()
+
+	logFile, err := os.Create(logPath)
+	if err != nil {
+		log.Fatalln("Failed to create log file", err)
+		os.Exit(2)
+	}
+	defer logFile.Close()
+	logger := newLogger(logFile)
+
+	err = WriteRegInfo(srcPath, srcPath, logger)
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(2)
+	}
+	return
+}
