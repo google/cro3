05b4cb6ad499b245fdf2bcd7355d4d21eed0f089
Derek Beckett <dbeckett@chromium.org>
2022-04-29 14:17:11 -0700

    cft: move cros-test docker building from dev/test to dev/src (4)
    This CL adds the base prep, along with the common "service" prep. The
    common service prep is capable for prepping the docker build context
    where only a Dockerfile and the executable is needed.
    
    BUG=b:230875828
    TEST=run_cros_test still works, cloudbuild works, all imports in these files work.
    
    Change-Id: Ic1269e2653bf5ea830f47ee088ad9f6f6d8f7032
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3617135
    Reviewed-by: Jaques Clapauch <jaquesc@google.com>
    Tested-by: Derek Beckett <dbeckett@chromium.org>
    Commit-Queue: Derek Beckett <dbeckett@chromium.org>

---
+++ src/chromiumos/test/python/src/docker_libs/build_libs/shared/base_prep.py
@@ -0,0 +1,85 @@
+# Copyright 2022 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Base Prep for Docker Build context creation."""
+
+import os
+
+# The default yaml for all current CFT builders.
+YAML_TEMPLATE = """
+substitutions:
+{subs}
+steps:
+- name: 'gcr.io/kaniko-project/executor:latest'
+  args:
+    [
+      "--dockerfile=Dockerfile",
+      "--context=.",
+      "--cache=true",
+      "--cache-ttl=366h",
+{destinations}
+{labels}
+    ]
+  timeout: 1800s
+options:
+  logging: CLOUD_LOGGING_ONLY
+"""
+
+SUB_VAR = '__BUILD_TAG{n}'
+LABEL_SUB_VAR = '__LABEL{n}'
+SUB_TEMPLATE = '  {var}: ""\n'
+DESTINATION_VAR = '      "--destination=${{{var}}}",\n'
+LABEL_VAR = '      "--label=${{{var}}}",\n'
+
+
+class BaseDockerPrepper():
+  """Prep Needed files for the Test Execution Container Docker Build."""
+
+  def __init__(self,
+               chroot: str,
+               sysroot: str,
+               tags: str,
+               labels: str,
+               service: str):
+    """@param args (ArgumentParser): .chroot, .sysroot, .path."""
+    self.tags = tags.split(',') if tags else []
+    self.labels = labels.split(',') if labels else []
+    self.chroot = chroot
+    self.sysroot = sysroot
+    self.outputdir = f'tmp/docker/{service}'
+    self.service = service
+
+    if self.sysroot.startswith('/'):
+      self.sysroot = self.sysroot[1:]
+    self.full_out_dir = os.path.join(self.chroot, self.sysroot, self.outputdir)
+
+  def prep_container(self):
+    """To be implemented by child class."""
+    raise NotImplementedError
+
+  def build_yaml(self):
+    """Construct and write the YAML into the docker context dir"""
+    subs = ''
+    destinations = ''
+    labels = ''
+
+    # Always add in atleast 1 tag. If none are given, 1 will be added by the
+    # builder by default, so ensure the cloudbuild yaml can support it.
+    for i in range(max(len(self.tags), 1)):
+      var = SUB_VAR.format(n=i)
+      subs += SUB_TEMPLATE.format(var=var)
+      destinations += DESTINATION_VAR.format(var=var)
+
+    for i in range(len(self.labels)):
+      var = LABEL_SUB_VAR.format(n=i)
+      subs += SUB_TEMPLATE.format(var=var)
+      labels += LABEL_VAR.format(var=var)
+
+    cloudbuild_yaml = YAML_TEMPLATE.format(subs=subs,
+                                           destinations=destinations,
+                                           labels=labels)
+
+    with open(os.path.join(self.full_out_dir, 'cloudbuild.yaml'), 'w') as wf:
+      wf.write(cloudbuild_yaml)
+      print(f'wrote yaml to {wf}')
---
+++ src/chromiumos/test/python/src/docker_libs/build_libs/shared/common_service_prep.py
@@ -0,0 +1,46 @@
+# Copyright 2022 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Docker build Context prep for Common Services."""
+
+import os
+import shutil
+import sys
+
+# Point up a few directories to make the other python modules discoverable.
+sys.path.append('../../../../')
+
+from src.docker_libs.build_libs.shared.base_prep import BaseDockerPrepper
+
+
+class CommonServiceDockerPrepper(BaseDockerPrepper):
+  """Prep Needed files for the Test Execution Container Docker Build."""
+
+  def __init__(self, chroot: str, sysroot: str, tags: str, labels: str,
+               service: str):
+    """@param args (ArgumentParser): .chroot, .sysroot, .path."""
+    super().__init__(chroot=chroot, sysroot=sysroot, tags=tags, labels=labels,
+                     service=service)
+
+    # TODO, better src discovery.
+
+  def prep_container(self):
+    """Will ONLY cp the given service and dockerfile to the build context."""
+    if os.path.exists(self.full_out_dir):
+      print(f'Deleting existing prepdir {self.full_out_dir}')
+      shutil.rmtree(self.full_out_dir)
+    os.makedirs(self.full_out_dir, exist_ok=True)
+
+    # TODO (b/230899120), this is a hardcode back up to the execution
+    # (cros-test) dir, I need to figure out a better way to do this,
+    #  but nothing comes to mind.
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    src = os.path.join(cwd, '../../../../../', f'dockerfiles/{self.service}/')
+    for item in os.listdir(src):
+      s = os.path.join(src, item)
+      d = os.path.join(self.full_out_dir, item)
+      shutil.copy2(s, d)
+
+    shutil.copy2(f'{self.chroot}/usr/bin/{self.service}',
+                 os.path.join(self.full_out_dir, self.service))
