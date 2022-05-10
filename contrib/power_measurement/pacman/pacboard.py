fcffc884c830db8f027f56d9921f7bcde6ac2494
Robert Zieba <robertzieba@google.com>
2022-05-10 16:09:07 -0600

    pacman: Add tool to provision pacdebugger serial numbers
    Pacdebuggers have a 128 byte EEPROM that is currently unused. This
    commit adds a tool that assign these devices a dedicated USB PID and
    serial number. This is a prerequisite to adding `servod` support for
    pacdebuggers. Serial numbers that start with `PACM` are reserved for
    'manually provisioned' use. This ensures that there will be no serial
    number collisions with any future manufacturer runs. Manually
    provisioned serial numbers are randomly generated 32-bit numbers,
    ensuring that the chance of a collision is minimal.
    
    BUG=b:231983777
    TEST=Tested with pacdebugger and verified that all operations work as
    expected
    
    Change-Id: I0184c944fb3ee5088219980be7d9cd96daff58a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3639630
    Auto-Submit: Robert Zieba <robertzieba@google.com>
    Commit-Queue: Ruben Rodriguez Buchillon <coconutruben@chromium.org>
    Reviewed-by: Ruben Rodriguez Buchillon <coconutruben@chromium.org>
    Tested-by: Robert Zieba <robertzieba@google.com>

---
+++ contrib/power_measurement/pacman/pacboard.py
@@ -0,0 +1,179 @@
+# Copyright 2022 The ChromiumOS Authors.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Module to deal with reading and writing pacdebugger board info"""
+
+import pyftdi.eeprom
+import pyftdi.ftdi
+import pyftdi.misc
+import pyftdi.spi
+
+
+class BoardError(Exception):
+    """Base exception class for errors related to the board"""
+
+class InvalidDevice(BoardError):
+    """Exception class for invalid devices"""
+    def __str__(self):
+        return 'Invaild Device'
+
+class InvalidSerial(BoardError):
+    """Exception class for an invalid serial number"""
+    def __str__(self):
+        return 'Invalid Serial'
+
+class PacDebugger:
+    """Board information for a pacdebugger"""
+    UNPROVISIONED_VID = 0x0403
+    UNPROVISIONED_PID = 0x6010
+
+    PROVISIONED_VID = 0x18d1
+    PROVISIONED_PID = 0x5211
+
+    CONFIG_FILE = 'ftdi_config/ft2232h_template.ini'
+
+    VID_PROPERTY = 'vendor_id'
+    PID_PROPERTY = 'product_id'
+    PRODUCT = 'PACDebuggerV1'
+
+    EEPROM_SIZE = 128
+    EEPROM_BYTE_WIDTH = 2
+
+    # 100 KHz should be supported by everything
+    UNBRICK_FREQ = 100000
+    # Write enable command
+    UNBRICK_WE_CMD = [0x98, 0x00]
+    # Write disable command
+    UNBRICK_WD_CMD = [0x90, 0x00]
+    # Erase command
+    UNBRICK_ERASE_CMD = 0xe0
+
+    @staticmethod
+    def get_boards():
+        """Get provisioned and unprovisioned PACdebuggers"""
+        provisioned = []
+        unprovisioned = []
+        devices = pyftdi.ftdi.Ftdi.find_all([
+            (PacDebugger.UNPROVISIONED_VID, PacDebugger.UNPROVISIONED_PID),
+            (PacDebugger.PROVISIONED_VID, PacDebugger.PROVISIONED_PID)
+        ])
+
+        index = 0
+        for ((_, pid, _, _, _, _, desc), _) in devices:
+            if pid == PacDebugger.PROVISIONED_PID:
+                board = PacDebugger(index)
+                board.read_info()
+                provisioned.append((index, board))
+            elif pid == PacDebugger.UNPROVISIONED_PID:
+                unprovisioned.append((index, desc))
+            index += 1
+
+        return (provisioned, unprovisioned)
+
+    @staticmethod
+    def configure_custom_devices():
+        """Allows our custom VID:PID to be recognized by the FTDI library"""
+        pyftdi.misc.add_custom_devices(pyftdi.ftdi.Ftdi, [
+            f'{PacDebugger.PROVISIONED_VID:#x}:{PacDebugger.PROVISIONED_PID:#x}'
+        ], force_hex=True)
+
+
+    @staticmethod
+    def device_to_url(device):
+        """Converts a device index into the corresponding device URL"""
+        devices = pyftdi.ftdi.Ftdi.find_all([
+            (PacDebugger.UNPROVISIONED_VID, PacDebugger.UNPROVISIONED_PID),
+            (PacDebugger.PROVISIONED_VID, PacDebugger.PROVISIONED_PID)
+        ])
+
+        if device < 0 or device >= len(devices):
+            raise InvalidDevice
+
+        ((vid, pid, bus, addr, _, _, _), _) = devices[device]
+        return f'ftdi://{vid:#x}:{pid:#x}:{bus:x}:{addr:x}/1'
+
+    @staticmethod
+    def unbrick(unbricker_url):
+        """Unbricks a PACDebugger using another FTDI USB<->SPI interface"""
+        controller = pyftdi.spi.SpiController()
+        controller.configure(unbricker_url)
+
+        # Use first CS pin on the interface
+        port = controller.get_port(cs=0, freq=PacDebugger.UNBRICK_FREQ)
+        # EEPROM CS is active high
+        port.force_select(level=False)
+
+        # Send the write enable cmd
+        port.force_select(level=True)
+        port.write(PacDebugger.UNBRICK_WE_CMD, start=False, stop=False)
+        port.force_select(level=False)
+
+        erase_count = PacDebugger.EEPROM_SIZE // PacDebugger.EEPROM_BYTE_WIDTH
+        for addr in range(0, erase_count):
+            cmd1 = PacDebugger.UNBRICK_ERASE_CMD
+            # Last addr bit doesn't fit in a single byte
+            cmd1 |= addr >> 1
+            cmd2 = (addr & 1) << 7
+
+            port.force_select(level=True)
+            port.write([cmd1, cmd2], start=False, stop=False)
+            port.force_select(level=False)
+
+        # Send write disable cmd
+        port.force_select(level=True)
+        port.write(PacDebugger.UNBRICK_WD_CMD, start=False, stop=False)
+        port.force_select(level=False)
+
+
+    def __init__(self, device):
+        """Connects to a provisioned PACDebugger"""
+        super().__init__()
+        self.serial = ''
+        self.name = ''
+
+        ftdi_url = PacDebugger.device_to_url(device)
+        self.eeprom = pyftdi.eeprom.FtdiEeprom()
+        self.eeprom.open(ftdi_url, size=PacDebugger.EEPROM_SIZE)
+
+
+    def read_info(self):
+        """Reads the serial number and product name from the EEPROM"""
+        self.serial = self.eeprom.serial
+        self.name = self.eeprom.product
+
+
+    def write_info(self):
+        """Writes current info to board header"""
+        # Use raw section because the ftdi library seems to have issues loading
+        # certain values from the human readable portion of the config
+        self.eeprom.load_config(open(PacDebugger.CONFIG_FILE), section='raw')
+
+        self.eeprom.set_property(PacDebugger.VID_PROPERTY,
+                                 PacDebugger.PROVISIONED_VID)
+        self.eeprom.set_property(PacDebugger.PID_PROPERTY,
+                                 PacDebugger.PROVISIONED_PID)
+
+        self.eeprom.set_product_name(PacDebugger.PRODUCT)
+
+        if not isinstance(self.serial, str) or self.serial == '':
+            raise InvalidSerial
+        self.eeprom.set_serial_number(self.serial)
+
+        self.eeprom.commit(dry_run=False)
+
+
+    def erase(self):
+        """Erases the EEPROM"""
+        self.eeprom.erase()
+        self.eeprom.commit(dry_run=False, no_crc=True)
+
+
+    def dump(self):
+        """Dumps EEPROM contents as hex"""
+        COLUMN_COUNT = 16
+        rows = len(self.eeprom.data) // COLUMN_COUNT
+
+        for j in range(0, rows):
+            for i in range(0, 16):
+                print(f'{self.eeprom.data[j * COLUMN_COUNT + i]:02x} ', end='')
+            print('')
---
+++ contrib/power_measurement/pacman/pactool.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright 2022 The ChromiumOS Authors.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Tool to provision, read and reset pacdebugger board info"""
+import argparse
+import random
+import sys
+
+import pacboard
+
+
+def validate_default_device(device):
+    """Ensures default device is only used when a single device is present"""
+    if device == -1:
+        (provisioned, unprovisioned) = pacboard.PacDebugger.get_boards()
+        if len(provisioned) + len(unprovisioned) > 1:
+            print(
+                'Multiple devices exist, specify device with --device/-d',
+                file=sys.stderr
+            )
+            sys.exit(1)
+
+        return 0
+
+    return device
+
+def dump(device):
+    """Dumps the EEPROM contents of a PACDebugger"""
+    device = validate_default_device(device)
+    board = pacboard.PacDebugger(device)
+    board.dump()
+
+
+def list_boards():
+    """Lists all attached pacdebuggers"""
+    (provisioned, unprovisioned) = pacboard.PacDebugger.get_boards()
+    if len(provisioned) > 0:
+        print('Provisioned')
+        print(f'Index {"Name":32} Serial')
+        for (index, board) in provisioned:
+            print(f'{index:<5} {board.name:32} {board.serial}')
+        print('')
+
+    if len(unprovisioned) > 0:
+        print('Unprovisioned')
+        print(f'Index {"Description":16}')
+        for (index, desc) in unprovisioned:
+            print(f'{index:<5} {desc:16}')
+
+
+def provision(device, serial):
+    """Provisions a pacdebugger with a serial number"""
+    device = validate_default_device(device)
+    board = pacboard.PacDebugger(device)
+
+    if serial is None or serial == '':
+        random.seed()
+        # Use PACM prefix for manually provisioned devices
+        serial = f'PACM{random.randrange(1, 2**32):#010}'
+
+    board.serial = serial
+    board.write_info()
+
+    print(f'Provisioned with serial number {serial}')
+
+
+def erase(device):
+    """Erase the EEPROM of a PACDebugger"""
+    device = validate_default_device(device)
+    board = pacboard.PacDebugger(device)
+    board.erase()
+
+
+def unbrick(unbricker_url):
+    """Unbricks a PACDebugger"""
+    pacboard.PacDebugger.unbrick(unbricker_url)
+
+
+def main():
+    """Pactool main function"""
+    parser = argparse.ArgumentParser()
+    sub_parsers = parser.add_subparsers(help='sub-command -h for specific help')
+
+    dump_parser = sub_parsers.add_parser(
+        'dump',
+        help='Dump EEPROM contents'
+    )
+    dump_parser.set_defaults(command='dump')
+    dump_parser.add_argument(
+        '--device',
+        '-d',
+        help='Index of device',
+        type=int,
+        default=-1
+    )
+
+    list_parser = sub_parsers.add_parser(
+        'list',
+        help='List currently connected devices'
+    )
+    list_parser.set_defaults(command='list')
+
+    provision_parser = sub_parsers.add_parser(
+        'provision',
+        help='Provisions a device'
+    )
+    provision_parser.set_defaults(command='provision')
+    provision_parser.add_argument(
+        '--device',
+        '-d',
+        help='Index of device',
+        type=int,
+        default=-1
+    )
+    provision_parser.add_argument(
+        '--serial',
+        '-s',
+        help='Serial number for device, \
+        0 results in a randomly generated manually provisioned serial number',
+        type=str,
+        default=None
+    )
+
+    erase_parser = sub_parsers.add_parser(
+        'erase',
+        help='Erases a provisioned device, unprovisioning it'
+    )
+    erase_parser.set_defaults(command='erase')
+    erase_parser.add_argument(
+        '--device',
+        '-d',
+        help='Index of device',
+        type=int,
+        default=-1
+    )
+
+    unbrick_parser = sub_parsers.add_parser(
+        'unbrick',
+        help='Unbricks a PACDebugger using a given FTDI USB<->SPI interface'
+    )
+    unbrick_parser.set_defaults(command='unbrick')
+    unbrick_parser.add_argument(
+        'unbricker_url',
+        help='FTDI URL for SPI interface'
+    )
+
+    args = parser.parse_args()
+
+    pacboard.PacDebugger.configure_custom_devices()
+
+    if args.command == 'dump':
+        dump(args.device)
+    elif args.command == 'list':
+        list_boards()
+    elif args.command == 'provision':
+        provision(args.device, args.serial)
+    elif args.command == 'erase':
+        erase(args.device)
+    elif args.command == 'unbrick':
+        unbrick(args.unbricker_url)
+    else:
+        args.print_help()
+
+
+if __name__ == '__main__':
+    main()
