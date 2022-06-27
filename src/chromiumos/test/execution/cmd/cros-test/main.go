a844d543c6696f2968b534e544442776ade5acda
Derek Beckett <dbeckett@chromium.org>
2022-06-27 12:29:59 -0700

    CFT - cros-test adding functionality to run as rpc server
    BUG=b:236759533
    TEST=fast build, emerge cros-test, build-dockerimages.py. Also ran it as a
    server inside docker container w/ a mocked client using an
    test-request-cli rpc, and got a valid response in the LRO.
    
    Disallow-Recycled-Builds: all
    Change-Id: Iea117df73d7b10bc3b66f056e02c3b92d5493c03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3726852
    Reviewed-by: Jaques Clapauch <jaquesc@google.com>
    Commit-Queue: Derek Beckett <dbeckett@chromium.org>
    Tested-by: Derek Beckett <dbeckett@chromium.org>
    Auto-Submit: Derek Beckett <dbeckett@chromium.org>

--- src/chromiumos/test/execution/cmd/cros-test/main.go
+++ src/chromiumos/test/execution/cmd/cros-test/main.go
@@ -11,8 +11,10 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"chromiumos/test/execution/cmd/cros-test/internal/common"
@@ -22,6 +24,9 @@ import (
 
 // Version is the version info of this command. It is filled in during emerge.
 var Version = "<unknown>"
+
+// defaulPort is the port where cros-test will bind to in server mode if a port is not provided.
+var defaultPort = 8001
 
 // createLogFile creates a file and its parent directory for logging purpose.
 func createLogFile(fullPath string) (*os.File, error) {
@@ -47,54 +52,182 @@ func newLogger(logFile *os.File) *log.Logger {
 	return log.New(mw, "", log.LstdFlags|log.LUTC)
 }
 
-func main() {
-	os.Exit(func() int {
-		t := time.Now()
-		defaultLogPath := filepath.Join(common.TestExecServerRoot, t.Format("20060102-150405"))
-		defaultRequestFile := filepath.Join(common.TestExecServerRoot, common.TestRequestJSONFile)
-		defaultResultFile := filepath.Join(common.TestExecServerRoot, common.TestResultJSONFile)
-		version := flag.Bool("version", false, "print version and exit")
-		log := flag.String("log", defaultLogPath, "specify the test execution server log directory")
-		input := flag.String("input", defaultRequestFile, "specify the test execution request json input file")
-		output := flag.String("output", defaultResultFile, "specify the test execution response json output file")
-		resultDir := flag.String("resultdir", common.TestResultDir, "specify default directory for test harnesses to store their run result")
-		tlwAddr := flag.String("tlwaddr", "", "specify the tlw address")
-		metadataDir := flag.String("metadatadir", common.TestMetadataDir,
-			"specify a directory that contain all test metadata proto files.")
+type args struct {
+	// Common input params.
+	logPath         string
+	inputPath       string
+	outputPath      string
+	resultsDirPath  string
+	tlwAddr         string
+	metadataDirPath string
+	version         bool
 
-		flag.Parse()
+	// Server mode params
+	port int
+}
 
-		if *version {
-			fmt.Println("executionservice version ", Version)
-			return 0
-		}
+// runCLI is the entry point for running cros-test (executionservice) in CLI mode.
+func runCLI(ctx context.Context, d []string) int {
+	t := time.Now()
+	defaultLogPath := filepath.Join(common.TestExecServerRoot, t.Format("20060102-150405"))
+	defaultRequestFile := filepath.Join(common.TestExecServerRoot, common.TestRequestJSONFile)
+	defaultResultFile := filepath.Join(common.TestExecServerRoot, common.TestResultJSONFile)
 
-		logFile, err := createLogFile(*log)
-		if err != nil {
-			return errors.WriteError(os.Stderr, err)
-		}
-		defer logFile.Close()
+	a := args{}
 
-		logger := newLogger(logFile)
-		logger.Println("Starting executionservice version ", Version)
+	fs := flag.NewFlagSet("Run cros-test", flag.ExitOnError)
+	fs.StringVar(&a.logPath, "log", defaultLogPath, fmt.Sprintf("Path to record execution logs. Default value is %s", defaultLogPath))
+	fs.StringVar(&a.inputPath, "input", defaultRequestFile, "specify the test execution request json input file")
+	fs.StringVar(&a.outputPath, "output", defaultResultFile, "specify the test execution response json output file")
+	fs.StringVar(&a.resultsDirPath, "resultdir", common.TestResultDir, "specify default directory for test harnesses to store their run result")
+	fs.StringVar(&a.tlwAddr, "tlwaddr", "", "specify the tlw address")
+	fs.StringVar(&a.metadataDirPath, "metadatadir", common.TestMetadataDir, "specify a directory that contain all test metadata proto files.")
+	fs.BoolVar(&a.version, "version", false, "print version and exit")
+	fs.Parse(d)
 
-		req, err := readInput(*input)
-		if err != nil {
-			return errors.WriteError(os.Stderr, err)
-		}
-
-		metadata, err := metadata.ReadDir(*metadataDir)
-		if err != nil {
-			return errors.WriteError(os.Stderr, err)
-		}
-		ctx := context.Background()
-		rspn, err := runTests(ctx, logger, *resultDir, *tlwAddr, metadata, req)
-		if err != nil {
-			return errors.WriteError(os.Stderr, err)
-		}
-		if err := writeOutput(*output, rspn); err != nil {
-			return errors.WriteError(os.Stderr, err)
-		}
+	if a.version {
+		fmt.Println("executionservice version ", Version)
 		return 0
-	}())
+	}
+
+	logFile, err := createLogFile(a.logPath)
+	if err != nil {
+		log.Fatalln("Failed to create log file", err)
+		return 2
+	}
+	defer logFile.Close()
+
+	logger := newLogger(logFile)
+	logger.Println("Starting executionservice version ", Version)
+
+	req, err := readInput(a.inputPath)
+	if err != nil {
+		log.Fatalf("Failed to read request input: %s", err)
+		return 2
+	}
+
+	metadata, err := metadata.ReadDir(a.metadataDirPath)
+	if err != nil {
+		log.Fatalf("Failed to read metadata input: %s", err)
+		return 2
+	}
+
+	rspn, err := runTests(ctx, logger, a.resultsDirPath, a.tlwAddr, metadata, req)
+	if err != nil {
+		logger.Fatalln("Failed to run tests: ", err)
+		return 1
+	}
+
+	logger.Printf("Writing line to %s", a.outputPath)
+	if err := writeOutput(a.outputPath, rspn); err != nil {
+		logger.Fatalf("Failed to write output file to %s: %d", a.outputPath, err)
+		return 2
+	}
+	return 0
+}
+
+// startServer is the entry point for running cros-test (executionservice) in server mode.
+func startServer(d []string) int {
+	a := args{}
+	t := time.Now()
+	defaultLogPath := filepath.Join(common.TestExecServerRoot, t.Format("20060102-150405"))
+
+	fs := flag.NewFlagSet("Start executionservice server", flag.ExitOnError)
+	fs.StringVar(&a.logPath, "log", defaultLogPath, fmt.Sprintf("Path to record execution logs. Default value is %s", defaultLogPath))
+	fs.StringVar(&a.resultsDirPath, "resultdir", common.TestResultDir, "specify the test execution request json input file")
+	fs.StringVar(&a.tlwAddr, "tlwaddr", "", "specify the tlw address")
+	fs.StringVar(&a.metadataDirPath, "metadatadir", common.TestMetadataDir, "specify a directory that contain all test metadata proto files.")
+	fs.IntVar(&a.port, "port", defaultPort, fmt.Sprintf("Specify the port for the server. Default value %d.", defaultPort))
+
+	logFile, err := createLogFile(a.logPath)
+	if err != nil {
+		log.Fatalln("Failed to create log file", err)
+		return 2
+	}
+	defer logFile.Close()
+
+	logger := newLogger(logFile)
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
+	if err != nil {
+		logger.Fatalln("Failed to create a net listener: ", err)
+		return 2
+	}
+	logger.Println("Starting executionservice on port ", a.port)
+
+	metadata, err := metadata.ReadDir(a.metadataDirPath)
+	if err != nil {
+		log.Fatalf("Failed to read metadata input: %s", err)
+		return 2
+	}
+
+	server, closer := NewServer(logger, a.resultsDirPath, a.tlwAddr, metadata)
+	defer closer()
+	err = server.Serve(l)
+	if err != nil {
+		logger.Fatalln("Failed to initialize server: ", err)
+		return 2
+	}
+	return 0
+}
+
+// Specify run mode for CLI.
+type runMode string
+
+const (
+	runCli     runMode = "cli"
+	runServer  runMode = "server"
+	runVersion runMode = "version"
+	runHelp    runMode = "help"
+
+	runCliDefault runMode = "cliDefault"
+)
+
+func getRunMode() (runMode, error) {
+	if len(os.Args) > 1 {
+		for _, a := range os.Args {
+			if a == "-version" {
+				return runVersion, nil
+			}
+		}
+		switch strings.ToLower(os.Args[1]) {
+		case "cli":
+			return runCli, nil
+		case "server":
+			return runServer, nil
+		case "help":
+			return runHelp, nil
+		}
+	}
+
+	// If we did not find special run mode then just run CLI to match legacy behavior.
+	return runCliDefault, nil
+}
+
+func mainInternal(ctx context.Context) int {
+	runMode, err := getRunMode()
+	if err != nil {
+		log.Fatalln(err)
+		return 2
+	}
+	switch runMode {
+
+	case runCliDefault:
+		log.Printf("No mode specified, assuming CLI.")
+		return runCLI(ctx, os.Args)
+	case runCli:
+		log.Printf("Running CLI mode!")
+		return runCLI(ctx, os.Args[2:])
+	case runServer:
+		log.Printf("Running server mode!")
+		return startServer(os.Args[2:])
+	case runVersion:
+		log.Printf("executionservice version: %s", Version)
+		return 0
+	}
+	return 0
+}
+
+func main() {
+	os.Exit(mainInternal(context.Background()))
 }
---
+++ src/chromiumos/test/execution/cmd/cros-test/testexecserver_rpc.go
@@ -0,0 +1,66 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"log"
+
+	"go.chromium.org/chromiumos/config/go/longrunning"
+	"go.chromium.org/chromiumos/config/go/test/api"
+	"go.chromium.org/luci/common/errors"
+	"google.golang.org/grpc"
+
+	"chromiumos/lro"
+)
+
+// TestServiceServer implementation of dut_service.proto
+type ExecutionServiceServer struct {
+	manager        *lro.Manager
+	logger         *log.Logger
+	resultsDirPath string
+	tlwAddr        string
+	metadata       *api.TestCaseMetadataList
+}
+
+// // NewServer creates an execution server.
+func NewServer(logger *log.Logger, resultRootDir, tlwAddr string, metadataList *api.TestCaseMetadataList) (*grpc.Server, func()) {
+	s := &ExecutionServiceServer{
+		manager: lro.New(),
+		logger:  logger,
+
+		resultsDirPath: resultRootDir,
+		tlwAddr:        tlwAddr,
+		metadata:       metadataList,
+	}
+
+	server := grpc.NewServer()
+	var conns []*grpc.ClientConn
+	closer := func() {
+		for _, conn := range conns {
+			conn.Close()
+		}
+		conns = nil
+	}
+	api.RegisterExecutionServiceServer(server, s)
+	longrunning.RegisterOperationsServer(server, s.manager)
+	logger.Println("crostestservice listen to request at ")
+	return server, closer
+}
+
+// RunTests calls the runTests flow in cros-test.
+func (s *ExecutionServiceServer) RunTests(ctx context.Context, req *api.CrosTestRequest) (*longrunning.Operation, error) {
+	op := s.manager.NewOperation()
+	s.logger.Println("Received api.CacheRequest: ", *req)
+
+	rspn, err := runTests(ctx, s.logger, s.resultsDirPath, s.tlwAddr, s.metadata, req)
+	if err != nil {
+		return op, errors.Annotate(err, "RunTests: failed to run tes").Err()
+	}
+	s.logger.Printf("Test RPC Command was successful")
+	// Note: We are setting the response on the LRO, rather than writing to a resultJson like CLI mode.
+	s.manager.SetResult(op.Name, rspn)
+	return op, nil
+}
