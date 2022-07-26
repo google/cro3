dd6ec86c7826fa9bda5c476c1a0f75bbbcb5cc3c
Nick Vaccaro <nvaccaro@google.com>
2022-07-25 22:34:51 -0700

    intel_sync_upstream: fix lint complaints
    BUG=None
    TEST=None
    
    Change-Id: Ia81b8e9c97c055f02ebef40c2fa4199c21e28295
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3787575
    Reviewed-by: Tarun Tuli <taruntuli@google.com>
    Commit-Queue: Nick Vaccaro <nvaccaro@google.com>
    Tested-by: Nick Vaccaro <nvaccaro@google.com>

--- contrib/firmware/intel_sync_upstream.sh
+++ contrib/firmware/intel_sync_upstream.sh
@@ -19,10 +19,10 @@ declare -A SOC_EDK_REPO_SUFFIX_MAP=( ["mtl"]="intelcollab" )
 
 function die()
 {
-  if [ $1 -ne 0 ]; then
+  if [ "$1" -ne 0 ]; then
     echo "Error: $1"
     echo "$2"
-    exit -42
+    exit 1
   fi;
 }
 
@@ -31,7 +31,7 @@ function usage()
   echo "Error: missing parameter."
   echo "Usage: $0 SOC [fsp|edk2|edk2-platforms|coreboot] version_string"
   echo "Example: $0 tgl fsp 'TGL.2527_17'"
-  exit -42
+  exit 1
 }
 
 # Verify param count
@@ -46,7 +46,7 @@ if [[ ! "${SOC_LIST[*]}" =~ ${SOC} ]]; then
   die 1 "SoC is not supported"
 fi
 
-case $DIR in
+case ${DIR} in
   fsp)
     if [ -v SOC_FSP_STAGING_REPO_MAP["${SOC}"] ]; then
         STAGING_NAME="${SOC_FSP_STAGING_REPO_MAP[${SOC}]}"
@@ -54,7 +54,7 @@ case $DIR in
         STAGING_NAME="${SOC}-staging"
     fi
     CHROMEOS_BRANCH=chromeos
-    SRC_DIR="${CHROMIUM_TOT_ROOT}/src/third_party/fsp/${SOC}/$DIR/$LOCAL_DIR"
+    SRC_DIR="${CHROMIUM_TOT_ROOT}/src/third_party/fsp/${SOC}/${DIR}/${LOCAL_DIR}"
     STAGING_REPO="https://chrome-internal.googlesource.com/chromeos/third_party/intel-fsp/${STAGING_NAME}"
     ;;
 
@@ -70,7 +70,7 @@ case $DIR in
     else
       CHROMEOS_BRANCH="chromeos-${SOC}-${LOCAL_DIR}"
     fi
-    SRC_DIR="${CHROMIUM_TOT_ROOT}/src/third_party/fsp/${SOC}/$DIR/$LOCAL_DIR"
+    SRC_DIR="${CHROMIUM_TOT_ROOT}/src/third_party/fsp/${SOC}/${DIR}/${LOCAL_DIR}"
     STAGING_REPO="https://chrome-internal.googlesource.com/chromeos/third_party/intel-fsp/${STAGING_NAME}"
     ;;
 
@@ -100,60 +100,57 @@ esac
 
 if [ -z "${CHROMIUM_TOT_ROOT}" ]; then
   die 1 "CHROMIUM_TOT_ROOT environment variable is not set"
-  exit -1
 fi
 
 # Clone the repo where the staging changes need to be pushed
-pushd "$SRC_DIR" > /dev/null
-die $? "Can't find $SRC_DIR"
+pushd "${SRC_DIR}" > /dev/null || die 1 "Can't find ${SRC_DIR}"
 
 # Add staging repo as remote repo to my local repo
-git remote add ${STAGING_NAME} "$STAGING_REPO"
+git remote add "${STAGING_NAME}" "${STAGING_REPO}"
 err=$?
 
 # If remote already exists, that's ok, but otherwise, exit on error
-if [ $err -ne 0 ] && [ $err -ne 3 ] && [ $err -ne 128 ]; then
-  die $err "Can't add remote ${STAGING_NAME}"
-elif [ $err -eq 0 ]; then
+if [ "${err}" -ne 0 ] && [ "${err}" -ne 3 ] && [ "${err}" -ne 128 ]; then
+  die "${err}" "Can't add remote ${STAGING_NAME}"
+elif [ "${err}" -eq 0 ]; then
   echo "Created remote ${STAGING_NAME}"
 else
   echo "Remote ${STAGING_NAME} already exists"
 fi;
 
-git fetch --tags --force ${STAGING_NAME}
+git fetch --tags --force "${STAGING_NAME}"
 die $? "Can't fetch ${STAGING_NAME}"
 
 # Detach from any branch before deleting
 git checkout --detach
 
 # Removing a stale branch
-git branch -D ${STAGING_NAME}-${VERSION}
+git branch -D "${STAGING_NAME}"-"${VERSION}"
 
 # Set up remote branch
-git checkout ${UPREV_BRANCH} -b ${STAGING_NAME}-${VERSION}
+git checkout "${UPREV_BRANCH}" -b "${STAGING_NAME}"-"${VERSION}"
 die $? "Can't checkout upstream/${VERSION}"
 echo "Checked out upstream/${VERSION} to branch ${STAGING_NAME}-${VERSION}"
 
 echo "Pushing to a staging repo to avoid 'forge commiter' permission issues"
-git push -o skip-validation -o nokeycheck cros-internal HEAD:refs/heads/staging/${STAGING_PREFIX}-${VERSION}
+git push -o skip-validation -o nokeycheck cros-internal HEAD:refs/heads/staging/"${STAGING_PREFIX}"-"${VERSION}"
 die $? "Could not push to a staging repo"
 
 # Checkout a local branch from remotes/cros-internal/${CHROMEOS_BRANCH}
 git branch -D chrome-internal-tot
-git checkout -b chrome-internal-tot cros-internal/${CHROMEOS_BRANCH}
+git checkout -b chrome-internal-tot cros-internal/"${CHROMEOS_BRANCH}"
 die $? "Error checking out cros-internal/${CHROMEOS_BRANCH}"
 echo "Checked out cros-internal/${CHROMEOS_BRANCH} to branch chrome-internal-tot"
 
 # Merge from staging branch
-git merge ${STAGING_NAME}-${VERSION} --strategy-option theirs --no-ff --log
-if [ $? -ne 0 ]; then
+if git merge "${STAGING_NAME}"-"${VERSION}" --strategy-option theirs --no-ff --log; then
   echo "Didn't merge cleanly to ${STAGING_NAME}-${VERSION}"
 
   while true
   do
     read -r -p "Do you want to force sync to the tag? [y/n] >" input
 
-    case $input in
+    case ${input} in
       [yY][eE][sS]|[yY])
         echo "Force syncing"
         # Remove all the changes that were not in the tag
@@ -165,7 +162,7 @@ if [ $? -ne 0 ]; then
         # No signoff used in FSP repo
         git commit
 
-        git diff -a chrome-internal-tot..${STAGING_NAME}-${VERSION} > /tmp/merge-to-tag.patch
+        git diff -a chrome-internal-tot.."${STAGING_NAME}"-"${VERSION}" > /tmp/merge-to-tag.patch
         patch -p1 < /tmp/merge-to-tag.patch
         rm /tmp/merge-to-tag.patch
         git add .
@@ -193,9 +190,9 @@ while true
 do
   read -r -p "Do you want to push your changes to cros-internal? [y/n] >" input
 
-  case $input in
+  case ${input} in
     [yY][eE][sS]|[yY])
-      git push cros-internal HEAD:refs/for/${CHROMEOS_BRANCH}
+      git push cros-internal HEAD:refs/for/"${CHROMEOS_BRANCH}"
       echo "Pushed merge of ${STAGING_NAME}-${VERSION}, ready for review."
       break
       ;;
