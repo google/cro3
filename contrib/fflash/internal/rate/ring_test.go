9cc20a474f98d69bdb988c8e3e676fa54079a12a
Li-Yu Yu <aaronyu@google.com>
2022-08-03 00:12:33 +0800

    fflash: Use quicktest for unit tests
    BUG=None
    TEST=go test ./...
    
    Change-Id: Ic6ffed28cc99b7a9f6f865d2ef5072eea41b0e56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3806880
    Commit-Queue: Li-Yu Yu <aaronyu@google.com>
    Tested-by: Li-Yu Yu <aaronyu@google.com>
    Reviewed-by: Terry Cheong <htcheong@chromium.org>

--- contrib/fflash/internal/rate/ring_test.go
+++ contrib/fflash/internal/rate/ring_test.go
@@ -6,6 +6,8 @@ package rate
 
 import (
 	"testing"
+
+	qt "github.com/frankban/quicktest"
 )
 
 func assertFrontBack[T comparable](t *testing.T, r *ring[T], front, back T) {
@@ -29,65 +31,57 @@ func assertEmpty[T comparable](t *testing.T, r *ring[T], empty bool) {
 
 func TestRing(t *testing.T) {
 	r := newRing[int](4)
-	assertEmpty(t, r, true)
+	c := qt.New(t)
+	c.Assert(r.Empty(), qt.Equals, true)
 
 	r.Push(1)
-	assertFrontBack(t, r, 1, 1)
-	assertEmpty(t, r, false)
+	c.Assert(r.Front(), qt.Equals, 1)
+	c.Assert(r.Back(), qt.Equals, 1)
+	c.Assert(r.Empty(), qt.Equals, false)
 
 	r.Push(2)
-	assertFrontBack(t, r, 1, 2)
-	assertEmpty(t, r, false)
+	c.Assert(r.Front(), qt.Equals, 1)
+	c.Assert(r.Back(), qt.Equals, 2)
+	c.Assert(r.Empty(), qt.Equals, false)
 
 	r.Push(3)
-	assertFrontBack(t, r, 1, 3)
-	assertEmpty(t, r, false)
+	c.Assert(r.Front(), qt.Equals, 1)
+	c.Assert(r.Back(), qt.Equals, 3)
+	c.Assert(r.Empty(), qt.Equals, false)
 
 	r.Push(4)
-	assertFrontBack(t, r, 1, 4)
-	assertEmpty(t, r, false)
+	c.Assert(r.Front(), qt.Equals, 1)
+	c.Assert(r.Back(), qt.Equals, 4)
+	c.Assert(r.Empty(), qt.Equals, false)
 
 	r.Push(5)
-	assertFrontBack(t, r, 2, 5)
-	assertEmpty(t, r, false)
+	c.Assert(r.Front(), qt.Equals, 2)
+	c.Assert(r.Back(), qt.Equals, 5)
+	c.Assert(r.Empty(), qt.Equals, false)
 
 	r.Push(6)
-	assertFrontBack(t, r, 3, 6)
-	assertEmpty(t, r, false)
+	c.Assert(r.Front(), qt.Equals, 3)
+	c.Assert(r.Back(), qt.Equals, 6)
+	c.Assert(r.Empty(), qt.Equals, false)
 
 	r.Push(7)
-	assertFrontBack(t, r, 4, 7)
-	assertEmpty(t, r, false)
+	c.Assert(r.Front(), qt.Equals, 4)
+	c.Assert(r.Back(), qt.Equals, 7)
+	c.Assert(r.Empty(), qt.Equals, false)
 
 	r.Push(8)
-	assertFrontBack(t, r, 5, 8)
-	assertEmpty(t, r, false)
+	c.Assert(r.Front(), qt.Equals, 5)
+	c.Assert(r.Back(), qt.Equals, 8)
+	c.Assert(r.Empty(), qt.Equals, false)
 
 	r.Push(9)
-	assertFrontBack(t, r, 6, 9)
-	assertEmpty(t, r, false)
+	c.Assert(r.Front(), qt.Equals, 6)
+	c.Assert(r.Back(), qt.Equals, 9)
+	c.Assert(r.Empty(), qt.Equals, false)
 }
 
-func TestEmptyRingBackPanic(t *testing.T) {
+func TestEmptyRingPanic(t *testing.T) {
 	empty := newRing[int](1)
-
-	defer func() {
-		if r := recover(); r == nil {
-			t.Errorf("Did not panic with empty.Back()")
-		}
-	}()
-
-	empty.Back()
-}
-
-func TestEmptyRingFrontPanic(t *testing.T) {
-	empty := newRing[int](1)
-
-	defer func() {
-		if r := recover(); r == nil {
-			t.Errorf("Did not panic with empty.Front()")
-		}
-	}()
-
-	empty.Front()
+	qt.Assert(t, func() { empty.Back() }, qt.PanicMatches, ".*")
+	qt.Assert(t, func() { empty.Front() }, qt.PanicMatches, ".*")
 }
