0335f8917df584aea78827a02c9c81e61f234158
Clayton Whitelaw <cawhitelaw@google.com>
2022-08-10 12:02:17 -0600

    device_summary: Add script to report a quick DUT summary
    This runs directly on a DUT and reports some hardware characteristics,
    hardware identifiers, and some stateful information like chrome OS
    version.
    
    Detects and reports an error when not being run on a DUT.
    
    BUG=b:239211050
    TEST=run from host directly outside chroot
    TEST=run from host directly inside chroot
    TEST=run via ssh on DUT remotely
    
    Change-Id: Ib54c4e769db374ed29fe39c4ef6df76cedb60792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3825039
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Auto-Submit: Clayton Whitelaw <cawhitelaw@google.com>
    Tested-by: Clayton Whitelaw <cawhitelaw@google.com>

---
+++ contrib/device_summary.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Copyright 2022 The ChromiumOS Authors.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Script run from a DUT to report broad summary of characteristics and state
+# Example usage: ssh $DUT <device_summary.sh
+
+# TODO(b/242093024) combine with get_device_info.sh
+
+function keyval {
+  # Pads keys with whitespace to align output
+  KEY_FIELD_LENGTH=20
+  printf "%-${KEY_FIELD_LENGTH}s: %s\n" "$1" "$2"
+}
+
+function usage-error {
+  2>&1 echo "This device doesn't appear to be a ChromeOS DUT."
+  2>&1 echo "Run this script on the DUT, not on the host."
+  2>&1 echo "To execute remotely, try 'ssh \$DUT <${0}'"
+  exit 1
+}
+
+# Detect host machine inside chroot
+if [ -f /etc/cros_chroot_version ]; then
+  usage-error
+fi
+
+BOARD="$(grep CHROMEOS_RELEASE_BOARD </etc/lsb-release)"
+# Detect host machine outside chroot
+if [ -z "${BOARD}" ]; then
+  usage-error
+fi
+keyval "Board" "${BOARD:23}"
+
+MODEL="$(cros_config / name)"
+keyval "Model" "${MODEL}"
+
+OS="$(grep CHROMEOS_RELEASE_VERSION </etc/lsb-release)"
+MILESTONE="$(grep CHROMEOS_RELEASE_CHROME_MILESTONE </etc/lsb-release)"
+keyval "ChromeOS" "${OS:25} (M${MILESTONE:34})"
+
+KERNEL="$(uname -r)"
+keyval "Kernel" "${KERNEL}"
+
+FW_ACTIVE="$(crossystem fwid)"
+keyval "Firmware (active)" "${FW_ACTIVE}"
+FW_RO="$(crossystem ro_fwid)"
+keyval "Firmware (read-only)" "${FW_RO}"
+
+HWID="$(crossystem hwid)"
+keyval "HWID" "${HWID}"
+
+SERIAL="$(vpd -g serial_number)"
+keyval "Serial" "${SERIAL}"
+
+SKU= # appease shellcheck
+eval "$(crosid)" # sets SKU
+keyval "Firmware SKU ID" "${SKU}"
+
+# Features that might vary by SKU
+
+CPU_MODEL="$(grep "^model name" /proc/cpuinfo | head -n1)"
+keyval "CPU" "${CPU_MODEL:13}"
+
+MEM_KB="$(grep '^MemTotal:' /proc/meminfo  | awk '{ print $2 }')"
+keyval "Memory" "$((MEM_KB / 1024))M"
