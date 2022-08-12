60b741c5eb22f0c335768903c009c85a4c7ccdb7
Łukasz Bartosik <ukaszb@google.com>
2022-08-12 15:02:24 +0200

    kernel-rebase: use build helpers from buildhelper.py
    Use build helpers from buildhelper.py
    
    BUG=b:220303520
    TEST=Run kernel continuous rebase triage
    
    Change-Id: I14575eaca4c1d7c27d8633daa90d526b488c82ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3829687
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Commit-Queue: Łukasz Bartosik <ukaszb@google.com>
    Tested-by: Łukasz Bartosik <ukaszb@google.com>

--- contrib/kernel-rebase/rebase.py
+++ contrib/kernel-rebase/rebase.py
@@ -26,15 +26,14 @@ See go/cont-rebase for details
 
 from datetime import datetime
 import importlib
-import multiprocessing
-from multiprocessing import Manager
 import os
 import pickle
 import re
 import sqlite3
 import sys
 
-from common import executor_io
+from buildhelpers import do_on_cros_sdk
+from buildhelpers import verify_build
 from common import rebasedb
 from githelpers import *
 from logging_console import LoggingConsole
@@ -58,53 +57,6 @@ def call_hook(sha, hook_type):
         if hook_type in entry['types']:
             hook = entry['hook']
             hook(sha, hook_type)
-
-def do_on_cros_sdk_impl(command, ret_by_arg=None):
-    result = {'exit_code': None, 'output': None, 'error_line': None}
-    os.system("echo '" + command + "' > " + executor_io + '/commands &')
-    os.system('cat ' + executor_io + '/output > output.log')
-    try:
-        with open(executor_io + '/last_exit') as last_exit:
-            ec = last_exit.read()
-        result['exit_code'] = int(ec[:-1])
-    except: # pylint: disable=bare-except
-        print('failed to read a valid exit code from last_exit')
-        return {}
-    try:
-        with open('output.log') as output:
-            result['output'] = output.read()
-        lines = result['output'].splitlines()
-        for n in range(len(lines)): # pylint: disable=C0200
-            if 'Error 1' in lines[n]:
-                result['error_line'] = n + 1
-                break
-    except: # pylint: disable=bare-except
-        print('failed to read output.log')
-    if ret_by_arg is not None:
-        for k, v in result.items():
-            ret_by_arg[k] = v
-    return result
-
-
-def do_on_cros_sdk(command, timeout_s=None):
-    if timeout_s is not None:
-        manager = Manager()
-        result = {}
-        shared_dict = manager.dict()
-        p = multiprocessing.Process(
-            target=do_on_cros_sdk_impl, args=(
-                command, shared_dict,))
-        p.start()
-        p.join(timeout_s)
-        if p.is_alive():
-            print('execution timed out, is executor.sh running in cros_sdk?')
-            p.terminate()
-            p.join()
-        else:
-            for k, v in shared_dict.items():
-                result[k] = v
-        return result
-    return do_on_cros_sdk_impl(command)
 
 def normalize():
     kernelupstream_branch = branch_name('kernelupstream', rebase_target, None)
@@ -201,16 +153,6 @@ def normalize():
         if ec != 0:
             print('exit code:', ec)
             return
-
-def verify_build(sha):
-    assert not is_dirty(
-        'kernel-upstream'), "There's a local diff in kernel repo. Clean it to continue."
-    if sha is not None:
-        checkout('kernel-upstream', sha)
-    return do_on_cros_sdk(
-        'emerge-' +
-        rebase_config.verify_board +
-        ' --color n -B ' + rebase_config.verify_package)
 
 class Rebaser:
     """Keeps all automatic rebase data"""
