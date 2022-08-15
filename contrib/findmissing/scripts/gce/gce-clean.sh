d6eb5c4fc056c9d582ed402438d4ecffbf1d77f0
Tzung-Bi Shih <tzungbi@chromium.org>
2022-08-15 08:04:49 +0000

    findmissing: prune the git repo if gc.log exists
    To fix:
    warning: There are too many unreachable loose objects; run 'git prune'
    to remove them.
    
    Assuming the git repo needs a prune when gc.log exists.  Even though it
    is not always the case.
    
    BUG=b:240879180
    TEST=./findmissing/scripts/gce/gce-clean.sh
    
    Change-Id: I539ea2d93a862db9b3495d08e568913ab64e686a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3812914
    Tested-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Commit-Queue: Tzung-Bi Shih <tzungbi@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>

---
+++ contrib/findmissing/scripts/gce/gce-clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Copyright 2022 The ChromiumOS Authors.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+KERNEL_REPOS_DIR="${HOME}/findmissing_workspace/kernel_repositories"
+LINUX_CHROME="${KERNEL_REPOS_DIR}/linux_chrome"
+LOG_FILE="/var/log/findmissing/findmissing.log"
+
+{
+	echo "Triggered clean at $(date)"
+	git -C "${LINUX_CHROME}" prune --expire=now --progress
+	rm -f "${LINUX_CHROME}/.git/gc.log"
+	echo -e "\n"
+} >> "${LOG_FILE}" 2>&1
