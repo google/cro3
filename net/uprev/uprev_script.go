0441d7e272784577244e0f7c0ddcfdb25595ca1e
Pavan Holla <pholla@chromium.org>
2022-08-26 02:29:27 +0000

    dev/net/uprev: Remove dependency on imerge, add ability to compile
    imerge can be difficult to understand, and failures can be hard to
    debug. Use git merge/ git merge --abort to perform a non conflicting
    merge.
    
    Add flag to compile on dedede and trogdor.
    
    Add flag to merge until next conflict on current repo.
    
    BUG=None
    TEST=go run ~/trunk/src/platform/dev/net/uprev/uprev_script.go
    
    Change-Id: Ie1a418e94aef8a9e51ddc58b1680fde66c56d94d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3862403
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Commit-Queue: Pavan Holla <pholla@google.com>
    Tested-by: Pavan Holla <pholla@google.com>

--- net/uprev/uprev_script.go
+++ net/uprev/uprev_script.go
@@ -4,7 +4,6 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -16,11 +15,12 @@ import (
 )
 
 var colorGreen = "\033[32m"
+var colorRed = "\033[31m"
 var colorReset = "\033[0m"
 
-func logFatal(s string) {
+func logPanic(s string) {
 	fmt.Print(s)
-	log.Fatal(s)
+	log.Panicln(s)
 }
 
 func runCmd(cmd *exec.Cmd, abortOnFail bool) (string, error) {
@@ -30,21 +30,54 @@ func runCmd(cmd *exec.Cmd, abortOnFail bool) (string, error) {
 	if err != nil {
 		log.Printf("err: %q\n", err)
 		if abortOnFail {
-			logFatal(err.Error())
+			logPanic(err.Error())
 		}
 		log.Printf("Ignoring non-fatal error")
 	}
 	log.Printf("Execution result: %s\n", string(out))
-	return string(out), nil
+	return string(out), err
 }
 
-// returns gerrit cl, conflicting commit on error, error
-func uprevRepo(rootDir string, repoName string, upstreamBranch string, cqDepend string, lastUpstreamCommitToMerge string, uploadFlag bool, forceFlag bool) (string, string, error) {
+// mergeUntilConflict merges the last non conflicting merge from the upstream branch
+func MergeUntilConflict(upstreamBranch string) (string, error) {
+	cmd := exec.Command("git", "log", "HEAD.."+upstreamBranch, "--abbrev", "--oneline", "--format=%h")
+	out, _ := runCmd(cmd, true)
+	if out == "" {
+		return "", nil
+	}
+
+	commits := strings.Split(out, "\n")
+	// The first element of this array is the most recent commit, and we're going backwards in time until we find one that merges cleanly
+	for i, commitSHA := range commits[:len(commits)-1] {
+		cmd := exec.Command("git", "merge", commitSHA)
+		_, e := runCmd(cmd, false)
+		if e != nil {
+			cmd := exec.Command("git", "merge", "--abort")
+			if _, err := runCmd(cmd, false); err != nil {
+				return "", fmt.Errorf("Could not abort merge: %w", e)
+			}
+		} else {
+			if i != 0 {
+				fmt.Println(colorRed, "Could not merge cros/upstream fully. Merged only until commit: ", commitSHA, colorReset)
+				fmt.Println(colorRed, "Warning: CQ does not support merge commits stacked on top of other commits. Your next commit needs to be a merge of ", commitSHA, "without any commits in between.", colorReset)
+			} else {
+				fmt.Println(colorGreen, "Merged ", upstreamBranch, colorReset)
+			}
+			return fmt.Sprintf(commitSHA), nil
+		}
+	}
+	fmt.Println(colorRed, "Could not merge any commit in cros/upstream", colorReset)
+	return "", nil
+}
+
+// returns gerrit cl, cleanup function
+func uprevRepo(rootDir string, repoName string, upstreamBranch string, cqDepend string, uploadFlag bool, compileFlag bool, forceFlag bool, cqP1Flag bool) (string, func()) {
+	cleanupFunc := func() {}
 	uprevDate := time.Now().Format("01-02-2006")
 	branchName := "merge-upstream-" + uprevDate
 	fmt.Println(rootDir, repoName, upstreamBranch, uprevDate)
 	if err := os.Chdir(rootDir + repoName); err != nil {
-		logFatal(err.Error())
+		logPanic(err.Error())
 	}
 	newDir, _ := os.Getwd()
 	fmt.Printf(colorGreen+"Working on : %s\n"+colorReset, newDir)
@@ -52,12 +85,13 @@ func uprevRepo(rootDir string, repoName string, upstreamBranch string, cqDepend 
 
 	cmd = exec.Command("git", "status", "--porcelain")
 	gitStatus, _ := runCmd(cmd, true)
-	if gitStatus != "" && !forceFlag {
-		logFatal("Please ensure that there are no uncommitted changes. Or run with --force=true")
-	}
 	if gitStatus != "" {
-		cmd = exec.Command("git", "stash", "cros/master")
-		runCmd(cmd, false)
+		if forceFlag {
+			cmd = exec.Command("git", "stash", "--include-untracked")
+			runCmd(cmd, false)
+		} else {
+			logPanic("Please ensure that there are no uncommitted changes. Or run with --force=true")
+		}
 	}
 
 	// Repo start a clean branch with HEAD cros/master and merge cros/upstream
@@ -65,137 +99,119 @@ func uprevRepo(rootDir string, repoName string, upstreamBranch string, cqDepend 
 	runCmd(cmd, !forceFlag)
 	cmd = exec.Command("git", "reset", "--hard", "cros/master")
 	runCmd(cmd, true)
-	cmd = exec.Command("git", "branch", "-D", branchName)
-	runCmd(cmd, false)
-	cmd = exec.Command("rm", "-r", "-f", ".git/refs/imerge")
-	runCmd(cmd, false)
-	cmd = exec.Command("rm", "-r", "-f", "/tmp/imerge-"+repoName)
+	if forceFlag {
+		cmd = exec.Command("git", "branch", "-D", branchName)
+	}
 	runCmd(cmd, false)
 	cmd = exec.Command("repo", "sync", ".")
 	runCmd(cmd, true)
 	cmd = exec.Command("repo", "start", branchName)
 	runCmd(cmd, true)
-	cmd = exec.Command("git", "checkout", "cros/master")
-	runCmd(cmd, true)
 	cmd = exec.Command("git", "fetch", "cros", upstreamBranch)
 	runCmd(cmd, true)
-
-	// Try to merge top of cros/upstream unless specified otherwise.
-	mergeTarget := ""
-	if lastUpstreamCommitToMerge == "" {
-		mergeTarget = "cros/" + upstreamBranch
-	} else {
-		mergeTarget = lastUpstreamCommitToMerge + "~1"
-	}
-
-	// git imerge performs an incremental merge. It merges one upstream commit at a time to figure out the conflicting commit.
-	cmd = exec.Command("git", "imerge", "start", "--name="+branchName, "--branch="+branchName, "--goal=merge", mergeTarget)
-	imergeResult, _ := runCmd(cmd, false)
-
-	if strings.Contains(imergeResult, "There are no commits") {
-		return "", "", nil
-	}
-
-	if _, err := os.Stat(".git/MERGE_HEAD"); errors.Is(err, os.ErrNotExist) {
-		// merge was successful
-		cmd = exec.Command("git", "imerge", "finish")
-		cmd.Stdin = strings.NewReader("\r\r\r\r:q")
-		runCmd(cmd, false) // the finish will fail because we cannot open an editor, but continue nevertheless
-		cmd = exec.Command("rm", "-r", "-f", ".git/refs/imerge")
-		runCmd(cmd, false)
-		lastCommitMerged := ""
-		if lastUpstreamCommitToMerge == "" {
-			lastCommitMerged = "cros/" + upstreamBranch
-		} else {
-			lastCommitMerged = lastUpstreamCommitToMerge + "~1"
-		}
-
-		cmd = exec.Command("git", "log", "cros/master.."+lastCommitMerged, "--abbrev", "--oneline", "--format=\"%C(auto) %h %s (%an)\"")
-		out, _ := runCmd(cmd, false)
-		commitMsg := "Merge cros/upstream to cros/master\n\nContains the following commits:\n\n" + out + "\n\nBUG=None\nTEST=None"
-		if cqDepend != "" {
-			commitMsg = commitMsg + "\n\nCq-Depend: " + cqDepend
-		}
-		if err := os.WriteFile("/tmp/imerge-commits.log", []byte(commitMsg), 0644); err != nil {
-			logFatal("Cannot write commits.log")
-		}
-		cmd = exec.Command("git", "commit", "--amend", "-F", "/tmp/imerge-commits.log")
-		runCmd(cmd, true)
-		// imerge sets upstream to cros/upstream. We want to upload changes to gerrit
-		cmd = exec.Command("git", "branch", "--set-upstream-to", "cros/master")
-		runCmd(cmd, true)
-
-		if !uploadFlag {
-			return "", "", nil
-		}
-		cmd = exec.Command("repo", "upload", "--cbr", ".", "--no-verify", "-o", "topic="+branchName, "-y")
-		cmd.Stdin = strings.NewReader("yes")
-		out, err = runCmd(cmd, true)
-		re := regexp.MustCompile(`\+/(.*) Merge`)
-		res := re.FindStringSubmatch(out)
-		fmt.Printf("%sUploaded chromium:%s\n%s", colorGreen, res[1], colorReset)
-		return "chromium:" + res[1] + " ", "", nil
-	}
-
-	re := regexp.MustCompile(`commit (.{40})`)
-	res := re.FindAllStringSubmatch(imergeResult, -1)
-	fmt.Printf("Merge conflict found. Email at /tmp/imerge-conflict.email")
-	imergeResult = "Please cherry pick " + res[1][1] + "\n\n\n\n Details:\n\n" + imergeResult
-	if err := os.WriteFile("/tmp/imerge-conflict.email", []byte(strings.Split(imergeResult, "Attempting automerge")[0]), 0644); err != nil {
-		logFatal("Could not write conflict email")
-	}
-	// return conflicting commit
-	return "", res[1][1], errors.New("Conflict found")
-}
-
-func postUprevChecks(cl string, err error, cqP1Flag bool) {
+	commitSHA, err := MergeUntilConflict("cros/" + upstreamBranch)
 	if err != nil {
-		os.Exit(1)
+		logPanic(fmt.Errorf("Could not merge: %w", err).Error())
 	}
-	if cl == "" || !cqP1Flag {
-		return
+	if commitSHA == "" {
+		fmt.Println(colorGreen, repoName, " is already up to date", colorReset)
+		return "", cleanupFunc
 	}
-	cl = strings.TrimRight(strings.Split(cl, "chromium:")[1], " ") // Convert chromium: 12345 -> 12345
-	runCmd(exec.Command("gerrit", "label-v", cl, "1"), false)
-	runCmd(exec.Command("gerrit", "label-cq", cl, "1"), false)
+
+	cmd = exec.Command("git", "log", "cros/master.."+commitSHA, "--abbrev", "--oneline", "--format=\"%C(auto) %h %s (%an)\"")
+	out, _ := runCmd(cmd, false)
+	commitMsg := "Merge cros/" + upstreamBranch + " to cros/master\n\nContains the following commits:\n\n" + out + "\n\nBUG=None\nTEST=None"
+	if cqDepend != "" {
+		commitMsg = commitMsg + "\n\nCq-Depend: " + cqDepend
+	}
+	if err := os.WriteFile("/tmp/commit-msg.log", []byte(commitMsg), 0644); err != nil {
+		logPanic("Cannot write commits.log")
+	}
+	cmd = exec.Command("git", "commit", "--amend", "-F", "/tmp/commit-msg.log")
+	runCmd(cmd, true)
+
+	if compileFlag {
+		cmd = exec.Command("cros_workon", "--board=trogdor", "start", repoName)
+		cmd = exec.Command("cros_workon", "--board=dedede", "start", repoName)
+		runCmd(cmd, true)
+		cleanupFunc = func() {
+			cmd = exec.Command("cros_workon", "--board=trogdor", "stop", repoName)
+			runCmd(cmd, false)
+			cmd = exec.Command("cros_workon", "--board=dedede", "stop", repoName)
+			runCmd(cmd, false)
+		}
+		cmd = exec.Command("emerge-trogdor", repoName)
+		runCmd(cmd, true)
+		cmd = exec.Command("emerge-dedede", repoName)
+		runCmd(cmd, true)
+	}
+
+	if !uploadFlag {
+		return "", cleanupFunc
+	}
+	cmd = exec.Command("repo", "upload", "--cbr", ".", "--no-verify", "-o", "topic="+branchName, "-y")
+	cmd.Stdin = strings.NewReader("yes")
+	out, err = runCmd(cmd, true)
+	re := regexp.MustCompile(`\+/(.*) Merge`)
+	res := re.FindStringSubmatch(out)
+	fmt.Printf("%sUploaded crrev.com/c/%s\n%s", colorGreen, res[1], colorReset)
+
+	if cqP1Flag {
+		runCmd(exec.Command("gerrit", "label-v", res[1], "1"), false)
+		runCmd(exec.Command("gerrit", "label-cq", res[1], "1"), false)
+	}
+
+	return "chromium:" + res[1] + " ", cleanupFunc
 }
 
 func main() {
 	file, err := os.OpenFile("/tmp/uprev.log", os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
-		logFatal(err.Error())
+		logPanic(err.Error())
 	}
 	log.SetOutput(file)
 	defer fmt.Println("Uprev logs in /tmp/uprev.log")
 
-	homeDir, _ := os.UserHomeDir()
-	_, err = os.Stat(homeDir + "/git-imerge/")
-	if os.IsNotExist(err) {
-		logFatal("~/git-imerge does not exist. Please run \n  cd ~ && git clone https://github.com/mhagger/git-imerge.git && cd git-imerge && git checkout 9bde208 && sudo python setup.py install")
-	}
-
 	uploadFlag := flag.Bool("upload", true, "upload to gerrit")
 	cqP1Flag := flag.Bool("cq", true, "V+1, CQ+1 on gerrit")
 	forceFlag := flag.Bool("force", false, "force a reset to cros/master. You will lose all changes on the current branch. Prefer stashing your changes instead")
+	compileFlag := flag.Bool("compile", false, "Compile on trogdor and dedede boards")
+	skipSetupBoardForCompileFlag := flag.Bool("skip-setup-board-for-compile", false, "If --compile is true, skip executing setup_board. You will have to ensure that it's already been run before.")
+	mergeUntilConflictFlag := flag.Bool("merge-until-conflict", false, "Merge the last non conflicting commit between current branch and merge-until-conflict-branch.")
+	mergeUntilConflictBranchFlag := flag.String("merge-until-conflict-branch", "cros/upstream", "Upstream branch to merge when merge-until-conflict is set")
 	flag.Parse()
 
+	if *mergeUntilConflictFlag {
+		commitSHA, err := MergeUntilConflict(*mergeUntilConflictBranchFlag)
+		if err != nil {
+			logPanic(err.Error())
+			return
+		}
+		fmt.Println(commitSHA, "is the last commit that can be merged without conflicts")
+		return
+	}
+
+	if *compileFlag && !*skipSetupBoardForCompileFlag {
+		cmd := exec.Command("setup_board", "--board=trogdor")
+		runCmd(cmd, true)
+		cmd = exec.Command("setup_board", "--board=dedede")
+		runCmd(cmd, true)
+	}
+
+	homeDir, _ := os.UserHomeDir()
 	rootDir := homeDir + "/chromiumos/src/third_party/"
+	cqDepend := ""
+	libqrtrCl, cleanupLibqrtr := uprevRepo(rootDir, "libqrtr-glib", "upstream/main", cqDepend, *uploadFlag, *compileFlag, *forceFlag, *cqP1Flag)
+	defer cleanupLibqrtr()
 
-	libqrtrCl, _, err := uprevRepo(rootDir, "libqrtr-glib", "upstream/main", "", "", *uploadFlag, *forceFlag)
-	postUprevChecks(libqrtrCl, err, *cqP1Flag)
-	libqmiCl, _, err := uprevRepo(rootDir, "libqmi", "upstream", "", "", *uploadFlag, *forceFlag)
-	postUprevChecks(libqmiCl, err, *cqP1Flag)
+	libqmiCl, cleanupLibqmi := uprevRepo(rootDir, "libqmi", "upstream", cqDepend, *uploadFlag, *compileFlag, *forceFlag, *cqP1Flag)
+	defer cleanupLibqmi()
 
-	libmbimCl, _, err := uprevRepo(rootDir, "libmbim", "upstream", "", "", *uploadFlag, *forceFlag)
-	postUprevChecks(libmbimCl, err, *cqP1Flag)
+	libmbimCl, cleanupLibmbim := uprevRepo(rootDir, "libmbim", "upstream", cqDepend, *uploadFlag, *compileFlag, *forceFlag, *cqP1Flag)
+	defer cleanupLibmbim()
 
-	// Attempt MM uprev to cros/upstream. If MM uprev fails, merge last conflict free commit.
-	mmCl, conflictCommit, err := uprevRepo(rootDir, "modemmanager-next", "upstream", libqrtrCl+libqmiCl+libmbimCl, "", *uploadFlag, *forceFlag)
-	if err == nil {
-		runCmd(exec.Command("rm", "/tmp/imerge*"), false)
-	}
-	if conflictCommit != "" {
-		mmCl, conflictCommit, err = uprevRepo(rootDir, "modemmanager-next", "upstream", libqrtrCl+libqmiCl+libmbimCl, conflictCommit, *uploadFlag, *forceFlag)
-	}
-	postUprevChecks(mmCl, err, *cqP1Flag)
+	cqDepend = libqrtrCl + libqmiCl + libmbimCl
+	cqDepend = strings.ReplaceAll(strings.TrimSpace(cqDepend), " ", ",")
+	_, cleanupMM := uprevRepo(rootDir, "modemmanager-next", "upstream", cqDepend, *uploadFlag, *compileFlag, *forceFlag, *cqP1Flag)
+	defer cleanupMM()
 }
