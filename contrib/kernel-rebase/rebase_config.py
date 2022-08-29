9d624dec29cd42217e904c11e8e9ac301970ff76
Lukasz Majczak <lmajczak@google.com>
2022-08-29 10:22:30 +0200

    kernel-rebase: add dependency for media topic branch v6.0-rc3
    BUG=None
    TEST=Run continuous kernel rebase
    
    Change-Id: Ib97a89813e91b637f15026f9046f8a5a9c48388f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3859620
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Commit-Queue: Lukasz Majczak <lmajczak@google.com>
    Tested-by: Lukasz Majczak <lmajczak@google.com>

--- contrib/kernel-rebase/rebase_config.py
+++ contrib/kernel-rebase/rebase_config.py
@@ -117,6 +117,7 @@ merge_fixups = [
 # }
 patch_deps = {
       '4638c45ac4c3': ['be850241385f'],
+      'bb10d4565c7d': ['5df238f1926b', '7212dd43f055'],
 }
 
 # Add entry here to overwrite default disposition on particular commit
