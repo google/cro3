d6a49e331fa7236343c35f9cbfc54c7b364375f7
Robert Zieba <robertzieba@google.com>
2022-08-29 09:47:49 -0600

    pacman: Add script to generate pacman configs from servod configs
    This commit adds the `gen_pacman_config.py` script that generates pacman
    configs from servod configs.
    
    BUG=b:243553954
    TEST=Generated and tested skyrim config from servod config
    
    Change-Id: Ie4a3ab79b7e5c02ecbccd3b1ce5dcf2e645315ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3862405
    Auto-Submit: Robert Zieba <robertzieba@google.com>
    Commit-Queue: Parker Holloway <jparkerh@google.com>
    Tested-by: Robert Zieba <robertzieba@google.com>
    Reviewed-by: Parker Holloway <jparkerh@google.com>

---
+++ contrib/power_measurement/pacman/gen_pacman_config.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright 2022 The ChromiumOS Authors.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Utility to convert servod configs to pacman configs"""
+
+import argparse
+import importlib.util
+import pathlib
+import sys
+
+
+def main():
+    """main function"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-o',
+        '--output',
+        type=pathlib.Path
+    )
+    parser.add_argument(
+        'input',
+        help='Input servod python config file',
+        type=pathlib.Path
+    )
+
+    args = parser.parse_args()
+
+    spec = importlib.util.spec_from_file_location('config', args.input)
+    config = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(config)
+
+    with open(args.output, 'w') as f:
+        f.write('# Generated from servod board config\n')
+        f.write('# addr:ch   name             nom    rsense\n')
+        f.write('PACS = [\n')
+
+        rails = []
+        for (_, addr, name, nom, rsense, _, _) in config.inas:
+            addrs = addr.split(':')
+            if len(addrs) != 2:
+                print(f'{addr} does not match addr:channel format',
+                    file=sys.stderr)
+                sys.exit(1)
+
+            addr = addrs[0]
+            channel = addrs[1]
+
+            rails.append(name)
+            f.write((f"    ('{addr}:{channel}', "
+                f"'{name.upper()}', {nom}, {rsense}),\n"))
+
+        f.write(']\n')
+
+        # Servod config files don't contain this info, so fill in what we can.
+        f.write('# rail             parent\n')
+        f.write('RAILS = [\n')
+        for rail in rails:
+            f.write(f"    ('{rail.upper()}', 'NA'),\n")
+        f.write(']\n')
+        f.write('GPIOS = []\n')
+
+
+if __name__ == '__main__':
+    main()
