eb2bc98fc7d02389ab0996649131391f5acdbd25
Fei Shao <fshao@chromium.org>
2022-09-08 12:29:37 +0800

    common: More message types with colorized text
    Extend the message types and print colorized text for readability.
    Add some color to the tedious life.
    
    BUG=None
    TEST=use the loggers in scripts
    
    Change-Id: Iceb3f0a8502f3962c92d02a92561cea58cfde7ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3882421
    Commit-Queue: Fei Shao <fshao@chromium.org>
    Tested-by: Fei Shao <fshao@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>

--- contrib/common.sh
+++ contrib/common.sh
@@ -5,9 +5,33 @@
 
 # Random helper utilities that scripts might want.
 
+# Set up variables for text color output.
+C_BOLD_RED=
+C_BOLD_GREEN=
+C_BOLD_YELLOW=
+C_OFF=
+
+# Only support the text color if we're outputting to TTY.
+if [[ -t 1 ]]; then
+  C_BOLD_RED='\e[1;31m'
+  C_BOLD_GREEN='\e[1;32m'
+  C_BOLD_YELLOW='\e[1;33m'
+  C_OFF='\e[0m'
+fi
+
+# Show an info message.
+info() {
+  echo -e "${C_BOLD_GREEN}INFO: $*${C_OFF}"
+}
+
+# Show a warning message.
+warn() {
+  echo -e "${C_BOLD_YELLOW}WARNING: $*${C_OFF}"
+}
+
 # Show an error message.
 error() {
-  echo "ERROR: $*" >&2
+  echo -e "${C_BOLD_RED}ERROR: $*${C_OFF}" >&2
 }
 
 # Show an error message and exit.
