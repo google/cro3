488bf8e1e9c0af6960edbc2d9c3c0715c23d03c4
Congbin Guo <guocb@google.com>
2022-09-29 09:06:29 -0700

    chore(gscache): deprecate the python script for gscache
    BUG=b:246599150
    TEST=None
    
    Change-Id: I59bb4e9e04b6b9146fb2261f8a329e4d7cf1e264
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3928515
    Commit-Queue: Allen Li <ayatane@chromium.org>
    Auto-Submit: Congbin Guo <guocb@chromium.org>
    Reviewed-by: Allen Li <ayatane@chromium.org>
    Tested-by: Congbin Guo <guocb@chromium.org>

--- gs_cache/constants.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 The ChromiumOS Authors
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-"""Some constants used by gs_cache."""
-
-READ_BUFFER_SIZE_BYTES = 1024 * 1024  # 1 MB
