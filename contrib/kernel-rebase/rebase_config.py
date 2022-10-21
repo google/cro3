d21c03920db0a2c5c157e0b69a5c738cbe2703d2
Lukasz Majczak <lmajczak@google.com>
2022-10-21 10:40:46 +0000

    kernel-rebase: add drop for drm topic branch for v6.1-rc1
    BUG=None
    TEST=run kernel continuous rebase triage
    
    Change-Id: I252c0e4e275e4ae4d4d86e307afc1b3d377e3c0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/3970758
    Commit-Queue: Lukasz Majczak <lmajczak@google.com>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Tested-by: Lukasz Majczak <lmajczak@google.com>

--- contrib/kernel-rebase/rebase_config.py
+++ contrib/kernel-rebase/rebase_config.py
@@ -299,3 +299,4 @@ disp_overlay['f4370ef58dae'] = 'drop' # BACKPORT: FROMGIT: ANDROID: xt_qtaguid: 
 disp_overlay['56fdd9cd9158'] = 'drop' # BACKPORT: FROMGIT: ANDROID: xt_qtaguid: fix UAF race
 disp_overlay['4378383d6f00'] = 'drop' # CHROMIUM: media: platform: mtk-mdp3: Add VIDEO_MEDIATEK_MDP3 in Kconfig and Makefile
 disp_overlay['ae921dd92ca5'] = 'drop' # BACKPORT: FROMLIST: mt76: mt7921: get rid of the false positive reset
+disp_overlay['210b8cc77ab7'] = 'drop' # CHROMIUM: drm/bridge/ite-6505: Use drm_debug_enabled() instead of drm_debug
