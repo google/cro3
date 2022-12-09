31bc90f9ce11455f9881fa6a0f2f2894241cb7c2
Clayton Whitelaw <cawhitelaw@google.com>
2022-12-09 12:09:07 -0700

    contrib: consolidate device summary scripts
    Device_summary and get_device_info had some features in common and some
    not shared between the two. Combine them into device_summary and remove
    the other, per discussion with the author of get_device_info.
    
    Additional fields now queried:
    - Chrome browser version
    - EC version
    - Storage size
    - Memory channels
    - CPU Cores
    - Drivers: Media, VA, Mesa
    
    Modifications made from get_device_info:
    - Always uses the same method to fetch firmware versions
    - Uses new heuristic for storage size as largest lsblk output
      - The previous method did not work for my Nipperkin device that had
        the stateful partition set to a device mapper ID (dm-3) instead of
        the physical NVME device. Rather than track that down, this goes for
        a simpler heuristic.
    - Memory channels: only looks at Locator fields
      - My local device had asset tags fields with "Channel" in the name too
    - Mesa version: uses numbered version string regex
    - VA: Reports as VA API since that was the primary output
    
    Also organized the output a little better, splitting by category.
    
    BUG=b:242093024
    TEST=run on local nipperkin devce
    
    Change-Id: Ic1e116e2284a0029ee456ce8cb006dd5884c0a0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/4091822
    Reviewed-by: Casey G Bowman <casey.g.bowman@intel.com>
    Reviewed-by: Brian Norris <briannorris@chromium.org>
    Commit-Queue: Clayton Whitelaw <cawhitelaw@google.com>
    Tested-by: Clayton Whitelaw <cawhitelaw@google.com>
    Auto-Submit: Clayton Whitelaw <cawhitelaw@google.com>

--- contrib/device_summary.sh
+++ contrib/device_summary.sh
@@ -6,15 +6,13 @@
 # Script run from a DUT to report broad summary of characteristics and state
 # Example usage: ssh $DUT <device_summary.sh
 
-# TODO(b/242093024) combine with get_device_info.sh
-
-function keyval {
+keyval() {
   # Pads keys with whitespace to align output
   KEY_FIELD_LENGTH=20
   printf "%-${KEY_FIELD_LENGTH}s: %s\n" "$1" "$2"
 }
 
-function usage-error {
+usage-error() {
   2>&1 echo "This device doesn't appear to be a ChromeOS DUT."
   2>&1 echo "Run this script on the DUT, not on the host."
   2>&1 echo "To execute remotely, try 'ssh \$DUT <${0}'"
@@ -25,6 +23,15 @@ function usage-error {
 if [ -f /etc/cros_chroot_version ]; then
   usage-error
 fi
+
+HWID="$(crossystem hwid)"
+keyval "HWID" "${HWID}"
+
+SERIAL="$(vpd -g serial_number)"
+keyval "Serial" "${SERIAL}"
+
+echo
+echo "#Software"
 
 BOARD="$(grep CHROMEOS_RELEASE_BOARD </etc/lsb-release)"
 # Detect host machine outside chroot
@@ -40,6 +47,9 @@ OS="$(grep CHROMEOS_RELEASE_VERSION </etc/lsb-release)"
 MILESTONE="$(grep CHROMEOS_RELEASE_CHROME_MILESTONE </etc/lsb-release)"
 keyval "ChromeOS" "${OS:25} (M${MILESTONE:34})"
 
+BROWSER="$(/opt/google/chrome/chrome --version)"
+keyval "Chrome Browser" "${BROWSER}"
+
 KERNEL="$(uname -r)"
 keyval "Kernel" "${KERNEL}"
 
@@ -48,20 +58,43 @@ keyval "Firmware (active)" "${FW_ACTIVE}"
 FW_RO="$(crossystem ro_fwid)"
 keyval "Firmware (read-only)" "${FW_RO}"
 
-HWID="$(crossystem hwid)"
-keyval "HWID" "${HWID}"
+EC="$(ectool version 2>&1 | grep 'Build info' | awk '{ print $3 }')"
+keyval "EC" "${EC}"
 
-SERIAL="$(vpd -g serial_number)"
-keyval "Serial" "${SERIAL}"
+# Features that might vary by SKU
+
+echo
+echo "#Hardware characteristics"
 
 SKU= # appease shellcheck
 eval "$(crosid)" # sets SKU
 keyval "Firmware SKU ID" "${SKU}"
 
-# Features that might vary by SKU
-
 CPU_MODEL="$(grep "^model name" /proc/cpuinfo | head -n1)"
 keyval "CPU" "${CPU_MODEL:13}"
 
+CORES="$(nproc --all)"
+keyval "CPU cores" "${CORES}"
+
 MEM_KB="$(grep '^MemTotal:' /proc/meminfo  | awk '{ print $2 }')"
-keyval "Memory" "$((MEM_KB / 1024))M"
+MEM_CHANNELS="$(dmidecode -t memory | grep Locator | grep -c Channel)"
+keyval "Memory" "$((MEM_KB / 1024))M, ${MEM_CHANNELS} channels"
+
+# Heuristic - storage size based on largest lsblk output
+STORAGE="$(lsblk -x SIZE -o SIZE | tail -n1)"
+keyval "Storage" "${STORAGE}"
+
+# Drivers
+echo
+echo "#Drivers"
+
+MEDIA="$(vainfo 2>/dev/null | grep 'Driver version' \
+  | awk -F : '{ print $3 }' | sed 's/^\s//')"
+keyval "Media" "${MEDIA}"
+
+LIBVA="$(vainfo 2>/dev/null | grep 'vainfo: VA-API version' \
+  | awk -F : '{ print $3 }' | sed 's/^\s//')"
+keyval "VA API" "${LIBVA}"
+
+MESA="$(wflinfo -p null -a gles2 2>/dev/null | grep -E -o 'Mesa (\.?[[:digit:]]+)*')"
+keyval "Mesa" "${MESA}"
