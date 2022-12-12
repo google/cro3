9a3e88a7c9c271408d7535032b4190b896501799
Michael Ellis <michaelellis@google.com>
2022-12-12 13:15:08 -0700

    Add Fingerprint board files
    BUG=b:254883166
    TEST=run pacman.py
    
    Change-Id: I8a8d11e7454610815316aaccf4a6fda2bdbdae69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/4095569
    Tested-by: Michael Ellis <michaelellis@google.com>
    Reviewed-by: Michael Ellis <michaelellis@google.com>
    Commit-Queue: Michael Ellis <michaelellis@google.com>
    Reviewed-by: Robert Zieba <robertzieba@google.com>

---
+++ contrib/power_measurement/pacman/boards/fp/icetower.py
@@ -0,0 +1,19 @@
+# Copyright 2022 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Config for icetower v3"""
+
+PACS = [
+    # addr:ch  name                 nom         rsense
+    ('0x10:0', 'PPVAR_MCU',         3.300,      0.5),#R1
+    ('0x10:1', 'PPVAR_FP',          3.300,      0.5),#R10
+    ('0x10:2', 'PP3300_FP',         3.300,      0.5),#R875
+    ('0x10:3', 'PP1800_FP',         1.800,      0.5),#R911
+]
+
+RAILS = [
+]
+
+GPIOS = [
+]
