7e77cffb1dafbdc6e169108e593e103769f17d5e
Pavan Holla <pholla@chromium.org>
2023-02-06 19:07:16 +0000

    gen_control_files: Schedule tests on multiple suites
    Schedule most cellular_ota and cellular_ota_flaky tests on the repair suite.
    Also, schedule a few tests as part of CQ.
    
    BUG=b:260735445
    TEST=python gen_control_files.py
    
    Change-Id: Id938377b8828729f7497732389e0500ba192f847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/4225253
    Tested-by: Pavan Holla <pholla@google.com>
    Reviewed-by: Madhav <madhavadas@google.com>

--- net/gen_control_files.py
+++ net/gen_control_files.py
@@ -20,14 +20,14 @@ prefix = '''# Copyright 2023 The ChromiumOS Authors
 # Do not edit manually. Generated using ~/chromiumos/src/platform/dev/net/gen_control_files.py
 
 AUTHOR = 'Chromium OS team'
-NAME = 'tast.{suite}-{carrier}-{tag}'
+NAME = 'tast.{primary_suite}-{carrier}-{tag}'
 METADATA = {{
     "contacts": ["chromeos-cellular-team@google.com"],
     "bug_component": "b:167157", # ChromeOS > Platform > Connectivity > Cellular
     }}
 TIME = 'MEDIUM'
 TEST_TYPE = 'Server'
-ATTRIBUTES = 'suite:{suite}'
+ATTRIBUTES = '{suites}'
 MAX_RESULT_SIZE_KB = 1024 * 1024
 PY_VERSION = 3
 DEPENDENCIES = "carrier:{carrier}"
@@ -59,15 +59,24 @@ parallel_simple(run, machines)
 '''
 
 """
-Writes a control file at out_dir/control.suite-carrier-tag. The argument tests should specify the tests that need to be run in the control file.
+Writes a control file at out_dir/control.suite-carrier-tag. The argument l_tests should specify the tests that need to be run in the control file. If l_suite contains more than one element, the l_suite[0] is used to name the control file.
 """
 
 
 def write_control_file(l_out_dir, l_suite, l_carrier, l_tag, l_tests):
-    with open(os.path.join(l_out_dir, f"control.{l_suite}-{l_carrier}-{l_tag}"),
+    primary_suite=l_suite[0] # used to name the file and autotest
+    suites=""
+    for i, s in enumerate(l_suite):
+        if i == 0:
+            suites = "suite:" + s
+        else:
+            suites = suites + ", suite:" + s
+
+    with open(os.path.join(l_out_dir, f"control.{primary_suite}-{l_carrier}-{l_tag}"),
               "w") as control_file:
         control_file.write(
-            prefix.format(suite=l_suite, carrier=l_carrier, tag=l_tag) + l_tests + suffix)
+                prefix.format(primary_suite=primary_suite, carrier=l_carrier, tag=l_tag, suites=suites) + l_tests + suffix)
+
 
 
 single_test_template = '''
@@ -82,6 +91,7 @@ single_test_template = '''
                     )
 '''
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "-o", "--out_dir", help="Output directory for control files")
@@ -93,56 +103,60 @@ for carrier in ['verizon', 'tmobile', 'att',
                 'amarisoft', 'vodafone', 'rakuten', 'ee', 'kddi',
                 'docomo', 'softbank']:
 
+    dut_check_suites = ['cellular_ota','cellular_repair']
+    if carrier in ['verizon','tmobile','att']:
+        dut_check_suites.append('cellular-cq') # other carriers are sparsely deployed in the lab, thus CQ may fail to find DUTs
+
     tests = single_test_template.format(
         test_exprs="['cellular.IsModemUp']")
-    write_control_file(out_dir, 'cellular_ota',
+    write_control_file(out_dir, dut_check_suites,
                        carrier, 'is_modem_up', tests)
 
     tests = single_test_template.format(
         test_exprs=f"['cellular.Identifiers.{carrier}','cellular.IsConnected.{carrier}','cellular.Smoke.{carrier}']")
-    write_control_file(out_dir, 'cellular_ota',
+    write_control_file(out_dir, dut_check_suites,
                        carrier, 'dut_check', tests)
 
     tests = single_test_template.format(
         test_exprs=f"['cellular.Autoconnect.{carrier}','cellular.ShillEnableDisable.{carrier}']")
-    write_control_file(out_dir, 'cellular_ota',
+    write_control_file(out_dir, ['cellular_ota', 'cellular_repair'],
                        carrier, 'autoconnect_enable', tests)
 
     tests = single_test_template.format(
         test_exprs="['cellular.HostCellularNetworkConnectivity']")
-    write_control_file(out_dir, 'cellular_ota_flaky',
+    write_control_file(out_dir, ['cellular_ota_flaky'],
                        carrier, 'for_stabilization', tests)
 
     tests = single_test_template.format(
         test_exprs="['cellular.ShillCellularEnableAndConnect','cellular.HostCellularStressEnableDisable','cellular.ModemmanagerEnableAndConnect','cellular.ShillCellularSafetyDance']")
-    write_control_file(out_dir, 'cellular_ota_flaky',
+    write_control_file(out_dir, ['cellular_ota_flaky'],
                        carrier, 'stress', tests)
 
     exclude_sms = "" if carrier in ["tmobile","att"] else " && !\"cellular_sms\""
     tests = single_test_template.format(
         test_exprs="['(\"group:cellular\" && \"cellular_sim_active\" && \"cellular_unstable\" && !\"cellular_run_isolated\" && !\"cellular_e2e\"" + exclude_sms + ")']")
-    write_control_file(out_dir, 'cellular_ota_flaky',
+    write_control_file(out_dir, ['cellular_ota_flaky', 'cellular_repair'],
                        carrier, 'platform', tests)
 
     tests = single_test_template.format(
         test_exprs="['(\"group:cellular\" && \"cellular_sim_active\" && !\"cellular_unstable\" && !\"cellular_run_isolated\" && !\"cellular_e2e\"" + exclude_sms + ")']")
-    write_control_file(out_dir, 'cellular_ota',
+    write_control_file(out_dir, ['cellular_ota', 'cellular_repair'],
                        carrier, 'platform', tests)
 
     tests = single_test_template.format(test_exprs = "['cellular.*SuspendResume*']")
-    write_control_file(out_dir, 'cellular_ota_flaky',
+    write_control_file(out_dir, ['cellular_ota_flaky', 'cellular_repair'],
                        carrier, 'suspend_resume', tests)
 
     tests = single_test_template.format(
         test_exprs="['(\"group:cellular\" && \"cellular_sim_active\" && \"cellular_unstable\" && \"cellular_e2e\")']")
-    write_control_file(out_dir, 'cellular_ota_flaky',
+    write_control_file(out_dir, ['cellular_ota_flaky'],
                        carrier, 'e2e', tests)
 
     tests = single_test_template.format(
         test_exprs="['(\"group:cellular\" && \"cellular_sim_active\" && !\"cellular_unstable\" && \"cellular_e2e\")']")
-    write_control_file(out_dir, 'cellular_ota',
+    write_control_file(out_dir, ['cellular_ota'],
                        carrier, 'e2e', tests)
     tests = single_test_template.format(
         test_exprs="['(\"group:cellular_crosbolt\" && \"cellular_crosbolt_perf_nightly\")']")
-    write_control_file(out_dir, 'cellular_ota_perf_flaky',
+    write_control_file(out_dir, ['cellular_ota_perf_flaky'],
                        carrier, 'perf', tests)
