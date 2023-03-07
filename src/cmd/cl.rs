c97121faa875bae11ff247eae7f6212905a5b3f7
Hikaru Nishida <hikalium@chromium.org>
2023-03-07 17:19:17 +0900

    lium: lium cl pick to support a specific patchset
    BUG=None
    TEST=make commit
    
    Change-Id: I33f799e0babd2c16ef77ed36d9eec30678f9c431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/dev-util/+/4313964
    Auto-Submit: Hikaru Nishida <hikalium@chromium.org>
    Tested-by: Hikaru Nishida <hikalium@chromium.org>
    Reviewed-by: Masami Hiramatsu <mhiramat@google.com>
    Commit-Queue: Masami Hiramatsu <mhiramat@google.com>

--- src/cmd/cl.rs
+++ src/cmd/cl.rs
@@ -2,10 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use anyhow::Context;
 use anyhow::Result;
 use argh::FromArgs;
+use lazy_static::lazy_static;
 use lium::chroot::Chroot;
 use lium::repo::get_repo_dir;
+use regex::Regex;
+
+lazy_static! {
+    static ref RE_GERRIT_CL: Regex = Regex::new(r"^(?P<cl>[0-9]+)/(?P<patchset>[0-9+])$").unwrap();
+}
 
 #[derive(FromArgs, PartialEq, Debug)]
 /// CL (Change List) helpers
@@ -26,7 +33,7 @@ pub fn run(args: &Args) -> Result<()> {
 }
 
 #[derive(FromArgs, PartialEq, Debug)]
-/// cherry-pick CLs
+/// cherry-pick a CL
 #[argh(subcommand, name = "pick")]
 pub struct ArgsPick {
     /// target cros repo dir
@@ -37,30 +44,44 @@ pub struct ArgsPick {
     #[argh(option)]
     dir: String,
 
-    /// CL number to checkout
+    /// CLs to checkout (e.g. "4196467", "4196467/2")
     #[argh(positional)]
-    cl: u64,
+    cl: String,
 }
 fn run_pick(args: &ArgsPick) -> Result<()> {
-    let cl = args.cl;
+    let capture = RE_GERRIT_CL
+        .captures(&args.cl)
+        .context("Invalid CL id. please specify patchset number as well (like '1234/5').")?;
+    let cl = &capture["cl"];
+    let cl_suffix = &cl[cl.len() - 2..];
+    let patchset = &capture["patchset"];
     let dir = &args.dir;
     let chroot = Chroot::new(&get_repo_dir(&args.repo)?)?;
     chroot.run_bash_script_in_chroot(
         "checkout",
         &format!(
             r###"
-gerrit --json --no-pager deps {cl} | tee tmp.txt
-export REF=`cat tmp.txt | jq -r '. | map([.currentPatchSet.ref, .currentPatchSet.revision])[0][0]'`
-export PROJ=`cat tmp.txt | jq -r '. | map([.currentPatchSet.ref, .currentPatchSet.revision, .project])[0][2]'`
-echo ${{PROJ}} ${{REF}}
-export BRANCH=`echo ${{REF}} | sed -E 's!^.*/([0-9]+)/([0-9]+)$!cl-\1_ps-\2!'`
-echo ${{BRANCH}}
 cd ~/chromiumos
-git -C {dir} fetch https://chromium.googlesource.com/${{PROJ}} ${{REF}} &&
-git -C {dir} cherry-pick FETCH_HEAD
+cd {dir}
+export PROJ=`repo info . | grep -e 'Project:' | cut -d ' ' -f 2`
+echo "PROJ=${{PROJ}}"
+git fetch https://chromium.googlesource.com/${{PROJ}} \
+  refs/changes/{cl_suffix}/{cl}/{patchset}
+git cherry-pick FETCH_HEAD || git cherry-pick --abort
 "###,
         ),
         None,
     )?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn regex() {
+        assert!(&RE_GERRIT_CL.captures("1234").is_none());
+        assert_eq!(&RE_GERRIT_CL.captures("1234/5").unwrap()["cl"], "1234");
+        assert_eq!(&RE_GERRIT_CL.captures("1234/5").unwrap()["patchset"], "5");
+    }
 }
