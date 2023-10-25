8538b0457ff31e0208f259b2f088554b4fa6a0e0
hikalium <hikalium@users.noreply.github.com>
2023-10-25 17:17:34 +0900

    Fix webpack usage to generate a sourcemap correctly (#112)
    Webpack was not properly used so the generated source maps were for the js files generated by tsc, not the original ts file. Fix it by generating the correct source map with webpack by calling tsc from it, not from the Makefile.
    
    Also add a CI recipe to make sure the typescript sources have no trivial errors by running build and lint.

---
+++ docs/servo_power_monitor/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true
+  },
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  parser: '@typescript-eslint/parser',
+  rules: {
+  }
+}
