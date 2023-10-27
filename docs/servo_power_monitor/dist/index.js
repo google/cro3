a87708922a1a0ad5d2e2c06d87f2988f55a8b035
wtrgjadmw <fukuda.m0113@icloud.com>
2023-10-27 10:59:14 +0900

    change serial console style

--- docs/servo_power_monitor/dist/index.js
+++ docs/servo_power_monitor/dist/index.js
@@ -33908,28 +33908,18 @@ selectDUTSerialButton.addEventListener('click', async () => {
         throw e;
     });
     await DUTPort.open({ baudRate: 115200 });
-    let listItem = document.createElement('li');
-    listItem.textContent = 'DUTPort is selected';
-    messages.appendChild(listItem);
+    messages.textContent += 'DUTPort is selected';
     const DUTReadable = DUTPort.readable;
     if (DUTReadable === null)
         return;
     const DUTReader = DUTReadable.getReader();
-    listItem = document.createElement('li');
-    messages.appendChild(listItem);
     DUTReader.read().then(function processText({ done, value }) {
         if (done) {
             console.log('Stream complete');
             return;
         }
         const chunk = decoder.decode(value, { stream: true });
-        const chunk_split_list = chunk.split('\n');
-        for (let i = 0; i < chunk_split_list.length - 1; i++) {
-            listItem.textContent += chunk_split_list[i];
-            listItem = document.createElement('li');
-            messages.appendChild(listItem);
-        }
-        listItem.textContent += chunk_split_list[chunk_split_list.length - 1];
+        messages.textContent += chunk;
         messages.scrollTo(0, messages.scrollHeight);
         DUTReader.read().then(processText);
     });
