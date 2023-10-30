825ed787d57cbd5ad03483deb26cd14be34a1438
wtrgjadmw <fukuda.m0113@icloud.com>
2023-10-30 14:38:15 +0900

    lint fix ui.d.ts

--- docs/servo_power_monitor/src/ui.d.ts
+++ docs/servo_power_monitor/src/ui.d.ts
@@ -1,4 +1,6 @@
-export declare function requestSerialAddClickEvent(fn: () => Promise<void>): void;
+export declare function requestSerialAddClickEvent(
+  fn: () => Promise<void>
+): void;
 export declare function requestUSBAddClickEvent(fn: () => Promise<void>): void;
 export declare function haltAddClickEvent(fn: () => Promise<void>): void;
 export declare function enabledRecordingButton(halt: boolean): void;
@@ -6,13 +8,25 @@ export declare function setPopupCloseButton(): void;
 export declare function closePopup(): void;
 export declare function addEmptyListItemToMessages(): void;
 export declare function addMessageToConsole(s: string): void;
-export declare function selectDUTSerialAddClickEvent(fn: () => Promise<void>): void;
-export declare function executeScriptAddClickEvent(fn: () => Promise<void>): void;
-export declare function formAddSubmitEvent(fn: (e: Event) => Promise<void>): void;
-export declare function inputAddKeydownEvent(fn: (e: KeyboardEvent) => Promise<void>): void;
+export declare function selectDUTSerialAddClickEvent(
+  fn: () => Promise<void>
+): void;
+export declare function executeScriptAddClickEvent(
+  fn: () => Promise<void>
+): void;
+export declare function formAddSubmitEvent(
+  fn: (e: Event) => Promise<void>
+): void;
+export declare function inputAddKeydownEvent(
+  fn: (e: KeyboardEvent) => Promise<void>
+): void;
 export declare function readInputValue(): string;
 export declare function downloadAddClickEvent(fn: () => void): void;
 export declare function analyzeAddClickEvent(fn: () => void): void;
 export declare function setDownloadAnchor(dataStr: string): void;
-export declare function dropZoneAddDragoverEvent(fn: (evt: DragEvent) => void): void;
-export declare function dropZoneAddDropEvent(fn: (evt: DragEvent) => void): void;
+export declare function dropZoneAddDragoverEvent(
+  fn: (evt: DragEvent) => void
+): void;
+export declare function dropZoneAddDropEvent(
+  fn: (evt: DragEvent) => void
+): void;
