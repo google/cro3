68a256ae421344d9032ba9010249bc85d0d8ab0f
fukuda <64108611+wtrgjadmw@users.noreply.github.com>
2023-11-13 15:22:23 +0900

    Add annotation (#140)
    Add annotation in the power graph when the "start" or "end" signal comes in DUT console.
    X-axis value is expressed as a relative time from the start of measurement
    to avoid adding annotation when copying shell script to DUT. Also the script is encoded in base64 to include annotation information to export the JSON file. The data format is defined in the design doc.

--- docs/servo_power_monitor/src/power_test_controller.ts
+++ docs/servo_power_monitor/src/power_test_controller.ts
@@ -2,20 +2,31 @@ import {ServoController} from './servo_controller';
 import {Graph} from './graph';
 import {Histogram} from './histogram';
 import {Ui} from './ui';
+import {TestRunner} from './test_runner';
 
+export type PowerData = [number, number];
+export type AnnotationData = [number, string];
 export class PowerTestController {
   private INTERVAL_MS = 100;
   public halt = true;
   private inProgress = false;
   private ui: Ui;
   private servoController: ServoController;
-  private powerData: Array<Array<Date | number>> = [];
+  private runner: TestRunner;
+  private powerDataList: Array<PowerData> = [];
+  private annotationList: Array<AnnotationData> = [];
   private graph: Graph;
   private histogram = new Histogram();
-  constructor(ui: Ui, graph: Graph, servoController: ServoController) {
+  constructor(
+    ui: Ui,
+    graph: Graph,
+    servoController: ServoController,
+    runner: TestRunner
+  ) {
     this.ui = ui;
     this.graph = graph;
     this.servoController = servoController;
+    this.runner = runner;
   }
   private changeHaltFlag(flag: boolean) {
     this.halt = flag;
@@ -42,9 +53,30 @@ export class PowerTestController {
       this.inProgress = false;
       if (currentPowerData === undefined) continue;
       this.ui.setSerialOutput(currentPowerData.originalData);
-      const e: Array<Date | number> = [new Date(), currentPowerData.power];
-      this.powerData.push(e);
-      this.graph.updateGraph(this.powerData);
+      const e: PowerData = [new Date().getTime(), currentPowerData.power];
+      this.powerDataList.push(e);
+      this.graph.updateGraph(this.powerDataList);
+    }
+  }
+  private async readDutLoop() {
+    this.runner.executeCommand('\n');
+    this.ui.addMessageToConsole('DutPort is selected');
+    for (;;) {
+      const dutData = await this.runner.readData();
+      if (dutData.includes('start')) {
+        this.annotationList.push([new Date().getTime(), 'start']);
+        this.graph.addAnnotation(
+          this.powerDataList[this.powerDataList.length - 1][0],
+          'start'
+        );
+      } else if (dutData.includes('end')) {
+        this.annotationList.push([new Date().getTime(), 'end']);
+        this.graph.addAnnotation(
+          this.powerDataList[this.powerDataList.length - 1][0],
+          'end'
+        );
+      }
+      this.ui.addMessageToConsole(dutData);
     }
   }
   public async startMeasurement() {
@@ -58,22 +90,43 @@ export class PowerTestController {
     this.inProgress = false;
     await this.servoController.servoShell.close();
   }
+  public async selectPort() {
+    await this.runner.dut.open();
+    this.runner.isOpened = true;
+    this.readDutLoop();
+  }
   public analyzePowerData() {
     // https://dygraphs.com/jsdoc/symbols/Dygraph.html#xAxisRange
     const xrange = this.graph.returnXrange();
     const left = xrange[0];
     const right = xrange[1];
-    this.histogram.paintHistogram(left, right, this.powerData);
+    this.histogram.paintHistogram(left, right, this.powerDataList);
   }
   public loadPowerData(s: string) {
     const data = JSON.parse(s);
-    this.powerData = data.power.map((d: string) => [new Date(d[0]), d[1]]);
-    this.graph.updateGraph(this.powerData);
+    this.powerDataList = data.power.map((d: {time: number; power: number}) => [
+      d.time,
+      d.power,
+    ]);
+    this.annotationList = data.annotation.map(
+      (d: {text: string; time: number}) => [d.time, d.text]
+    );
+    this.graph.updateGraph(this.powerDataList);
+    this.graph.findAnnotationPoint(this.powerDataList, this.annotationList);
   }
   public exportPowerData() {
     const dataStr =
       'data:text/json;charset=utf-8,' +
-      encodeURIComponent(JSON.stringify({power: this.powerData}));
+      encodeURIComponent(
+        JSON.stringify({
+          power: this.powerDataList.map(d => {
+            return {time: d[0], power: d[1]};
+          }),
+          annotation: this.annotationList.map(d => {
+            return {time: d[0], text: d[1]};
+          }),
+        })
+      );
     return dataStr;
   }
   public setupDisconnectEvent() {
