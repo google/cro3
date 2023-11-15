4411f60a37042fbaf2566fb2a293bc155fd0b0d3
Anri Nakamura <anrin@google.com>
2023-11-15 19:37:41 +0900

    Add commands and --extra-args option.

--- src/cmd/vm.rs
+++ src/cmd/vm.rs
@@ -12,11 +12,22 @@ use lium::config::Config;
 #[derive(FromArgs, PartialEq, Debug)]
 /// create a virtual machine
 #[argh(subcommand, name = "vm")]
-pub struct Args {}
+pub struct Args {
+    #[argh(subcommand)]
+    nested: SubCommand,
+}
 
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand)]
+enum SubCommand {
+    Connect(ArgsConnect),
+    Setup(ArgsSetup),
+    Start(ArgsStart),
+    Push(ArgsPush),
+}
+
+#[tracing::instrument(level = "trace")]
 pub fn run(args: &Args) -> Result<()> {
-    let _arg = args;
-
     let config = Config::read()?;
     if !config.is_internal() {
         return Err(anyhow!(
@@ -24,5 +35,62 @@ pub fn run(args: &Args) -> Result<()> {
         ));
     }
 
+    match &args.nested {
+        SubCommand::Connect(args) => run_connect(args),
+        SubCommand::Setup(args) => run_setup(args),
+        SubCommand::Start(args) => run_start(args),
+        SubCommand::Push(args) => run_push(args),
+    }
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// connects to a running betty instance via SSH
+#[argh(subcommand, name = "connect")]
+pub struct ArgsConnect {
+    /// extra arguments
+    #[argh(option)]
+    extra_args: Vec<String>,
+}
+
+fn run_connect(_args: &ArgsConnect) -> Result<()> {
+    Ok(())
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// run first time setup, installs necessary dependencies
+#[argh(subcommand, name = "setup")]
+pub struct ArgsSetup {
+    /// extra arguments
+    #[argh(option)]
+    extra_args: Vec<String>,
+}
+
+fn run_setup(_args: &ArgsSetup) -> Result<()> {
+    Ok(())
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// start a betty VM instance
+#[argh(subcommand, name = "start")]
+pub struct ArgsStart {
+    /// extra arguments
+    #[argh(option)]
+    extra_args: Vec<String>,
+}
+
+fn run_start(_args: &ArgsStart) -> Result<()> {
+    Ok(())
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// pushes an Android build a running betty instance
+#[argh(subcommand, name = "push")]
+pub struct ArgsPush {
+    /// extra arguments
+    #[argh(option)]
+    extra_args: Vec<String>,
+}
+
+fn run_push(_args: &ArgsPush) -> Result<()> {
     Ok(())
 }
