dd0af020dca2efb882f785310f2148041cbe227f
Anri Nakamura <anrin@google.com>
2023-11-16 11:39:18 +0900

    Add options of the betty script.

--- src/cmd/vm.rs
+++ src/cmd/vm.rs
@@ -73,12 +73,59 @@ fn run_setup(_args: &ArgsSetup) -> Result<()> {
 /// start a betty VM instance
 #[argh(subcommand, name = "start")]
 pub struct ArgsStart {
+    /// the BOARD to run (e.g. betty-pi-arc)
+    #[argh(option, short = 'b')]
+    board: String,
+
+    /// reuse the VM image. It is true by default.
+    #[argh(option, default = "true")]
+    reuse_disk_image: bool,
+
+    /// specifies the display device for the VM. "vnc" (default) creates a VNC
+    /// display on localhost:5900; "virgl" enables guest 3D acceleration,
+    /// assuming X11 (or use xvfb-run) and qemu compiled with GTK/virgl support;
+    /// "none" removes the VGA device, which can be used to simulate the GCE
+    /// environment (i.e. L1 betty).
+    #[argh(option)]
+    display: Option<String>,
+
+    /// the ChromeOS version to use (e.g. R72-11268.0.0). Alternatively,
+    /// postsubmit builds since R96-14175.0.0-53101 can also be specified. It is
+    /// the latest version by default.
+    #[argh(option)]
+    version: Option<String>,
+
+    /// the android version to push. This is passed to push_to_device.py. e.g.
+    /// cheets_x86/userdebug/123456
+    #[argh(option, short = 'a')]
+    android_build: Option<String>,
+
     /// extra arguments
     #[argh(option)]
     extra_args: Vec<String>,
 }
 
-fn run_start(_args: &ArgsStart) -> Result<()> {
+fn run_start(args: &ArgsStart) -> Result<()> {
+    let mut vec = Vec::new();
+    vec.append(&mut vec!["--board", &args.board]);
+    if !args.reuse_disk_image {
+        vec.append(&mut vec!["--reset_image"])
+    }
+    if let Some(display) = &args.display {
+        vec.append(&mut vec!["--display", display]);
+    }
+    if let Some(version) = &args.version {
+        vec.append(&mut vec!["--release", version]);
+    }
+    if let Some(android_build) = &args.android_build {
+        vec.append(&mut vec!["--android_build", android_build]);
+    }
+
+    let mut options: Vec<String> = vec.iter().map(|s| s.to_string()).collect();
+
+    let extra_args = &args.extra_args;
+    options.append(&mut extra_args.clone());
+
     Ok(())
 }
 
@@ -86,11 +133,23 @@ fn run_start(_args: &ArgsStart) -> Result<()> {
 /// pushes an Android build a running betty instance
 #[argh(subcommand, name = "push")]
 pub struct ArgsPush {
+    /// the android version to push. This is passed to push_to_device.py. e.g.
+    /// cheets_x86/userdebug/123456
+    #[argh(option, short = 'a')]
+    android_build: String,
+
     /// extra arguments
     #[argh(option)]
     extra_args: Vec<String>,
 }
 
-fn run_push(_args: &ArgsPush) -> Result<()> {
+fn run_push(args: &ArgsPush) -> Result<()> {
+    let vec = ["android_build", &args.android_build];
+
+    let mut options: Vec<String> = vec.iter().map(|s| s.to_string()).collect();
+
+    let extra_args = &args.extra_args;
+    options.append(&mut extra_args.clone());
+
     Ok(())
 }
