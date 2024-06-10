df5502c29f3efdf703a4867af4f18af581867c3c
Masami Hiramatsu <mhiramat@google.com>
2024-06-10 12:09:22 +0900

    bash_completion_check.sh: Add tast list completion check

--- scripts/bash_completion_check.sh
+++ scripts/bash_completion_check.sh
@@ -19,10 +19,30 @@ Commands:
   shibuya
   roppongi
   dut
+  tast
 EOF
   fi
   if [ "$*" = "dut list --ids" ]; then
     echo "dut1 dut2 dut3"
+  fi
+
+  if [ "$*" = "tast run --help" ]; then
+    cat << EOF
+Usage: cro3 tast run <tests>
+
+Get tast test for the target DUT
+
+Positional Arguments:
+  tests             test name or pattern
+
+Options:
+  --help            display usage information
+EOF
+  fi
+  if [ "$*" = "tast list --cached" ]; then
+    echo "dummy.Test.First"
+    echo "dummy.Test.Second"
+    echo "dummy.TestThird"
   fi
 }
 
@@ -49,4 +69,10 @@ echo "${COMPREPLY[@]}" | grep -wq "dut1"
 echo "${COMPREPLY[@]}" | grep -wq "dut2"
 echo "${COMPREPLY[@]}" | grep -wq "dut5" && exit 1
 
+COMP_CWORD=3
+COMP_WORDS=("cro3" "tast" "run" "")
+test_complete
+echo "${COMPREPLY[@]}" | grep -wq "dummy.Test.First"
+echo "${COMPREPLY[@]}" | grep -wq "dummy.Test.Second"
+echo "${COMPREPLY[@]}" | grep -wq "dummy.Third" && exit 1
 exit 0
